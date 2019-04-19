a02259a9239a7a16284b7916953cd81e3bf743a4
Orhan CINAR <orhancinar35@gmail.com>
2019-04-19 02:04:11 +0200

    add array rotation

---
+++ C-Sharp-Practice/Arrays/ArrayRotation.cs
@@ -0,0 +1,26 @@
+﻿namespace C_Sharp_Practice.Arrays
+{
+    public class ArrayRotation
+    {
+        public int[] ArrayRoration1(int[] input, int d, int n)
+        {
+            int[] tmpArray = new int[d];
+            for (int i = 0; i < d; i++)
+            {
+                tmpArray[i] = input[i];
+            }
+
+            for (int i = d; i < n; i++)
+            {
+                input[i-d] = input[i];
+            }
+
+            for (int i = 0; i < d; i++)
+            {
+                input[n - d + i] = tmpArray[i];
+            }
+
+            return input;
+        }
+    }
+}
---
+++ XUnitTestProject/Arrays/ArrayRotationTest.cs
@@ -0,0 +1,22 @@
+﻿using C_Sharp_Practice.Arrays;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace XUnitTestProject.Arrays
+{
+    public class ArrayRotationTest
+    {
+
+        ArrayRotation _arrayRotation = new ArrayRotation();
+
+        [Fact]
+        public void Test_BubbleSortAsc()
+        {
+            var result = _arrayRotation.ArrayRoration1(new[] { 1, 2, 3, 4, 5, 6, 7 },2,7);
+            var expectedAsc = new[] { 3, 4, 5, 6, 7, 1, 2 };
+            Assert.Equal(expectedAsc, result);
+        }
+    }
+}
