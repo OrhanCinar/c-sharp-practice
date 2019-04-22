d98d44c598b38dd2c38849c53f30472cbbfec384
Orhan CINAR <orhancinar35@gmail.com>
2019-04-22 15:35:47 +0200

    add sort test

---
+++ XUnitTestProject/Sorting/BubbleSortTest.cs
@@ -0,0 +1,31 @@
+using C_Sharp_Practice;
+using Xunit;
+
+namespace XUnitTestProject
+{
+    public class SortTests
+    {
+        BubbleSort bubbleSort;
+
+        public SortTests()
+        {
+            bubbleSort = new BubbleSort();
+        }
+
+        [Fact]
+        public void Test_BubbleSortAsc()
+        {
+            var result = bubbleSort.SortAsc(new[] { 1, 659, 54, 69, 0 });
+            var expectedAsc = new[] { 0, 1, 54, 69, 659 };
+            Assert.Equal(expectedAsc, result);
+        }
+
+        [Fact]
+        public void Test_BubbleSortDesc()
+        {
+            var result = bubbleSort.SortDesc(new[] { 1, 659, 54, 69, 0 });
+            var expectedDesc = new[] { 659, 69, 54, 1, 0 };
+            Assert.Equal(expectedDesc, result);
+        }
+    }
+}
