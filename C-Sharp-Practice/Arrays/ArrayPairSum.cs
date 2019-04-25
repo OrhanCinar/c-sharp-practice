967f65271d367b9028705b54bfb36827f2721dae
Orhan CINAR <orhancinar35@gmail.com>
2019-04-26 00:02:15 +0200

    added array pair sum

---
+++ C-Sharp-Practice/Arrays/ArrayPairSum.cs
@@ -0,0 +1,39 @@
+﻿namespace C_Sharp_Practice.Arrays
+{
+    public class ArrayPairSum
+    {
+        public bool CheckArrayPairSum(int[] arr, int sum)
+        {
+            int i;
+            for (i = 0; i < arr.Length - 1; i++)
+            {
+                if (arr[i] > arr[i + 1])
+                {
+                    break;
+                }
+            }
+
+            int l = (i + 1) % arr.Length;
+
+            int r = 1;
+
+            while (l != r)
+            {
+                if (arr[l] + arr[r] == sum)
+                {
+                    return true;
+                }
+
+                if (arr[l] + arr[r] < sum)
+                {
+                    l = (l + 1) % arr.Length;
+                }
+                else
+                {
+                    r = (arr.Length + r - 1) % arr.Length;
+                }
+            }
+            return false;
+        }
+    }
+}
--- XUnitTestProject/Arrays/ArrayRotationTest.cs
+++ XUnitTestProject/Arrays/ArrayRotationTest.cs
@@ -13,6 +13,7 @@ namespace XUnitTestProject.Arrays
         ArrayRotationBlockSwap _arrayRotationBlock = new ArrayRotationBlockSwap();
         ArrayRotationReversal _arrayRotationReversal = new ArrayRotationReversal();
         ArrayRotationCyclically _arrayRotationCyclically = new ArrayRotationCyclically();
+        ArrayPairSum _arrayPairSum = new ArrayPairSum();
 
         [Fact]
         public void Test_ArrayRoration1()
@@ -60,6 +61,14 @@ namespace XUnitTestProject.Arrays
             var result = _arrayRotationCyclically.RotateArray(new[] { 1, 2, 3, 4, 5});
             var expected = new[] { 5, 1, 2, 3, 4 };
             Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public void Test_CheckArrayPairSum()
+        {
+            var result = _arrayPairSum.CheckArrayPairSum(new[] { 11, 15, 6, 8, 9, 10 },16);
+            
+            Assert.Equal(true, result);
         }
     }
 }
