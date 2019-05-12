7d7f857af8a36d2628f4c5f8752e2c5d0714272e
Orhan CINAR <orhancinar35@gmail.com>
2019-05-12 21:46:02 +0200

    added array rotation find element index

---
+++ C-Sharp-Practice/Arrays/ArrayRotationElementIndex.cs
@@ -0,0 +1,28 @@
+﻿namespace C_Sharp_Practice.Arrays
+{
+    public class ArrayRotationElementIndex
+    {
+        public int FindElement(int[] arr, int[,] ranges, int rotations, int index)
+        {
+            for (int i = rotations - 1; i >= 0; i--)
+            {
+                int left = ranges[i, 0];
+                int right = ranges[i, 1];
+
+                if (left <= index && right >= index)
+                {
+                    if (index == left)
+                    {
+                        index = right;
+                    }
+                    else
+                    {
+                        index--;
+                    }
+                }
+            }
+
+            return arr[index];
+        }
+    }
+}
