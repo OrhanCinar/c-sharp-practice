0e86b10a819c2283f56010546f08f84f7ebe4e3c
Orhan CINAR <orhancinar35@gmail.com>
2019-05-15 03:35:34 +0200

    add array rearrange

---
+++ C-Sharp-Practice/Arrays/ArrayReArrange.cs
@@ -0,0 +1,34 @@
+﻿namespace C_Sharp_Practice.Arrays
+{
+    public class ArrayReArrange
+    {
+        public int[] ReArrangeArray(int[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] != -1 && arr[i] != i) 
+                {
+                    int x = arr[i];
+
+                    while (arr[x] != -1 && arr[x] != x)
+                    {
+                        int y = arr[x];
+
+                        arr[x] = x;
+
+                        x = y;
+                    }
+
+                    arr[x] = x;
+
+                    if (arr[i] != i)
+                    {
+                        arr[i] = -1;
+                    }
+                }
+            }
+
+            return arr;
+        }
+    }
+}
