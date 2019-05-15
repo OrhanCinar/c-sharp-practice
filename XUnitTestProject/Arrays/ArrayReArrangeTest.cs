49beddabed0e2e78b2770eb3201851abf9e86799
Orhan CINAR <orhancinar35@gmail.com>
2019-05-15 03:35:42 +0200

    add test

---
+++ XUnitTestProject/Arrays/ArrayReArrangeTest.cs
@@ -0,0 +1,18 @@
+﻿using C_Sharp_Practice.Arrays;
+using Xunit;
+
+namespace XUnitTestProject.Arrays
+{
+    public class ArrayReArrangeTest
+    {
+        ArrayReArrange _arrayReArrange = new ArrayReArrange();
+
+        [Fact]
+        public void Test_ArrayReArrange()
+        {
+            var result = _arrayReArrange.ReArrangeArray(new[] { -1, -1, 6, 1, 9, 3, 2, -1, 4, -1 });
+            var expected = new[] { -1, 1, 2, 3, 4, -1, 6, -1, -1, 9 };
+            Assert.Equal(expected, result);
+        }
+    }
+}
