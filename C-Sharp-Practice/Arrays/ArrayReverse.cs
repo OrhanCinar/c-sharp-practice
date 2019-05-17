f20351d612cd521ab49ea19a4c1753a790ae2ae6
Orhan CINAR <orhancinar35@gmail.com>
2019-05-18 01:13:58 +0200

    add array reverse

---
+++ C-Sharp-Practice/Arrays/ArrayReverse.cs
@@ -0,0 +1,21 @@
+﻿namespace C_Sharp_Practice.Arrays
+{
+    public class ArrayReverse
+    {
+        public int[] ReverseArray(int[] arr, int start, int end)
+        {
+            int tmp;
+
+            while (start < end)
+            {
+                tmp = arr[start];
+                arr[start] = arr[end];
+                arr[end] = tmp;
+                start++;
+                end--;
+            }
+
+            return arr;
+        }
+    }
+}
