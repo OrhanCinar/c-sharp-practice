3470e052c27370f5b83a1c2dcdfdc743498f38b3
Orhan CINAR <orhancinar35@gmail.com>
2019-05-18 01:14:09 +0200

    add array reverse test

---
+++ XUnitTestProject/Arrays/ArrayReverseTest.cs
@@ -0,0 +1,18 @@
+﻿using C_Sharp_Practice.Arrays;
+using Xunit;
+
+namespace XUnitTestProject.Arrays
+{
+    public class ArrayReverseTest
+    {
+        [Fact]
+        public void Test_ArrayReverse()
+        {
+            var arrayReverse = new ArrayReverse();
+
+            var result = arrayReverse.ReverseArray(new[] { 1, 2, 3, 4, 5, 6 }, 0, 5);
+            var expected = new[] { 6, 5, 4, 3, 2, 1 };
+            Assert.Equal(expected, result);
+        }
+    }
+}
