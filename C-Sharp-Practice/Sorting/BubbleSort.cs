0939c2b4eba107e2d3cf32a2a6484dffa496b640
Orhan CINAR <orhancinar35@gmail.com>
2019-05-23 15:26:34 +0200

    add bubble sort

---
+++ C-Sharp-Practice/Sorting/BubbleSort.cs
@@ -0,0 +1,51 @@
+﻿namespace C_Sharp_Practice
+{
+    public class BubbleSort
+    {        
+        public int[] SortDesc(int[] elements)
+        {
+            int temp;
+            bool flag = true;
+
+            for (int i = 0; i < elements.Length && flag; i++)
+            {
+                flag = false;
+                for (int j = 0; j < elements.Length - 1; j++)
+                {
+                    if (elements[j + 1] > elements[j])
+                    {
+                        temp = elements[j];
+                        elements[j] = elements[j + 1];
+                        elements[j + 1] = temp;
+                        flag = true;
+                    }
+                }
+            }
+
+            return elements;
+        }
+
+        public int[] SortAsc(int[] elements)
+        {
+            int temp;
+            bool flag = true;
+
+            for (int i = 0; i < elements.Length && flag; i++)
+            {
+                flag = false;
+                for (int j = 0; j < elements.Length - 1; j++)
+                {
+                    if (elements[j + 1] < elements[j])
+                    {
+                        temp = elements[j];
+                        elements[j] = elements[j + 1];
+                        elements[j + 1] = temp;
+                        flag = true;
+                    }
+                }
+            }
+
+            return elements;
+        }
+    }
+}
