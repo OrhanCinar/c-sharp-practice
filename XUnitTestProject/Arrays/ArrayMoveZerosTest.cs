daa607c9c64ce162a51a6ebb4d386046842ea1d0
Orhan CINAR <orhancinar35@gmail.com>
2019-06-03 01:28:59 +0200

    add test

---
+++ XUnitTestProject/Arrays/ArrayMoveZerosTest.cs
@@ -0,0 +1,20 @@
+﻿using C_Sharp_Practice.Arrays;
+using Xunit;
+
+namespace XUnitTestProject.Arrays
+{
+    public class ArrayMoveZerosTest
+    {
+        ArrayMoveZeros _arrayMoveZeros = new ArrayMoveZeros();
+
+        [Fact]
+        public void Test_ArrayMoveZeros()
+        {
+            var arr = new[] { 1, 9, 8, 4, 0, 0, 2, 7, 0, 6, 0, 9 };
+            int n = arr.Length;
+            var result = _arrayMoveZeros.PushZerosToEnd(arr, n);
+            var expected = new[] { 1, 9, 8, 4, 2, 7, 6, 9, 0, 0, 0, 0 };
+            Assert.Equal(expected, result);
+        }
+    }
+}
