beb1becc06dfa66d6f9430fef1162890c18853ab
Orhan CINAR <orhancinar35@gmail.com>
2019-06-02 12:32:20 +0200

    add array move zero

---
+++ C-Sharp-Practice/Arrays/ArrayMoveZeros.cs
@@ -0,0 +1,25 @@
+﻿namespace C_Sharp_Practice.Arrays
+{
+    public class ArrayMoveZeros
+    {
+        public int[] PushZerosToEnd(int[] arr, int n)
+        {
+            int count = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                if (arr[i] != 0)
+                {
+                    arr[count++] = arr[i];
+                }
+            }
+
+            while (count < n)
+            {
+                arr[count++] = 0;
+            }
+
+            return arr;
+        }
+    }
+}
