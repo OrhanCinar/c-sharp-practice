d130406afaec812a4a54e1b68c8f83e7c4b0ff08
Orhan CINAR <orhancinar35@gmail.com>
2019-06-04 23:27:25 +0200

    add array move zero 2

--- C-Sharp-Practice/Arrays/ArrayMoveZeros.cs
+++ C-Sharp-Practice/Arrays/ArrayMoveZeros.cs
@@ -21,5 +21,24 @@
 
             return arr;
         }
+
+        public int[] PushZerosToEnd2(int[] arr, int n)
+        {
+            int count = 0;
+            int tmp;
+
+            for (int i = 0; i < n; i++)
+            {
+                if (arr[i] != 0)
+                {
+                    tmp = arr[count];
+                    arr[count] = arr[i];
+                    arr[i] = tmp;
+                    count++;
+                }
+            }
+
+            return arr;
+        }
     }
 }
