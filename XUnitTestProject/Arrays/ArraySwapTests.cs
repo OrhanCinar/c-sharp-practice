241036e36563fc936d485712df4bbb10ee33e53a
Orhan CINAR <orhancinar35@gmail.com>
2019-06-12 14:30:12 +0200

    add test

---
+++ XUnitTestProject/Arrays/ArraySwapTests.cs
@@ -0,0 +1,27 @@
+﻿using C_Sharp_Practice.Arrays;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace XUnitTestProject.Arrays
+{
+    public class ArraySwapTests
+    {
+
+
+        ArraySwap _arraySwap = new ArraySwap();
+
+        [Fact]
+        public void Test_ArrayMoveZeros()
+        {
+            var arr = new[] { 2, 1, 5, 6, 3 };
+            int n = arr.Length;
+            int k = 3;
+
+            var result = _arraySwap.GetMinSwap(arr, n, k);
+            var expected = 1;
+            Assert.Equal(expected, result);
+        }
+    }
+}
