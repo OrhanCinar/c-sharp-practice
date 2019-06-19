4c99e3c37f665a5c1d8fea1536e5f48748db7f16
Orhan CINAR <orhancinar35@gmail.com>
2019-06-19 21:46:29 +0200

    add array rearrange smallest

--- C-Sharp-Practice/Arrays/ArrayReArrange.cs
+++ C-Sharp-Practice/Arrays/ArrayReArrange.cs
@@ -177,5 +177,37 @@ namespace C_Sharp_Practice.Arrays
 
             return ans;
         }
+
+        public int[] ReArrangeSmallest(int[] arr, int n)
+        {
+            Array.Sort(arr);
+
+            int[] tempArr = new int[n];
+
+            int ArrIndex = 0;
+
+            for (int i = 0, j = n - 1; i <= n / 2 || j > n / 2; i++, j--)
+            {
+                if (ArrIndex < n)
+                {
+                    tempArr[ArrIndex] = arr[i];
+                    ArrIndex++;
+                }
+
+                if (ArrIndex < n)
+                {
+                    tempArr[ArrIndex] = arr[j];
+                    ArrIndex++;
+                }
+            }
+
+
+            for (int i = 0; i < n; i++)
+            {
+                arr[i] = tempArr[i];
+            }
+
+            return arr;
+        }
     }
 }
