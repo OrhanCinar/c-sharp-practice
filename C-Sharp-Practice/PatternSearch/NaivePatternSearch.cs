f93cc684286b9eaf0aeedbfe999a7ff9690a800c
Orhan CINAR <orhancinar35@gmail.com>
2020-02-15 02:43:55 +0100

    add naive pattern search

---
+++ C-Sharp-Practice/PatternSearch/NaivePatternSearch.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace C_Sharp_Practice.PatternSearch
+{
+    public class NaivePatternSearch
+    {
+        public List<int> NaiveSearch(string text, string pat)
+        {
+            int m = pat.Length;
+            int n = text.Length;
+            List<int> patternList = new List<int>();
+
+            for (int i = 0; i <= n-m; i++)
+            {
+                int j;
+
+                for (j = 0; j < m; j++)
+                {
+                    if (text[i+j] != pat[j])
+                    {
+                        break;
+                    }                   
+                }
+
+                if (j == m)
+                {
+                    patternList.Add(i);
+                }
+
+            }
+
+            return patternList;
+        }
+    }
+}
---
+++ XUnitTestProject/PatternSearch/NaivePatternSearchTest.cs
@@ -0,0 +1,24 @@
+﻿using C_Sharp_Practice.PatternSearch;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace XUnitTestProject.PatternSearch
+{
+    public class NaivePatternSearchTest
+    {
+        NaivePatternSearch naivePatternSearch = new NaivePatternSearch();
+
+        [Fact]
+        public void TestNaiveSearch()
+        {
+            string text = "AABAACAADAABAAABAA";
+            string pat = "AABA";
+            var actual = naivePatternSearch.NaiveSearch(text, pat);
+
+            Assert.Equal(3, actual.Count);
+        }
+
+    }
+}
