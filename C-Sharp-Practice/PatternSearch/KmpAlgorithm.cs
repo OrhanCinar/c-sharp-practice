08142c0004d23ff715ad86b4d68ceab5e832218b
Orhan CINAR <orhancinar35@gmail.com>
2020-02-18 15:59:17 +0100

    add kmp algo

---
+++ C-Sharp-Practice/PatternSearch/KmpAlgorithm.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace C_Sharp_Practice.PatternSearch
+{
+    public class KmpAlgorithm
+    {
+        public List<int> KmpAlgorithmSearch(string text, string pat)
+        {
+            int m = pat.Length;
+            int n = text.Length;
+            List<int> patternList = new List<int>();
+
+            int[] lps = new int[m];
+            int j = 0;
+
+            computeLPSArray(text, m, lps);
+            int i = 0;
+            while (i < n)
+            {
+
+                if (pat[j] == text[i])
+                {
+                    j++;
+                    i++;
+                }
+
+                if (j == m)
+                {
+                    patternList.Add(i - j);
+                    j = lps[j - 1];
+                }
+                else if (i < n && pat[j] != text[i])
+                {
+                    if (j != 0)
+                    {
+                        j = lps[j - 1];
+                    }
+                    else
+                    {
+                        i = i + 1;
+                    }
+                }
+            }
+
+            return patternList;
+        }
+
+        private void computeLPSArray(string text, int m, int[] lps)
+        {
+            int len = 0;
+            int i = 1;
+            lps[0] = 0;
+
+            while (i < m)
+            {
+                if (text[i] == text[len])
+                {
+                    len++;
+                    lps[i] = len;
+                    i++;
+                }
+                else
+                {
+                    if (len != 0)
+                    {
+                        len = lps[len - 1];
+                    }
+                    else
+                    {
+                        lps[i] = len;
+                        i++;
+                    }
+                }
+            }
+
+        }
+    }
+}
---
+++ XUnitTestProject/PatternSearch/KmpPatternSearchTest.cs
@@ -0,0 +1,24 @@
+﻿using C_Sharp_Practice.PatternSearch;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace XUnitTestProject.PatternSearch
+{
+    public class KmpPatternSearchTest
+    {
+        KmpAlgorithm kmpSearch = new KmpAlgorithm();
+
+        [Fact]
+        public void TestKMPSearch()
+        {
+            string text = "ABABDABACDABABCABAB";
+            string pat = "ABABCABAB";
+            var actual = kmpSearch.KmpAlgorithmSearch(text, pat);
+
+            Assert.Equal(1, actual.Count);
+        }
+
+    }
+}
