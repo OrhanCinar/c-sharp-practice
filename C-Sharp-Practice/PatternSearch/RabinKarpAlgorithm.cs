2cbd3af5726a625043e8693dce5c75dfc92378b4
Orhan CINAR <orhancinar35@gmail.com>
2020-02-19 14:44:49 +0100

    add rabin karp search

---
+++ C-Sharp-Practice/PatternSearch/RabinKarpAlgorithm.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace C_Sharp_Practice.PatternSearch
+{
+    public class RabinKarpAlgorithm
+    {
+        public List<int> Search(string pat, string txt, int q)
+        {
+            int m = pat.Length;
+            int n = txt.Length;
+            int i, j = 0;
+            int p = 0; // has value for pattern
+            int t = 0; // hach value for txt
+            int h = 1;
+            int d = 256;
+            List<int> patternList = new List<int>();
+
+            for (i = 0; i < m - 1; i++)
+            {
+                h = (h * d) % q;
+            }
+
+            for (i = 0; i < m; i++)
+            {
+                p = (d * p + pat[i]) % q;
+                t = (d * t + txt[i]) % q;
+            }
+
+
+            for (i = 0; i <= n - m; i++)
+            {
+                if (p == t)
+                {
+                    for (j = 0; j < m; j++)
+                    {
+                        if (txt[i + j] != pat[j])
+                            break;
+                    }
+
+                    if (j == m)
+                    {
+                        patternList.Add(i);
+                    }
+                }
+
+                if (i < n - m)
+                {
+                    t = (d * (t - txt[i] * h) + txt[i + m]) % q;
+
+                    if (t < 0)
+                    {
+                        t += q;
+                    }
+                }
+            }
+
+
+            return patternList;
+        }
+    }
+}
---
+++ XUnitTestProject/PatternSearch/RabinKarpAlgorithmTest.cs
@@ -0,0 +1,27 @@
+﻿using C_Sharp_Practice.PatternSearch;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace XUnitTestProject.PatternSearch
+{
+    public class RabinKarpAlgorithmTest
+    {
+        RabinKarpAlgorithm rabinKarp = new RabinKarpAlgorithm();
+
+        [Fact]
+        public void TestRabinKarpAlgorithm()
+        {
+            string text = "GEEKS FOR GEEKS";
+            string pat = "GEEK";
+            int q = 101;
+
+            var actual = rabinKarp.Search(pat,text,q);
+            //rabinKarp.search(pat,text, q);
+
+            Assert.Equal(2, actual.Count);
+        }
+
+    }
+}
