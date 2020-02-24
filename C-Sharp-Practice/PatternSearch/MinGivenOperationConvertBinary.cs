19e04371aaba0f72d3eba2b3d572b0f66b4b5855
Orhan CINAR <orhancinar35@gmail.com>
2020-02-24 16:03:34 +0100

    add Minimum given operations required to convert a given binary string to all 1’s

---
+++ C-Sharp-Practice/PatternSearch/MinGivenOperationConvertBinary.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace C_Sharp_Practice.PatternSearch
+{
+    public class MinGivenOperationConvertBinary
+    {
+        public int Convert(string str)
+        {
+            str = RemoveZerosFromFront(str);
+
+            int i = ChangeToOnes(str);
+            return i;
+        }
+
+        private string RemoveZerosFromFront(string str)
+        {
+            int i = 0;
+
+            while (i < str.Length && str[i] == '0')
+            {
+                i++;
+            }
+
+            string s;
+            if (i == str.Length)
+            {
+                s = "0";
+            }
+            else
+            {
+                s = str.Substring(i, str.Length - i);
+            }
+
+            return s;
+        }
+
+        private int ChangeToOnes(string str)
+        {
+            int i, l, ctr = 0;
+            l = str.Length;
+
+            for (i = l - 1; i >= 0; i--)
+            {
+                if (str[i] == '1')
+                {
+                    ctr++;
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            return l - ctr;
+        }
+
+    }
+}
---
+++ XUnitTestProject/PatternSearch/MinGivenOperationConvertBinaryTest.cs
@@ -0,0 +1,24 @@
+﻿using C_Sharp_Practice.PatternSearch;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace XUnitTestProject.PatternSearch
+{
+    public class MinGivenOperationConvertBinaryTest
+    {
+
+        MinGivenOperationConvertBinary minGivenOperation = new MinGivenOperationConvertBinary();
+
+        [Fact]
+        public void Test_MinGivenOperationConvertBinary()
+        {
+            string str = "10010111";
+            var actual = minGivenOperation.Convert(str);
+            var expected = 5;
+
+            Assert.Equal(expected, actual);
+        }
+    }
+}
