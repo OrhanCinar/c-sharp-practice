14795bfb66e570f04e694e1f85ec19564ebf4bec
Orhan CINAR <orhancinar35@gmail.com>
2020-02-26 15:52:33 +0100

    add Sum of the digits of square of the given number which has only 1’s as its digits

---
+++ C-Sharp-Practice/PatternSearch/SumDigitsSquare.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace C_Sharp_Practice.PatternSearch
+{
+    public class SumDigitsSquare
+    {
+        public int SquareDigitSum(string number)
+        {
+            int sum = 0;
+
+            int num = int.Parse(number);
+
+            int squareNum = num * num;
+
+            while(squareNum > 0)
+            {
+                sum += (squareNum % 10);
+                squareNum /= 10;
+            }
+
+
+            return sum;
+        }
+    }
+}
---
+++ XUnitTestProject/PatternSearch/SumDigitsSquareTest.cs
@@ -0,0 +1,23 @@
+﻿using C_Sharp_Practice.PatternSearch;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace XUnitTestProject.PatternSearch
+{
+    public class SumDigitsSquareTest
+    {
+        SumDigitsSquare sumDigitsSquare = new SumDigitsSquare();
+
+        [Fact]
+        public void TestSumDigitsSquare()
+        {
+            string s = "1111";
+            var expected = 16;
+            var actual = sumDigitsSquare.SquareDigitSum(s);
+
+            Assert.Equal(expected, actual);
+        }
+    }
+}
