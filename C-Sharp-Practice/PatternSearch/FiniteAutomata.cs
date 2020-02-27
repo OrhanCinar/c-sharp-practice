ac954d24c907f21facfc7be733e5446569916699
Orhan CINAR <orhancinar35@gmail.com>
2020-02-27 17:02:44 +0100

    add finite automata

---
+++ C-Sharp-Practice/PatternSearch/FiniteAutomata.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace C_Sharp_Practice.PatternSearch
+{
+    public class FiniteAutomata
+    {
+        static int NO_OF_CHARS = 256;
+        public List<int> Search(char[] pat, char[] txt)
+        {
+            int m = pat.Length;
+            int n = txt.Length;
+
+            List<int> patternList = new List<int>();
+
+            int[][] tf = ReturnRectangularIntArray(m + 1, NO_OF_CHARS);
+
+            computeTF(pat, m, tf);
+
+            int i, state = 0;
+
+            for (i = 0; i < n; i++)
+            {
+                state = tf[state][txt[i]];
+                if (state == m)
+                {
+                    patternList.Add(i - m + 1);
+                }
+            }
+
+
+            return patternList;
+        }
+
+        public static void computeTF(char[] pat,
+                                       int M, int[][] TF)
+        {
+            int state, x;
+            for (state = 0; state <= M; ++state)
+            {
+                for (x = 0; x < NO_OF_CHARS; ++x)
+                {
+                    TF[state][x] = getNextState(pat, M,
+                                                state, x);
+                }
+            }
+        }
+
+        public static int getNextState(char[] pat, int M,
+                                          int state, int x)
+        {
+
+            // If the character c is same as next  
+            // character in pattern,then simply  
+            // increment state  
+            if (state < M && (char)x == pat[state])
+            {
+                return state + 1;
+            }
+
+            // ns stores the result  
+            // which is next state  
+            int ns, i;
+
+            // ns finally contains the longest  
+            // prefix which is also suffix in  
+            // "pat[0..state-1]c"  
+
+            // Start from the largest possible   
+            // value and stop when you find a  
+            // prefix which is also suffix  
+            for (ns = state; ns > 0; ns--)
+            {
+                if (pat[ns - 1] == (char)x)
+                {
+                    for (i = 0; i < ns - 1; i++)
+                    {
+                        if (pat[i] != pat[state - ns + 1 + i])
+                        {
+                            break;
+                        }
+                    }
+                    if (i == ns - 1)
+                    {
+                        return ns;
+                    }
+                }
+            }
+
+            return 0;
+        }
+
+        public static int[][] ReturnRectangularIntArray(int size1,
+                                                              int size2)
+        {
+            int[][] newArray = new int[size1][];
+            for (int array1 = 0; array1 < size1; array1++)
+            {
+                newArray[array1] = new int[size2];
+            }
+
+            return newArray;
+        }
+
+    }
+}
---
+++ XUnitTestProject/PatternSearch/FiniteAutomataTest.cs
@@ -0,0 +1,29 @@
+﻿using C_Sharp_Practice.PatternSearch;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace XUnitTestProject.PatternSearch
+{
+    public class FiniteAutomataTest
+    {
+        FiniteAutomata finiteAutomata = new FiniteAutomata();
+
+        [Fact]
+        public void TestFiniteAutomata()
+        {
+            char[] pat = "AABAACAADAABAAABAA".ToCharArray();
+            char[] txt = "AABA".ToCharArray();
+
+           
+
+            var actual = finiteAutomata.Search(txt, pat);
+            var expected = 3;
+
+            Assert.Equal(expected, actual.Count);
+        }
+
+
+    }
+}
