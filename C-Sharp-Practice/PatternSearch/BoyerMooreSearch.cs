c6397464a55abf944f8920c35ffd7b59e9408d95
Orhan CINAR <orhancinar35@gmail.com>
2020-02-29 14:50:16 +0100

    add boyer moore search

---
+++ C-Sharp-Practice/PatternSearch/BoyerMooreSearch.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace C_Sharp_Practice.PatternSearch
+{
+    public class BoyerMooreSearch
+    {
+        static int NO_OF_CHARS = 256;
+
+        public List<int> Search(char[] txt, char[] pat)
+        {
+            var patternList = new List<int>();
+            int m = pat.Length;
+            int n = txt.Length;
+
+            int[] badChar = new int[NO_OF_CHARS];
+
+            badCharHeuristic(pat, m, badChar);
+
+            int s = 0;
+
+            while(s <= (n - m))
+            {
+                int j = m - 1;
+
+                while(j >= 0 && pat[j] == txt[s+j])
+                {
+                    j--;
+                }
+
+
+                if (j < 0)
+                {
+                    patternList.Add(s);
+
+                    s += (s+m<n) ? m-badChar[txt[s+m]] : 1;
+                }
+                else
+                {
+                    s += max(1, j - badChar[txt[s+j]]);
+                }
+
+            }
+
+            return patternList;
+
+        }
+
+        private int max(int a, int b)
+        {
+            return a > b ? a : b;
+        }
+
+        private void badCharHeuristic(char[] str, int size, int[] badChar)
+        {
+            int i;
+
+            for (i = 0; i < NO_OF_CHARS; i++)
+            {
+                badChar[i] = -1;
+            }
+
+            for (i = 0; i < size; i++)
+            {
+                badChar[(int)str[i]] = i;
+            }
+        }
+    }
+}
---
+++ XUnitTestProject/PatternSearch/BoyerMooreSearchTest.cs
@@ -0,0 +1,24 @@
+﻿using C_Sharp_Practice.PatternSearch;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace XUnitTestProject.PatternSearch
+{
+    public class BoyerMooreSearchTest
+    {
+        BoyerMooreSearch boyerMooreSearch = new BoyerMooreSearch();
+
+        [Fact]
+        public void TestBoyerMooreSearch()
+        {
+            char[] txt = "ABAAABCD".ToCharArray();
+            char[] pat = "ABC".ToCharArray();
+            var actual = boyerMooreSearch.Search(txt, pat);
+            var expected = 1;
+
+            Assert.Equal(expected, actual.Count);
+        }
+    }
+}
