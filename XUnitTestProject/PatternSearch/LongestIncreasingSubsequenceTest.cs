1bd4621a2fdc8c77a661f93bfbafae2319777b07
Orhan CINAR <orhancinar35@gmail.com>
2020-03-02 15:40:57 +0100

    add Longest Increasing Subsequence using Longest Common Subsequence Algorithm

--- XUnitTestProject/PatternSearch/LongestIncreasingSubsequenceTest.cs
+++ XUnitTestProject/PatternSearch/LongestIncreasingSubsequenceTest.cs
@@ -9,6 +9,7 @@ namespace XUnitTestProject.PatternSearch
     public class LongestIncreasingSubsequenceTest
     {
         LongestIncreasingSubsequence longestIncreasingSubsequence = new LongestIncreasingSubsequence();
+        LongestIncreasingSubsequenceLongestCommon longestIncreasingSubsequence2 = new LongestIncreasingSubsequenceLongestCommon();
 
         [Fact]
         public void TestLongestIncreasingSubsequence()
@@ -28,6 +29,24 @@ namespace XUnitTestProject.PatternSearch
             Assert.Equal(4, actual);
         }
 
-       
+        [Fact]
+        public void TestLongestIncreasingSubsequenceLongestCommon()
+        {
+
+            List<int> sequence = new List<int>
+            {
+                12,
+                34,
+                1,
+                5,
+                40,
+                80
+            };
+
+            var actual = longestIncreasingSubsequence2.Search(sequence);
+            Assert.Equal(4, actual);
+        }
+        
+
     }
 }
