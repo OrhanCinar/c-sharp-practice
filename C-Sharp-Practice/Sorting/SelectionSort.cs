d2467f9af717a5b2fc6c50de8b7de6164ced5bf3
Orhan CINAR <orhancinar35@gmail.com>
2020-03-03 16:16:42 +0100

    add selection sort

---
+++ C-Sharp-Practice/Sorting/SelectionSort.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace C_Sharp_Practice.Sorting
+{
+    public class SelectionSort
+    {
+        public int[] Sort(int[] arr)
+        {
+            int n = arr.Length;
+
+            for (int i = 0; i < n-1; i++)
+            {
+                int min_idx = i;
+
+                for (int j = i+1; j < n; j++)
+                {
+                    if (arr[j] < arr[min_idx])
+                    {
+                        min_idx = j;
+                    }
+                }
+
+                int temp = arr[min_idx];
+                arr[min_idx] = arr[i];
+                arr[i] = temp;
+            }
+
+            return arr;
+        }
+    }
+}
---
+++ XUnitTestProject/Sorting/SelectionSortTest.cs
@@ -0,0 +1,22 @@
+﻿using C_Sharp_Practice.Sorting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace XUnitTestProject.Sorting
+{
+    public class SelectionSortTest
+    {
+        SelectionSort selectionSort = new SelectionSort();
+
+        [Fact]
+        public void TestSelectionSort()
+        {
+            int[] arr = { 64, 25, 12, 22, 11 };
+            var actual = selectionSort.Sort(arr);
+            var expected = new int[] { 11, 12, 22, 25, 64 };
+            Assert.Equal(expected, actual);
+        }
+    }
+}
