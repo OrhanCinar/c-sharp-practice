f44c7ace2f0ad76595d1d99cb59898dc510f5916
Orhan CINAR <orhancinar35@gmail.com>
2020-03-04 15:09:21 +0100

    add bubble sort recursive

---
+++ C-Sharp-Practice/Sorting/BubbleSortRecursive.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace C_Sharp_Practice.Sorting
+{
+    public class BubbleSortRecursive
+    {
+        public int[] Sort(int[] arr, int n)
+        {
+
+            if (n == 1)
+            {
+                return arr;
+            }
+
+            for (int i = 0; i < n-1; i++)
+            {
+                if (arr[i] > arr[i + 1])
+                {
+                    int temp = arr[i];
+                    arr[i] = arr[i + 1];
+                    arr[i+1] = temp;
+                }
+            }
+
+            Sort(arr, n-1);
+
+            return arr;
+        }
+    }
+}
---
+++ XUnitTestProject/Sorting/BubbleSortRecursiveTest.cs
@@ -0,0 +1,21 @@
+using C_Sharp_Practice;
+using C_Sharp_Practice.Sorting;
+using Xunit;
+
+namespace XUnitTestProject
+{
+    public class BubbleSortRecursiveTest
+    {
+        BubbleSortRecursive bubbleSort = new BubbleSortRecursive();
+
+
+        [Fact]
+        public void TestBubbleSortRecursive()
+        {
+            int[] arr = { 64, 34, 25, 12, 22, 11, 90 };
+            var result = bubbleSort.Sort(arr, arr.Length);
+            var expected = new[] { 11, 12, 22, 25, 34, 64, 90 };
+            Assert.Equal(expected, result);
+        }
+    }
+}
