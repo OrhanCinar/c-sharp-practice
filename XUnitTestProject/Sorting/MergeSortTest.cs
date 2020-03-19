9efea8e6ae4b73fe03a74a26b4a88a5ff62875e6
Orhan CINAR <orhancinar35@gmail.com>
2020-03-19 16:46:42 +0100

    add merge sort test

--- XUnitTestProject/Sorting/MergeSortTest.cs
+++ XUnitTestProject/Sorting/MergeSortTest.cs
@@ -6,6 +6,7 @@ namespace XUnitTestProject.Sorting
     public class MergeSortTest
     {
         MergeSort mergeSort = new MergeSort();
+        MergeSortIterative mergeSortIterative = new MergeSortIterative();
 
         [Fact]
         public void Test_MergeSort()
@@ -13,6 +14,17 @@ namespace XUnitTestProject.Sorting
             var arr = new[] { 12, 11, 13, 5, 6, 7 };
 
             var result = mergeSort.Sort(arr, 0, arr.Length - 1);
+            var expected = new[] { 5, 6, 7, 11, 12, 13 };
+            Assert.Equal(expected, result);
+        }
+
+
+        [Fact]
+        public void Test_MergeSortIterative()
+        {
+            var arr = new[] { 12, 11, 13, 5, 6, 7 };
+
+            var result = mergeSortIterative.Sort(arr,arr.Length);
             var expected = new[] { 5, 6, 7, 11, 12, 13 };
             Assert.Equal(expected, result);
         }
