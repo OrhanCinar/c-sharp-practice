93ff83509b7315289f55ffb4cb44c086048545df
Orhan CINAR <orhancinar35@gmail.com>
2020-03-20 14:49:00 +0100

    add quick sort

---
+++ C-Sharp-Practice/Sorting/QuickSort.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace C_Sharp_Practice.Sorting
+{
+    public class QuickSort
+    {
+        public int[] Sort(int[] arr, int low , int high)
+        {
+            if (low< high)
+            {
+                int partionIndex = Partition(arr, low, high);
+                Sort(arr, low, partionIndex-1);
+                Sort(arr, partionIndex + 1, high);
+
+            }
+
+            return arr;
+        }
+
+        private int Partition(int[] arr, int low, int high)
+        {
+            int pivot = arr[high];
+
+            int i = low - 1;
+
+            for (int j = low; j < high; j++)
+            {
+                if (arr[j] < pivot)
+                {
+                    i++;
+
+                    int tmp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = tmp;
+                }
+            }
+
+
+
+            int tmp1 = arr[i+1];
+            arr[i+1] = arr[high];
+            arr[high] = tmp1;
+
+            return i+1;
+        }
+    }
+}
---
+++ XUnitTestProject/Sorting/QuickSortTest.cs
@@ -0,0 +1,23 @@
+﻿using C_Sharp_Practice.Sorting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace XUnitTestProject.Sorting
+{
+    public class QuickSortTest
+    {
+        QuickSort sort = new QuickSort();
+
+        [Fact]
+        public void TestQuickSort()
+        {
+            int[] arr = { 10, 7, 8, 9, 1, 5 };
+            int n = arr.Length;
+            var actual = sort.Sort(arr, 0, n - 1);
+            var expected = new int[] { 1, 5, 7, 8, 9, 10 };
+            Assert.Equal(expected, actual);
+        }
+    }
+}
