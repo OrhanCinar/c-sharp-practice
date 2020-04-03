d1f659ed6e42ddece10e1200c8cec79ce514bf21
Orhan CINAR <orhancinar35@gmail.com>
2020-04-03 14:59:14 +0200

    add heap sort

---
+++ XUnitTestProject/Sorting/HeapSortTest.cs
@@ -0,0 +1,23 @@
+﻿using C_Sharp_Practice.Sorting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace XUnitTestProject.Sorting
+{
+    public class HeapSortTest
+    {
+        HeapSort sort = new HeapSort();
+
+        [Fact]
+        public void TestHeapSort()
+        {
+            int[] arr = { 12, 11, 13, 5, 6, 7 };
+
+            var actual = sort.Sort(arr);
+            var expected = new int[] { 5, 6, 7, 11, 12, 13 };
+            Assert.Equal(expected, actual);
+        }
+    }
+}
