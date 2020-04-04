5dd2c3d9491f26dc031038b48dc0d9d78f140276
Orhan CINAR <orhancinar35@gmail.com>
2020-04-04 21:53:55 +0200

    add counting sort

---
+++ XUnitTestProject/Sorting/CountingSortTest.cs
@@ -0,0 +1,24 @@
+﻿using C_Sharp_Practice.Sorting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace XUnitTestProject.Sorting
+{
+    public class CountingSortTest
+    {
+
+        CountingSort sort = new CountingSort();
+
+        [Fact]
+        public void Test_CountingSort()
+        {
+            char[] arr = {'g', 'e', 'e', 'k', 's', 'f', 'o','r', 'g', 'e', 'e', 'k', 's'};
+            var actual = sort.Sort(arr);
+            char[] expected = { 'e', 'e','e', 'e', 'f', 'g', 'g', 'k', 'k', 'o', 'r', 's', 's' };
+
+            Assert.Equal(expected, actual);
+        }
+    }
+}
