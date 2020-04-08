e22acacaa0ddd8212829e59279603f584673c6b7
Orhan CINAR <orhancinar35@gmail.com>
2020-04-08 14:39:20 +0200

    add shell sort

---
+++ XUnitTestProject/Sorting/ShellSortTest.cs
@@ -0,0 +1,24 @@
+﻿using C_Sharp_Practice.Sorting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace XUnitTestProject.Sorting
+{
+    public class ShellSortTest
+    {
+
+        ShellSort sort = new ShellSort();
+
+        [Fact]
+        public void Test_ShellSort()
+        {
+            int[] arr = { 170, 45, 75, 90, 802, 24, 2, 66 };
+                    var actual = sort.Sort(arr);
+            int[] expected = { 2, 24, 45, 66, 75, 90, 170, 802 };
+
+            Assert.Equal(expected, actual);
+        }
+    }
+}
