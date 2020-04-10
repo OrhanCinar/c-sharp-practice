e3985796592374881ca84795a7a965928808b3d3
Orhan CINAR <orhancinar35@gmail.com>
2020-04-10 23:41:12 +0200

    add comb sort

---
+++ C-Sharp-Practice/Sorting/CombSort.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace C_Sharp_Practice.Sorting
+{
+    public class CombSort
+    {
+        public int[] Sort(int[] arr)
+        {
+            int n = arr.Length;
+            int gap = n;
+
+
+            bool swapped = true;
+
+
+            while (gap != 1 || swapped)
+            {
+                gap = getNextGap(gap);
+
+                swapped = false;
+
+                for (int i = 0; i < n - gap; i++)
+                {
+                    if (arr[i] > arr[i + gap])
+                    {
+                        int temp = arr[i];
+                        arr[i] = arr[i + gap];
+                        arr[i + gap] = temp;
+
+
+                        swapped = true;
+                    }
+                }
+
+            }
+
+
+            return arr;
+        }
+
+        private int getNextGap(int gap)
+        {
+            gap = (gap * 10) / 13;
+
+            return gap < 1 ? 1 : gap;
+        }
+    }
+}
---
+++ XUnitTestProject/Sorting/CombSortTest.cs
@@ -0,0 +1,24 @@
+﻿using C_Sharp_Practice.Sorting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace XUnitTestProject.Sorting
+{
+    public class CombSortTest
+    {
+
+        CombSort sort = new CombSort();
+
+        [Fact]
+        public void Test_CombSort()
+        {
+            int[] arr = { 170, 45, 75, 90, 802, 24, 2, 66 };
+                    var actual = sort.Sort(arr);
+            int[] expected = { 2, 24, 45, 66, 75, 90, 170, 802 };
+
+            Assert.Equal(expected, actual);
+        }
+    }
+}
