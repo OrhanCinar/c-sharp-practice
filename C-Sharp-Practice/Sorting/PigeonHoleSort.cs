c1fe6c82ac36bb70a92c5590f6c5d3ae0edb8681
Orhan CINAR <orhancinar35@gmail.com>
2020-04-13 16:14:18 +0200

    add pigeonhole sort

---
+++ C-Sharp-Practice/Sorting/PigeonHoleSort.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace C_Sharp_Practice.Sorting
+{
+    public class PigeonHoleSort
+    {
+        public int[] Sort(int[] arr, int n)
+        {
+
+            int min = arr[0];
+            int max = arr[0];
+            int range, i, j, index;
+
+
+            for (int a = 0; a < n; a++)
+            {
+                if (arr[a] > max)
+                {
+                    max = arr[a];
+                }
+
+                if (arr[a] < min)
+                {
+                    min = arr[a];
+                }
+            }
+
+
+            range = max - min + 1;
+            int[] phole = new int[range];
+
+            for (i = 0; i < n; i++)
+            {
+                phole[i] = 0;
+            }
+
+            for (i = 0; i < n; i++)
+            {
+                phole[arr[i] - min]++;
+            }
+
+            index = 0;
+
+            for (j = 0; j < range; j++)
+            {
+                while (phole[j]-- > 0)
+                {
+                    arr[index++] = j + min;
+                }
+            }
+
+
+
+            return arr;
+        }
+    }
+}
---
+++ XUnitTestProject/Sorting/PigeonHoleSortTest.cs
@@ -0,0 +1,23 @@
+﻿using C_Sharp_Practice.Sorting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace XUnitTestProject.Sorting
+{
+    public class PigeonHoleSortTest
+    {
+        PigeonHoleSort sort = new PigeonHoleSort();
+
+        [Fact]
+        public void TestPigeonHoleSort()
+        {
+            int[] arr = { 10, 7, 8, 9, 1, 5 };
+            int n = arr.Length;
+            var actual = sort.Sort(arr, n);
+            var expected = new int[] { 1, 5, 7, 8, 9, 10 };
+            Assert.Equal(expected, actual);
+        }
+    }
+}
