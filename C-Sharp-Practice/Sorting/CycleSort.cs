dbe3d8bd9fb2e28ef7d56a7336761f825d95b452
Orhan CINAR <orhancinar35@gmail.com>
2020-04-14 23:00:02 +0200

    add cycle sort

---
+++ C-Sharp-Practice/Sorting/CycleSort.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace C_Sharp_Practice.Sorting
+{
+    public class CycleSort
+    {
+        public int[] Sort(int[] arr, int n)
+        {
+
+            int writes = 0;
+
+            for (int cycle_start = 0; cycle_start < n-2; cycle_start++)
+            {
+                int item = arr[cycle_start];
+
+                int pos = cycle_start;
+
+                for (int i = cycle_start+1; i < n; i++)
+                {
+                    if (arr[i]< item)
+                    {
+                        pos++;
+                    }
+                }
+
+                if (pos == cycle_start)
+                {
+                    continue;
+                }
+
+                while(item == arr[pos])
+                {
+                    pos++;
+                }
+
+                if (pos != cycle_start)
+                {
+                    int temp = item;
+                    item = arr[pos];
+                    arr[pos] = temp;
+                    writes++;
+                }
+
+                while(pos != cycle_start)
+                {
+                    pos = cycle_start;
+
+
+                    for (int i = cycle_start+1; i < n; i++)
+                    {
+                        if (arr[i]< item)
+                        {
+                            pos++;
+                        }
+                    }
+
+                    while(item == arr[pos])
+                    {
+                        pos++;
+                    }
+
+                    if (item != cycle_start)
+                    {
+                        int temp = item;
+                        item = arr[pos];
+                        arr[pos] = temp;
+                        writes++;
+                    }
+
+                }
+
+            }
+
+            return arr;
+        }
+    }
+}
---
+++ XUnitTestProject/Sorting/CycleSortTest.cs
@@ -0,0 +1,23 @@
+﻿using C_Sharp_Practice.Sorting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace XUnitTestProject.Sorting
+{
+    public class CycleSortTest
+    {
+        CycleSort sort = new CycleSort();
+
+        [Fact]
+        public void TestCycleSort()
+        {
+            int[] arr = { 10, 7, 8, 9, 1, 5 };
+            int n = arr.Length;
+            var actual = sort.Sort(arr, n);
+            var expected = new int[] { 1, 5, 7, 8, 9, 10 };
+            Assert.Equal(expected, actual);
+        }
+    }
+}
