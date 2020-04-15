5bc4f7d51bd1cb3a3c328a128735e5d30c253a40
Orhan CINAR <orhancinar35@gmail.com>
2020-04-15 15:37:00 +0200

    add cocktail sort

---
+++ C-Sharp-Practice/Sorting/CocktailSort.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace C_Sharp_Practice.Sorting
+{
+    public class CocktailSort
+    {
+        public int[] Sort(int[] arr)
+        {
+            bool swapped = true;
+            int start = 0;
+            int end = arr.Length;
+
+
+            while (swapped)
+            {
+                swapped = false;
+
+                for (int i = start; i < end - 1; ++i)
+                {
+                    if (arr[i] > arr[i + 1])
+                    {
+                        int temp = arr[i];
+                        arr[i] = arr[i + 1];
+                        arr[i + 1] = temp;
+                        swapped = true;
+                    }
+                }
+
+
+                if (!swapped)
+                {
+                    break;
+                }
+
+                swapped = false;
+
+                end -= 1;
+
+                for (int i = end - 1; i >= start; i--)
+                {
+                    if (arr[i] > arr[i + 1])
+                    {
+                        int temp = arr[i];
+                        arr[i] = arr[i + 1];
+                        arr[i + 1] = temp;
+                        swapped = true;
+                    }
+                }
+
+                start += 1;
+
+            }
+
+            return arr;
+        }
+    }
+}
---
+++ XUnitTestProject/Sorting/CocktailSortTest.cs
@@ -0,0 +1,22 @@
+﻿using C_Sharp_Practice.Sorting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace XUnitTestProject.Sorting
+{
+    public class CocktailSortTest
+    {
+        CocktailSort sort = new CocktailSort();
+
+        [Fact]
+        public void TestCocktailSort()
+        {
+            int[] arr = { 10, 7, 8, 9, 1, 5 };
+            var actual = sort.Sort(arr);
+            var expected = new int[] { 1, 5, 7, 8, 9, 10 };
+            Assert.Equal(expected, actual);
+        }
+    }
+}
