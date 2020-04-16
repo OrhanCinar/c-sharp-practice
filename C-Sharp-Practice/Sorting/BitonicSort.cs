0b3b0c2bc54784a675fe1f98b7aee502ee851239
Orhan CINAR <orhancinar35@gmail.com>
2020-04-16 16:31:12 +0200

    add bitonic sort

---
+++ C-Sharp-Practice/Sorting/BitonicSort.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace C_Sharp_Practice.Sorting
+{
+    public class BitonicSort
+    {
+        public int[] Sort(int[] arr, int n, int up)
+        {
+
+            BitonicSorting(arr, 0, n, up);
+
+            return arr;
+        }
+
+        private void BitonicSorting(int[] arr, int low, int cnt, int dir)
+        {
+            if (cnt > 1)
+            {
+                int k = cnt / 2;
+
+                BitonicSorting(arr, low, k, 1);
+
+                BitonicSorting(arr, low + k, k, 0);
+
+                Merge(arr, low, cnt, dir);
+
+            }
+        }
+
+        private void Merge(int[] arr, int low, int cnt, int dir)
+        {
+            if (cnt > 1)
+            {
+                int k = cnt / 2;
+                for (int i = low; i < low + k; i++)
+                {
+                    CompareAndSwap(arr, i, i + k, dir);                  
+                }
+                BitonicSorting(arr, low, k, dir);
+                BitonicSorting(arr, low + k, k, dir);
+            }
+        }
+
+        private void CompareAndSwap(int[] arr, int i, int j, int dir)
+        {
+            int k;
+            if (arr[i] > arr[j])
+            {
+                k = 1;
+            }
+            else
+            {
+                k = 0;
+            }
+
+            if (dir == k)
+            {
+                Swap(ref arr[i], ref arr[j]);
+            }
+
+        }
+
+        private void Swap(ref int v1, ref int v2)
+        {
+            int temp;
+
+            temp = v1;
+            v1 = v2;
+            v2 = temp;
+
+        }
+    }
+}
---
+++ XUnitTestProject/Sorting/BitonicSortTest.cs
@@ -0,0 +1,24 @@
+﻿using C_Sharp_Practice.Sorting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace XUnitTestProject.Sorting
+{
+    public class BitonicSortTest
+    {
+        BitonicSort sort = new BitonicSort();
+
+        [Fact]
+        public void TestBitonicSort()
+        {
+            int[] arr = { 3, 7, 4, 8, 6, 2, 1, 5 };
+            int n = arr.Length;
+            int up = 1;
+            var actual = sort.Sort(arr, n, up);
+            var expected = new int[] { 1, 2, 3, 4, 5, 6, 7, 8 };
+            Assert.Equal(expected, actual);
+        }
+    }
+}
