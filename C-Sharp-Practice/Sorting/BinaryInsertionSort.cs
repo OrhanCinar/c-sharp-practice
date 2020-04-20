e1ef9e060c7f125d1b490ad9f9ce96cad8a53d76
Orhan CINAR <orhancinar35@gmail.com>
2020-04-20 17:23:46 +0200

    add binary insertion sort

---
+++ C-Sharp-Practice/Sorting/BinaryInsertionSort.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace C_Sharp_Practice.Sorting
+{
+    public class BinaryInsertionSort
+    {
+        public int[] Sort(int[] arr)
+        {
+            for (int i = 1; i < arr.Length; i++)
+            {
+                int x = arr[i];
+                int j = Math.Abs(Array.BinarySearch(arr, 0, i, x) + 1);
+
+                Array.Copy(arr, j, arr, j + 1, i - j);
+                arr[j] = x;
+            }
+
+            return arr;
+        }
+    }
+}
---
+++ XUnitTestProject/Sorting/BinaryInsertionSortTest.cs
@@ -0,0 +1,23 @@
+﻿using C_Sharp_Practice.Sorting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace XUnitTestProject.Sorting
+{
+    public class BinaryInsertionSortTest
+    {
+        BinaryInsertionSort sort = new BinaryInsertionSort();
+
+        [Fact]
+        public void TestBinaryInsertionSort()
+        {
+            int[] arr = { 3, 7, 4, 8, 6, 2, 1, 5 };
+    
+            var actual = sort.Sort(arr);
+            var expected = new int[] { 1, 2, 3, 4, 5, 6, 7, 8 };
+            Assert.Equal(expected, actual);
+        }
+    }
+}
