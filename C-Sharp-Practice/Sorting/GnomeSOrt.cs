2b4323d23c263e6f1a8a1735e6884218374614e3
Orhan CINAR <orhancinar35@gmail.com>
2020-04-22 22:13:43 +0200

    add gnome sort

---
+++ C-Sharp-Practice/Sorting/GnomeSOrt.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace C_Sharp_Practice.Sorting
+{
+    public class GnomeSort
+    {
+        public int[]Sort(int[] arr, int n)
+        {
+            int index = 0;
+
+            while (index<n)
+            {
+                if(index == 0)
+                {
+                    index++;
+                }
+
+                if (arr[index] >= arr[index - 1])
+                {
+                    index++;
+                }else
+                {
+                    int temp = 0;
+                    temp = arr[index];
+                    arr[index] = arr[index - 1];
+                    arr[index - 1] = temp;
+                    index--;
+                }
+            }
+
+            return arr;
+        }
+    }
+}
---
+++ XUnitTestProject/Sorting/GnomeSortTest.cs
@@ -0,0 +1,24 @@
+﻿using C_Sharp_Practice.Sorting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace XUnitTestProject.Sorting
+{
+    public class GnomeSortTest
+    {
+        GnomeSort sort = new GnomeSort();
+
+        [Fact]
+        public void TestGnomeSort()
+        {
+            int[] arr = { 3, 7, 4, 8, 6, 2, 1, 5 };
+            int n = arr.Length;
+      
+            var actual = sort.Sort(arr, n);
+            var expected = new int[] { 1, 2, 3, 4, 5, 6, 7, 8 };
+            Assert.Equal(expected, actual);
+        }
+    }
+}
