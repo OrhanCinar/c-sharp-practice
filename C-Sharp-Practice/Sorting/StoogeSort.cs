79e2c0859312f748090587da881a6a6be8d5ff8e
Orhan CINAR <orhancinar35@gmail.com>
2020-04-23 17:53:59 +0200

    add stoogesort

---
+++ C-Sharp-Practice/Sorting/StoogeSort.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace C_Sharp_Practice.Sorting
+{
+    public class StoogeSort
+    {
+        public int[] Sort(int[] arr, int l, int h)
+        {
+
+            if (l >= h)
+            {
+                return arr;
+            }
+
+            if (arr[l] > arr[h])
+            {
+                int t = arr[l];
+                arr[l] = arr[h];
+                arr[h] = t;
+            }
+
+
+            if (h - l + 1 > 2)
+            {
+                int t = (h - l + 1) / 3;
+
+                Sort(arr, l, h - t);
+                Sort(arr, l + t, h);
+                Sort(arr, l, h - t);
+            }
+
+            return arr;
+        }
+    }
+}
---
+++ XUnitTestProject/Sorting/StoogeSortTest.cs
@@ -0,0 +1,23 @@
+﻿using C_Sharp_Practice.Sorting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace XUnitTestProject.Sorting
+{
+    public class StoogeSortTest
+    {
+        StoogeSort sort = new StoogeSort();
+
+        [Fact]
+        public void TestStoogeSort()
+        {
+            int[] arr = { 3, 7, 4, 8, 6, 2, 1, 5 };
+            int n = arr.Length;
+            var actual = sort.Sort(arr, 0, n - 1);
+            var expected = new int[] { 1, 2, 3, 4, 5, 6, 7, 8 };
+            Assert.Equal(expected, actual);
+        }
+    }
+}
