2dc40f235ee790aaf589109f1cc8af9674ad510d
Orhan CINAR <orhancinar35@gmail.com>
2020-04-26 17:04:47 +0200

    add OddEvenBrickSort

---
+++ C-Sharp-Practice/Sorting/OddEvenBrickSort.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace C_Sharp_Practice.Sorting
+{
+    public class OddEvenBrickSort
+    {
+        public int[] Sort(int[] arr, int n)
+        {
+
+            bool isSorted = false;
+
+            while (!isSorted)
+            {
+                isSorted = true;
+                int temp = 0;
+
+
+                for (int i = 1; i <= n - 2; i += 2)
+                {
+                    if (arr[i] > arr[i + 1])
+                    {
+                        temp = arr[i];
+                        arr[i] = arr[i + 1];
+                        arr[i + 1] = temp;
+                        isSorted = false;
+
+                    }
+                }
+
+                for (int i = 0; i <= n - 2; i += 2)
+                {
+                    if (arr[i] > arr[i + 1])
+                    {
+                        temp = arr[i];
+                        arr[i] = arr[i + 1];
+                        arr[i + 1] = temp;
+                        isSorted = false;
+
+                    }
+                }
+
+            }
+
+            return arr;
+        }
+    }
+}
