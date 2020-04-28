8b708d04f18f57fc4dcb21090458fd96529a3b77
Orhan CINAR <orhancinar35@gmail.com>
2020-04-28 23:46:38 +0200

    add quick sort single linked list

---
+++ C-Sharp-Practice/Sorting/QuickSortSingleLinkedList.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace C_Sharp_Practice.Sorting
+{
+    public class QuickSortSingleLinkedList
+    {
+        QuickNode PartitionLast(QuickNode start, QuickNode end)
+        {
+            if (start == end || start == null || end == null)
+            {
+                return start;
+            }
+
+            QuickNode pivot_prev = start;
+            QuickNode curr = start;
+            int pivot = end.data;
+
+            int temp;
+
+
+            while (start != end)
+            {
+                if (start.data < pivot)
+                {
+                    pivot_prev = curr;
+                    temp = curr.data;
+                    curr.data = start.data;
+                    start.data = temp;
+                    curr = curr.next;
+                }
+
+                start = start.next;
+            }
+
+
+            temp = curr.data;
+            curr.data = pivot;
+            end.data = temp;
+
+            return pivot_prev;
+
+        }
+
+
+        public QuickNode Sort(QuickNode start, QuickNode end)
+        {
+
+            if (start == end)
+                return start;
+
+            QuickNode pivot_prev = PartitionLast(start, end);
+            Sort(start, pivot_prev);
+
+
+            if (pivot_prev != null && pivot_prev == start)
+            {
+                Sort(pivot_prev.next, end);
+            }
+            else if (pivot_prev != null && pivot_prev.next != null)
+            {
+                Sort(pivot_prev.next.next, end);
+            }
+
+            return start;
+        }
+
+
+        public QuickNode head;
+        public void addNode(int data)
+        {
+            if (head == null)
+            {
+                head = new QuickNode(data);
+                return;
+            }
+
+            QuickNode curr = head;
+
+            while (curr.next != null)
+            {
+                curr = curr.next;
+            }
+
+            QuickNode newNode = new QuickNode(data);
+            curr.next = newNode;
+
+        }
+
+
+
+
+        public class QuickNode
+        {
+            public int data;
+            public QuickNode next;
+
+            public QuickNode(int d)
+            {
+                data = d;
+                next = null;
+            }
+        }
+
+    }
+
+}
---
+++ XUnitTestProject/Sorting/QuickSortSingleLinkedListTest.cs
@@ -0,0 +1,48 @@
+﻿using C_Sharp_Practice.Sorting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using static C_Sharp_Practice.Sorting.QuickSortSingleLinkedList;
+
+namespace XUnitTestProject.Sorting
+{
+    public class QuickSortSingleLinkedListTest
+    {
+        QuickSortSingleLinkedList sort = new QuickSortSingleLinkedList();
+
+        [Fact]
+        public void TestQuickSortSingleLinkedList()
+        {
+            sort.addNode(30);
+            sort.addNode(3);
+            sort.addNode(4);
+            sort.addNode(20);
+            sort.addNode(5);
+            var n = sort.head;
+            while (n.next != null)
+            {
+                n = n.next;
+            }
+
+            var actual = sort.Sort(sort.head, n);
+            var testArray = new int[] { 3, 4, 5, 20, 30 };
+
+            int counter = 0;
+            bool isSorted = true;
+
+            n = sort.head;
+            while (n.next != null)
+            {
+                if (testArray[counter++] != n.data)
+                {
+                    isSorted = false;
+                }
+
+                n = n.next;   
+            }
+
+            Assert.True(isSorted);
+        }
+    }
+}
