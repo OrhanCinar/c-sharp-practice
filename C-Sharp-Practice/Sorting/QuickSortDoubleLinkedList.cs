165d370f2a9c10e8a9155ad4e44978820e8990e0
Orhan CINAR <orhancinar35@gmail.com>
2020-05-01 18:38:17 +0200

    add quick sort double linkeds

---
+++ C-Sharp-Practice/Sorting/QuickSortDoubleLinkedList.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace C_Sharp_Practice.Sorting
+{
+    public class QuickSortDoubleLinkedList
+    {
+        public NodeQs head;
+        public void Sort(NodeQs node)
+        {
+            NodeQs head = LastNode(node);
+            QuickSortSub(node, head);
+        }
+
+        public void Push(int data)
+        {
+            NodeQs newNode = new NodeQs(data);
+
+            if (head == null)
+            {
+                head = newNode;
+                return;
+            }
+
+            newNode.next = head;
+            head.prev = newNode;
+            newNode.prev = null;
+            head = newNode;
+        }
+
+        NodeQs LastNode(NodeQs node)
+        {
+            while (node.next != null)
+            {
+                node = node.next;
+            }
+
+            return node;
+        }
+
+
+        NodeQs Partition(NodeQs l, NodeQs h)
+        {
+            int x = h.data;
+
+            NodeQs i = l.prev;
+            int temp;
+
+            for (NodeQs j = l; j != h; j = j.next)
+            {
+                if (j.data <= x)
+                {
+                    i = i == null ? l : i.next;
+                    temp = i.data;
+                    i.data = j.data;
+                    j.data = temp;
+                }
+            }
+
+            i = i == null ? l : i.next;
+            temp = i.data;
+            i.data = h.data;
+            h.data = temp;
+
+            return i;
+        }
+
+        void QuickSortSub(NodeQs l, NodeQs h)
+        {
+            if (h != null && l != h && l != h.next)
+            {
+                NodeQs temp = Partition(l, h);
+                QuickSortSub(l, temp.prev);
+                QuickSortSub(temp.next, h);
+            }
+        }
+    }
+
+    public class NodeQs
+    {
+        public int data;
+        public NodeQs next;
+        public NodeQs prev;
+
+        public NodeQs(int d)
+        {
+            this.data = d;
+            next = null;
+            prev = null;
+        }
+    }
+}
---
+++ XUnitTestProject/Sorting/QuickSortDoubleLinkedListTest.cs
@@ -0,0 +1,43 @@
+﻿using C_Sharp_Practice.Sorting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using static C_Sharp_Practice.Sorting.QuickSortSingleLinkedList;
+
+namespace XUnitTestProject.Sorting
+{
+    public class QuickSortDoubleLinkedListTest
+    {
+        QuickSortDoubleLinkedList sort = new QuickSortDoubleLinkedList();
+
+        [Fact]
+        public void TestQuickSortDoubleLinkedList()
+        {
+            sort.Push(30);
+            sort.Push(3);
+            sort.Push(4);
+            sort.Push(20);
+            sort.Push(5);
+            
+            sort.Sort(sort.head);
+            var testArray = new int[] { 3, 4, 5, 20, 30 };
+
+            int counter = 0;
+            bool isSorted = true;
+
+         
+            while (sort.head.next != null)
+            {
+                if (testArray[counter++] != sort.head.data)
+                {
+                    isSorted = false;
+                }
+
+                sort.head = sort.head.next;   
+            }
+
+            Assert.True(isSorted);
+        }
+    }
+}
