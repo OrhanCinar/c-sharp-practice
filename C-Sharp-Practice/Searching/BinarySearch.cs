b3fa5f5129f66a9e53a5d7b882b808fb29d9b402
Orhan CINAR <orhancinar35@gmail.com>
2020-05-04 16:10:27 +0200

    add binary search

---
+++ C-Sharp-Practice/Searching/BinarySearch.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace C_Sharp_Practice.Searching
+{
+    public class BinarySearch
+    {
+        public int Search(int[] arr, int l, int r, int x)
+        {
+
+            if (r >= l)
+            {
+                int mid = l + (r - l) / 2;
+
+                if (arr[mid] == x)
+                {
+                    return mid;
+                }
+
+                if (arr[mid] > x)
+                {
+                    return Search(arr, l, mid - 1, x);
+                }
+
+                return Search(arr, mid + 1, r, x);
+
+            }
+
+            return -1;
+        }
+    }
+}
---
+++ XUnitTestProject/LinearSearch/BinarySearchTest.cs
@@ -0,0 +1,25 @@
+﻿using C_Sharp_Practice.Searching;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace XUnitTestProject.Searching
+{
+    public class BinarySearchTest
+    {
+        BinarySearch search = new BinarySearch();
+
+        [Fact]
+        public void TestBinarySearch()
+        {
+            int[] arr = { 2, 3, 4, 10, 40,6 };
+            int x = 10;
+            int n = arr.Length;
+
+            var actual = search.Search(arr, 0, n - 1, x);
+            var expected = 3;
+            Assert.Equal(expected, actual);
+        }
+    }
+}
