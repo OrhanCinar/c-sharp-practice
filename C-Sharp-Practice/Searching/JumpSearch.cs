879b142d4f906996bd76e317537379fa26c7c8f1
Orhan CINAR <orhancinar35@gmail.com>
2020-05-06 23:22:21 +0200

    add jump search

---
+++ C-Sharp-Practice/Searching/JumpSearch.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace C_Sharp_Practice.Searching
+{
+    public class JumpSearch
+    {
+        public int Search(int[] arr, int x)
+        {
+            int n = arr.Length;
+
+            int step = (int)Math.Floor(Math.Sqrt(n));
+
+            int prev = 0;
+
+            while (arr[Math.Min(step, n) - 1] < x)
+            {
+                prev = step;
+                step += (int)Math.Floor(Math.Sqrt(n));
+                if (prev >= n)
+                {
+                    return -1;
+                }
+            }
+
+            while (arr[prev] < x)
+            {
+                prev++;
+
+                if (prev == Math.Min(step, n))
+                {
+                    return -1;
+                }
+            }
+
+            if (arr[prev] == x)
+            {
+                return prev;
+            }
+
+            return -1;
+        }
+    }
+}
---
+++ XUnitTestProject/Searching/JumpSearchTest.cs
@@ -0,0 +1,29 @@
+﻿using C_Sharp_Practice.Searching;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+
+namespace XUnitTestProject.Searching
+{
+    public class JumpSearchTest
+    {
+        JumpSearch search = new JumpSearch();
+
+        [Fact]
+        public void TestJumpSearch()
+        {
+            int[] arr = {0, 1, 1, 2, 3, 5, 8, 13, 21,
+                    34, 55, 89, 144, 233, 377, 610 };
+
+            arr = arr.OrderBy(c => c).ToArray();
+            int x = 55;
+
+
+            var actual = search.Search(arr, x);
+            var expected = 10;
+            Assert.Equal(expected, actual);
+        }
+    }
+}
