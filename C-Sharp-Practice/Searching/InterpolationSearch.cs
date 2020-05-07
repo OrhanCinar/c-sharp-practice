b9c1a10cd8fbf34b680dd8037a918fb2f52f367a
Orhan CINAR <orhancinar35@gmail.com>
2020-05-07 17:31:26 +0200

    add interpolation search

---
+++ C-Sharp-Practice/Searching/InterpolationSearch.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace C_Sharp_Practice.Searching
+{
+    public class InterpolationSearch
+    {
+        public int Search(int[]arr, int x)
+        {
+            int lo = 0;
+            int hi = arr.Length - 1;
+
+
+            while(lo <= hi && x >= arr[lo] && x <= arr[hi])
+            {
+                if (lo == hi)
+                {
+                    if (arr[lo] == x)
+                    {
+                        return lo;
+                    }
+
+                    return -1;
+                }
+
+                int pos = lo + ((hi - lo) / (arr[hi] - arr[lo])) * (x - arr[lo]);
+
+                if (arr[pos] == x)
+                {
+                    return pos;
+                }
+
+                if (arr[pos] < x)
+                {
+                    lo = pos +1;
+                }else
+                {
+                    hi = pos - 1;
+                }
+            }
+
+            return -1;
+        }
+    }
+}
---
+++ XUnitTestProject/Searching/InterpolationTest.cs
@@ -0,0 +1,28 @@
+﻿using C_Sharp_Practice.Searching;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+
+namespace XUnitTestProject.Searching
+{
+    public class InterpolationSearchTest
+    {
+        InterpolationSearch search = new InterpolationSearch();
+
+        [Fact]
+        public void TestInterpolationSearch()
+        {
+            int[] arr = {10, 12, 13, 16, 18,19, 20, 21, 22, 23,24, 33, 35, 42, 47 };
+
+            arr = arr.OrderBy(c => c).ToArray();
+            int x = 18;
+
+
+            var actual = search.Search(arr, x);
+            var expected = 4;
+            Assert.Equal(expected, actual);
+        }
+    }
+}
