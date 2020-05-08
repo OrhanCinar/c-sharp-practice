14a54fc2e3e872eeb48adcad3a0375bd025d77de
Orhan CINAR <orhancinar35@gmail.com>
2020-05-08 16:35:05 +0200

    add exponential search

---
+++ C-Sharp-Practice/Searching/ExponentialSearch.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace C_Sharp_Practice.Searching
+{
+    public class ExponentialSearch
+    {
+        public int Search(int[] arr, int n, int x)
+        {
+            if (arr[0] == x)
+            {
+                return 0;
+            }
+
+
+            int i = 1;
+
+            while (i < n && arr[i] <= x)
+            {
+                i *= 2;
+            }
+
+
+
+            return BinarySearch(arr, i / 2, Math.Min(i, n), x);
+        }
+
+
+        private int BinarySearch(int[] arr, int l, int r, int x)
+        {
+            if (r >= l)
+            {
+                int mid = l + (r - l) / 2;
+
+                if (arr[mid] == x)
+                {
+                    return mid;
+                }
+
+                if (arr[mid] > x)
+                {
+                    return BinarySearch(arr, l, mid - 1, x);
+                }
+
+                return BinarySearch(arr, mid + 1, r, x);
+
+            }
+
+            return -1;
+        }
+    }
+}
---
+++ XUnitTestProject/Searching/ExponentialSearchTest.cs
@@ -0,0 +1,28 @@
+﻿using C_Sharp_Practice.Searching;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+
+namespace XUnitTestProject.Searching
+{
+    public class ExponentialSearchTest
+    {
+        ExponentialSearch search = new ExponentialSearch();
+
+        [Fact]
+        public void TestExponentialSearch()
+        {
+            int[] arr = { 10, 12, 13, 16, 18, 19, 20, 21, 22, 23, 24, 33, 35, 42, 47 };
+
+            arr = arr.OrderBy(c => c).ToArray();
+            int x = 18;
+            int n = arr.Length;
+
+            var actual = search.Search(arr, n, x);
+            var expected = 4;
+            Assert.Equal(expected, actual);
+        }
+    }
+}
