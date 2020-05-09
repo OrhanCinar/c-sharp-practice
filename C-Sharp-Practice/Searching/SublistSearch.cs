80721f0655407a116a38d095215a0d4ad3fc1ea4
Orhan CINAR <orhancinar35@gmail.com>
2020-05-09 15:03:20 +0200

    add sublist search

---
+++ C-Sharp-Practice/Searching/SublistSearch.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace C_Sharp_Practice.Searching
+{
+    public class SublistSearch
+    {
+        public bool Search(SSNode first, SSNode second)
+        {
+            SSNode ptr1 = first;
+            SSNode ptr2 = second;
+
+            if (first == null && second == null)
+            {
+                return true;
+            }
+
+            if (first == null || (first != null && second == null))
+            {
+                return false;
+            }
+
+            while (second != null)
+            {
+                ptr2 = second;
+
+                while (ptr1 != null)
+                {
+                    if (ptr2 == null)
+                    {
+                        return false;
+                    }
+                    else if (ptr1.data == ptr2.data)
+                    {
+                        ptr1 = ptr1.next;
+                        ptr2 = ptr2.next;
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+
+                if (ptr1 == null)
+                {
+                    return true;
+                }
+
+                ptr1 = first;
+
+                second = second.next;
+
+            }
+
+            return false;
+        }
+
+        public SSNode newNode(int key)
+        {
+            SSNode temp = new SSNode
+            {
+                data = key,
+                next = null
+            };
+            return temp;
+        }
+    }
+
+    public class SSNode
+    {
+        public int data;
+        public SSNode next;
+    }
+}
---
+++ XUnitTestProject/Searching/SublistSearchTest.cs
@@ -0,0 +1,33 @@
+﻿using C_Sharp_Practice.Searching;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace XUnitTestProject.Searching
+{
+    public class SublistSearchTest
+    {
+        SublistSearch search = new SublistSearch();
+
+        [Fact]
+        public void TestSublistSearch()
+        {
+            SSNode a = search.newNode(1);
+            a.next = search.newNode(2);
+            a.next.next = search.newNode(3);
+            a.next.next.next = search.newNode(4);
+
+            SSNode b = search.newNode(1);
+            b.next = search.newNode(2);
+            b.next.next = search.newNode(1);
+            b.next.next.next = search.newNode(2);
+            b.next.next.next.next = search.newNode(3);
+            b.next.next.next.next.next = search.newNode(4);
+
+            var actual = search.Search(a,b);
+            var expected = true;
+            Assert.Equal(expected, actual);
+        }
+    }
+}
