8a22fc3ff8a4a40cc849d388117e32ec26a16c5c
Orhan CINAR <orhancinar35@gmail.com>
2020-05-11 14:56:19 +0200

    add fibonacci search

---
+++ C-Sharp-Practice/Searching/FibonacciSearch.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace C_Sharp_Practice.Searching
+{
+    public class FibonacciSearch
+    {
+        public int Search(int[] arr, int x, int n)
+        {
+            int fibMm2 = 0; //m-2
+            int fibMm1 = 1; //m-1
+
+            int fibM = fibMm2 + fibMm1; // m'th
+
+            while (fibM < n)
+            {
+                fibMm2 = fibMm1;
+                fibMm1 = fibM;
+                fibM = fibMm2 + fibMm1;
+            }
+
+            int offset = -1;
+
+            while (fibM > 1)
+            {
+                int i = Math.Min(offset + fibMm2, n - 1);
+
+                if (arr[i] < x)
+                {
+                    fibM = fibMm1;
+                    fibMm1 = fibMm2;
+                    fibMm2 = fibM - fibMm1;
+                    offset = i;
+                }
+                else if (arr[i] > x)
+                {
+                    fibM = fibMm2;
+                    fibMm1 = fibMm1 - fibMm2;
+                    fibMm2 = fibM - fibMm1;
+                }
+                else
+                {
+                    return i;
+                }
+            }
+            if (fibMm1 == 1 && arr[offset + 1] == x)
+            {
+                return offset + 1;
+            }
+
+
+            return 1;
+        }
+    }
+}
---
+++ XUnitTestProject/Searching/FibonacciSearchTest.cs
@@ -0,0 +1,26 @@
+﻿using C_Sharp_Practice.Searching;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace XUnitTestProject.Searching
+{
+    public class FibonacciSearchTest
+    {
+        FibonacciSearch search = new FibonacciSearch();
+
+        [Fact]
+        public void TestFibonacciSearch()
+        {
+            int[] arr = { 10, 22, 35, 40, 45, 50,
+                    80, 82, 85, 90, 100 };
+            int x = 85;
+            int n = 11;
+
+            var actual = search.Search(arr, x, n);
+            var expected = 8;
+            Assert.Equal(expected, actual);
+        }
+    }
+}
