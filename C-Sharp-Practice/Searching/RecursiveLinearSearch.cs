53c5604194ece69e637e4d8049d96b79533cf51a
Orhan CINAR <orhancinar35@gmail.com>
2020-05-12 18:20:40 +0200

    add recursive search

---
+++ C-Sharp-Practice/Searching/RecursiveLinearSearch.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace C_Sharp_Practice.Searching
+{
+    public class RecursiveLinearSearch
+    {
+        public int Search(int[] arr, int l, int r , int x)
+        {
+            if (r < l)
+            {
+                return -1;
+            }
+
+            if (arr[l] == x)
+            {
+                return l;
+            }
+
+            if (arr[r] == x)
+            {
+                return r;
+            }
+
+            return Search(arr, l+1, r-1, x);
+        }
+    }
+}
