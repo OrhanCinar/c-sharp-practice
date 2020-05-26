a56bc767a8967bdc6c72977d9913eb1792302175
Orhan CINAR <orhancinar35@gmail.com>
2020-05-26 16:10:36 +0200

    add stack infix to postfix implementation

---
+++ C-Sharp-Practice/DataStructures/StackInfixToPostfix.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace C_Sharp_Practice.DataStructures
+{
+    public class StackInfixToPostfix
+    {
+        int Prec(char ch)
+        {
+            switch (ch)
+            {
+                case '+':
+                case '-':
+                    return 1;
+
+                case '*':
+                case '/':
+                    return 2;
+                case '^':
+                    return 3;
+                default:
+                    return -1;
+
+            }
+        }
+
+        public string InfixToPostfix(string exp)
+        {
+            string result = "";
+
+
+            Stack<char> stack = new Stack<char>();
+
+            for (int i = 0; i < exp.Length; ++i)
+            {
+                char c = exp[i];
+
+                if (char.IsLetterOrDigit(c))
+                {
+                    result += c;
+                }
+                else if (c == '(')
+                {
+                    stack.Push(c);
+                }
+                else if (c == ')')
+                {
+                    while (stack.Count > 0 && stack.Peek() != '(')
+                    {
+                        result += stack.Pop();
+                    }
+
+                    if (stack.Count > 0 && stack.Peek() != '(')
+                    {
+                        return "Invalid Expression";
+                    }
+                    else
+                    {
+                        stack.Pop();
+                    }
+                }
+                else
+                {
+                    while (stack.Count > 0 && Prec(c) <= Prec(stack.Peek()))
+                    {
+                        result += stack.Pop();
+                    }
+                    stack.Push(c);
+                }
+
+
+            }
+            while (stack.Count > 0)
+            {
+                result += stack.Pop();
+            }
+            return result;
+        }
+    }
+}
---
+++ XUnitTestProject/DataStructures/StackInfixToPostfixTest.cs
@@ -0,0 +1,26 @@
+﻿using C_Sharp_Practice.DataStructures;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace XUnitTestProject.DataStructures
+{
+    public class StackInfixToPostfixTest
+    {
+
+        StackInfixToPostfix stackInfixToPostfix = new StackInfixToPostfix();
+
+        [Fact]
+
+        public void TestStackInfixToPostfixT()
+        {
+            string exp = "a+b*(c^d-e)^(f+g*h)-i";
+
+            var actual = stackInfixToPostfix.InfixToPostfix(exp);
+            var expected = "abcd^e-fgh*+^*+i-";
+            Assert.Equal(expected, actual);
+
+        }
+    }
+}
