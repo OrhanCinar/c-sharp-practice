ec0d4a74190b69a1d170c1e770cedf36bb393430
Orhan CINAR <orhancinar35@gmail.com>
2020-05-29 23:17:47 +0200

    add stack postfix to prefix

---
+++ C-Sharp-Practice/DataStructures/StackPostfixToPrefix.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace C_Sharp_Practice.DataStructures
+{
+    public class StackPostfixToPrefix
+    {
+        public string PostToPrefix(string exp)
+        {
+            Stack<string> s = new Stack<string>();
+
+            int length = exp.Length;
+
+            for (int i = 0; i < length; i++)
+            {
+                if (IsOperator(exp[i]))
+                {
+                    string op1 = (string)s.Peek();
+                    s.Pop();
+
+                    string op2 = (string)s.Peek();
+                    s.Pop();
+
+                    string temp = exp[i] + op2 + op1;
+                    s.Push(temp);
+                }
+                else
+                {
+                    s.Push(exp[i] + "");
+                }
+            }
+
+            return (string)s.Peek();
+        }
+
+
+        bool IsOperator(char x)
+        {
+            switch (x)
+            {
+                case '+':
+                case '-':
+                case '/':
+                case '*':
+                    return true;
+                default:
+                    return false;
+            }
+        }
+    }
+}
---
+++ XUnitTestProject/DataStructures/PostfixToPrefixTest.cs
@@ -0,0 +1,26 @@
+﻿using C_Sharp_Practice.DataStructures;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace XUnitTestProject.DataStructures
+{
+    public class PostfixToPrefixTest
+    {
+
+        StackPostfixToPrefix prefix = new StackPostfixToPrefix();
+
+        [Fact]
+
+        public void TestPostfixToPrefix()
+        {
+            string exp = "ABC/-AK/L-*";
+
+            var actual = prefix.PostToPrefix(exp);
+            var expected = "*-A/BC-/AKL";
+            Assert.Equal(expected, actual);
+
+        }
+    }
+}
