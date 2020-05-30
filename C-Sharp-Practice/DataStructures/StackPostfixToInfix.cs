64eb9d793288763764f9ab722d1935b3d374c561
Orhan CINAR <orhancinar35@gmail.com>
2020-05-30 20:43:32 +0200

    add  StackPostfixToInfix

---
+++ C-Sharp-Practice/DataStructures/StackPostfixToInfix.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace C_Sharp_Practice.DataStructures
+{
+    public class StackPostfixToInfix
+    {
+        public string PostfixToınfix(string exp)
+        {
+            Stack<string> s = new Stack<string>();
+
+            for (int i = 0; i < exp.Length; i++)
+            {
+                if (IsOperand(exp[i]))
+                {
+                    s.Push(exp[i] + "");
+                }
+                else
+                {
+                    string op1 = s.Peek();
+                    s.Pop();
+                    string op2 = s.Peek();
+                    s.Pop();
+
+                    s.Push("(" + op2 + exp[i] + op1 + ")");
+                }
+            }
+
+            return s.Peek();
+        }
+
+        bool IsOperand(char x)
+        {
+            return (x >= 'a' && x <= 'z') || (x >= 'A' && x <= 'Z');
+        }
+    }
+}
---
+++ XUnitTestProject/DataStructures/PostfixToInfixTest.cs
@@ -0,0 +1,23 @@
+﻿using C_Sharp_Practice.DataStructures;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace XUnitTestProject.DataStructures
+{
+    public class PostfixToInfixTest
+    {
+        StackPostfixToInfix stackPostfixToInfix = new StackPostfixToInfix();
+
+        [Fact]
+        public void TestPostfixToInfix()
+        {
+            string exp = "ab*c+";
+
+            var actual = stackPostfixToInfix.PostfixToınfix(exp);
+            var expected = "((a*b)+c)";
+            Assert.Equal(expected, actual);
+        }
+    }
+}
