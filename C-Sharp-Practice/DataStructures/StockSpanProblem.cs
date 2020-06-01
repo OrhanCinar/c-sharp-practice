7f083dabea4e98d72b86d2e8aa3671f49621f98c
Orhan CINAR <orhancinar35@gmail.com>
2020-06-01 16:38:48 +0200

    add stock span problem

---
+++ C-Sharp-Practice/DataStructures/StockSpanProblem.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace C_Sharp_Practice.DataStructures
+{
+    public class StockSpanProblem
+    {
+        public void CalculateSpan(int[] price, int n, int[] s)
+        {
+            s[0] = 1;
+
+            for (int i = 1; i < n; i++)
+            {
+                s[i] = 1;
+
+                for (int j = i - 1; j >= 0 && price[i] >= price[j]; j--)
+                {
+                    s[i]++;
+                }
+            }
+        }
+    }
+}
---
+++ XUnitTestProject/DataStructures/StockSpanProblemTest.cs
@@ -0,0 +1,25 @@
+﻿using C_Sharp_Practice.DataStructures;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace XUnitTestProject.DataStructures
+{
+    public class StockSpanProblemTest
+    {
+        StockSpanProblem stockSpanProblem = new StockSpanProblem();
+
+        [Fact]
+        public void TestStockSpanProblem()
+        {
+            int[] price = { 10, 4, 5, 90, 120, 80 };
+            int n = price.Length;
+            int[] s = new int[n];
+            stockSpanProblem.CalculateSpan(price, n, s);
+            int[] expected = { 1, 1, 2, 4, 5, 1 };
+
+            Assert.Equal(expected, s);
+        }
+    }
+}
