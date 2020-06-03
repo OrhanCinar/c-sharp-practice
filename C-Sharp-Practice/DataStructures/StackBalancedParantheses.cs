7f32cf59a5ae4b9f438b8639937c8f65cab1509a
Orhan CINAR <orhancinar35@gmail.com>
2020-06-03 19:34:17 +0200

    add StackBalancedParantheses

---
+++ C-Sharp-Practice/DataStructures/StackBalancedParantheses.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace C_Sharp_Practice.DataStructures
+{
+    public class StackBalancedParantheses
+    {
+
+        bool IsMatchingPair(char character1, char character2)
+        {
+            if (character1 == '(' && character2 == ')')
+            {
+                return true;
+            }
+
+            if (character1 == '{' && character2 == '}')
+            {
+                return true;
+            }
+
+            if (character1 == '[' && character2 == ']')
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        public bool AreparanthesisBalances(char[] exp)
+        {
+            Stack<char> s = new Stack<char>();
+
+            for (int i = 0; i < exp.Length; i++)
+            {
+                if (exp[i] == '{' || exp[i] == '(' || exp[i] == '[')
+                {
+                    s.Push(exp[i]);
+                }
+
+                if (exp[i] == '}' || exp[i] == ')' || exp[i] == ']')
+                {
+                    if (s.Count == 0)
+                    {
+                        return false;
+                    }
+                    
+                    if (!IsMatchingPair(s.Pop(), exp[i]))
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            return s.Count == 0;
+        }
+    }
+}
---
+++ XUnitTestProject/DataStructures/StackBalancedParanthesesTest.cs
@@ -0,0 +1,22 @@
+﻿using C_Sharp_Practice.DataStructures;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace XUnitTestProject.DataStructures
+{
+    public class StackBalancedParanthesesTest
+    {
+        StackBalancedParantheses stackBalancedParantheses = new StackBalancedParantheses();
+
+        [Fact]
+        public void TestStackBalancedParantheses()
+        {
+            char[] exp = { '{', '(', ')', '}', '[', ']' };
+            var actual = stackBalancedParantheses.AreparanthesisBalances(exp);
+            var expected = true;
+            Assert.Equal(expected, actual);
+        }
+    }
+}
