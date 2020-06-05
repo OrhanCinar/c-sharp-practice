9f21b9134cd04f1614fbb69f5a94539454aac834
Orhan CINAR <orhancinar35@gmail.com>
2020-06-05 16:08:31 +0200

    add find next greater element

---
+++ C-Sharp-Practice/DataStructures/NextGreaterElement.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace C_Sharp_Practice.DataStructures
+{
+    public class NextGreaterElement
+    {
+        public void FindNextGreaterElement(int[] arr, int n)
+        {
+            int next, i, j;
+
+
+            for (i = 0; i < n; i++)
+            {
+                next = -1;
+
+                for (j = i + 1; j < n; j++)
+                {
+                    if (arr[i] < arr[j])
+                    {
+                        next = arr[j];
+                        break;
+                    }
+                }
+            }
+        }
+    }
+}
