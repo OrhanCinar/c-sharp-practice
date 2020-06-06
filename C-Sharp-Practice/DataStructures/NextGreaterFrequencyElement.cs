adfc31154c7d27867227ebf7f3da9cb918793a7c
Orhan CINAR <orhancinar35@gmail.com>
2020-06-06 16:00:33 +0200

    add NextGreaterFrequencyElement

---
+++ C-Sharp-Practice/DataStructures/NextGreaterFrequencyElement.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace C_Sharp_Practice.DataStructures
+{
+    public class NextGreaterFrequencyElement
+    {
+        public string FindNextGreaterFrequency(int[] a, int n, int[] freq)
+        {
+            var sb = new StringBuilder();
+
+            Stack<int> s = new Stack<int>();
+            s.Push(0);
+
+            int[] res = new int[n];
+
+
+            for (int i = 0; i < n; i++)
+            {
+                res[i] = 0;
+            }
+
+            for (int i = 1; i < n; i++)
+            {
+                if (freq[a[s.Peek()]] > freq[a[i]])
+                {
+                    s.Push(i);
+                }
+                else
+                {
+                    while (freq[a[s.Peek()]] < freq[a[i]] && s.Count > 0)
+                    {
+                        res[s.Peek()] = a[i];
+                        s.Pop();
+                    }
+
+                    s.Push(i);
+                }
+            }
+
+            while (s.Count > 0)
+            {
+                res[s.Peek()] = -1;
+                s.Pop();
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+                sb.Append(res[i] + ",");
+            }
+
+            return sb.ToString();
+        }
+    }
+}
---
+++ XUnitTestProject/DataStructures/NextGreaterFrequencyElementTest.cs
@@ -0,0 +1,43 @@
+﻿using C_Sharp_Practice.DataStructures;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace XUnitTestProject.DataStructures
+{
+    public class NextGreaterFrequencyElementTest
+    {
+        NextGreaterFrequencyElement nextGreaterFrequency = new NextGreaterFrequencyElement();
+
+        [Fact]
+        public void TestNextGreaterFrequencyElement()
+        {
+            int[] a = { 1, 1, 2, 3, 4, 2, 1 };
+            int len = 7;
+            int max = int.MinValue;
+            for (int i = 0; i < len; i++)
+            {
+                // Getting the max element of the array 
+                if (a[i] > max)
+                {
+                    max = a[i];
+                }
+            }
+            int[] freq = new int[max + 1];
+
+            for (int i = 0; i < max + 1; i++)
+                freq[i] = 0;
+
+            // Calculating frequency of each element 
+            for (int i = 0; i < len; i++)
+            {
+                freq[a[i]]++;
+            }
+            var actual = nextGreaterFrequency.FindNextGreaterFrequency(a, len, freq);
+            var expected = "-1,-1,1,2,2,1,-1,";
+
+            Assert.Equal(expected, actual);
+        }
+    }
+}
