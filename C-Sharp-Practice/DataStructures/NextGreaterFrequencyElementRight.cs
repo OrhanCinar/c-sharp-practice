049fc4192b7d55f944295e743f7405f72c889812
Orhan CINAR <orhancinar35@gmail.com>
2020-06-07 17:42:43 +0200

    add NextGreaterFrequencyElementRight

---
+++ C-Sharp-Practice/DataStructures/NextGreaterFrequencyElementRight.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace C_Sharp_Practice.DataStructures
+{
+    public class NextGreaterFrequencyElementRight
+    {
+        public int GetGreater(int[] dp, int index)
+        {
+            return dp[index];
+        }
+
+        private void FillNext(int[] next, int[] a, int n)
+        {
+            Stack<int> s = new Stack<int>();
+
+            s.Push(0);
+
+            for (int i = 1; i < n; i++)
+            {
+                while (s.Count > 0)
+                {
+                    int cur = s.Peek();
+
+                    if (a[cur] < a[i])
+                    {
+                        next[cur] = i;
+                        s.Pop();
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+
+                s.Push(i);
+            }
+
+            while (s.Count > 0)
+            {
+                int cur = s.Peek();
+                next[cur] = -1;
+                s.Pop();
+            }
+        }
+
+        public void Count(int[] a, int[] dp, int n)
+        {
+            int[] next = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                next[i] = 0;
+            }
+
+            FillNext(next, a, n);
+
+            for (int i = n - 2; i >= 0; i--)
+            {
+                if (next[i] == -1)
+                {
+                    dp[i] = 0;
+                }
+                else
+                {
+                    dp[i] = 1 + dp[next[i]];
+                }
+            }
+        }
+    }
+}
---
+++ XUnitTestProject/DataStructures/NextGreaterFrequencyElementRightTest.cs
@@ -0,0 +1,28 @@
+﻿using C_Sharp_Practice.DataStructures;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace XUnitTestProject.DataStructures
+{
+    public class NextGreaterFrequencyElementRightTest
+    {
+        NextGreaterFrequencyElementRight nextGreaterFrequency = new NextGreaterFrequencyElementRight();
+
+        [Fact]
+        public void TestNextGreaterFrequencyElement()
+        {
+            int[] a = { 3, 4, 2, 7, 5, 8, 10, 6 };
+            int n = a.Length;
+
+            int[] dp = new int[n];
+
+            nextGreaterFrequency.Count(a, dp, n);            
+            var actual = nextGreaterFrequency.GetGreater(dp, 3);
+            var expected = 2;
+
+            Assert.Equal(expected, actual);
+        }
+    }
+}
