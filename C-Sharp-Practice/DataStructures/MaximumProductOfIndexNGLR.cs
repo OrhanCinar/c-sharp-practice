0a9d16c07770f1336702f8b7ed7f67098a2de03e
Orhan CINAR <orhancinar35@gmail.com>
2020-06-30 16:40:55 +0200

    add MaximumProductOfIndexNGLR

---
+++ C-Sharp-Practice/DataStructures/MaximumProductOfIndexNGLR.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+
+namespace C_Sharp_Practice.DataStructures
+{
+    public class MaximumProductOfIndexNGLR
+    {
+        int MAX = 1000;
+
+        public int Search(int[] arr, int n)
+        {
+            int[] left = NextGreaterInLeft(arr, n);
+            int[] right = NextGreaterInRight(arr, n);
+            int ans = -1;
+
+            for (int i = 0; i <= n; i++)
+            {
+                ans = Math.Max(ans, left[i] * right[i]);
+            }
+
+            return ans;
+
+        }
+
+        private int[] NextGreaterInLeft(int[] a, int n)
+        {
+            int[] left_index = new int[MAX];
+
+            Stack<int> s = new Stack<int>();
+
+            for (int i = n - 1; i > 0; i--)
+            {
+                while (s.Count != 0 && a[i] > a[s.Peek() - 1])
+                {
+                    int r = s.Peek();
+                    s.Pop();
+
+                    left_index[r - 1] = i + 1;
+                }
+
+                s.Push(i + 1);
+            }
+
+            return left_index;
+        }
+
+        private int[] NextGreaterInRight(int[] a, int n)
+        {
+            int[] right_index = new int[MAX];
+            Stack<int> s = new Stack<int>();
+
+            for (int i = 0; i < n; ++i)
+            {
+                while (s.Count != 0 && a[i] > a[s.Peek() - 1])
+                {
+                    int r = s.Peek();
+                    s.Pop();
+
+                    right_index[r - 1] = i + 1;
+                }
+
+                s.Push(i + 1);
+            }
+
+            return right_index;
+        }
+
+    }
+}
---
+++ XUnitTestProject/DataStructures/MaximumProductOfIndexNGLRTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using C_Sharp_Practice.DataStructures;
+using Xunit;
+
+namespace XUnitTestProject.DataStructures
+{
+    public class MaximumProductOfIndexNGLRTest
+    {
+        MaximumProductOfIndexNGLR maximumProductOfIndexNGLR = new MaximumProductOfIndexNGLR();
+
+        [Fact]
+        public void Test_MaximumProductOfIndexNGLR()
+        {
+            int[] arr = new int[] { 5, 4, 3, 4, 5 };
+            int n = arr.Length;
+            var actual = maximumProductOfIndexNGLR.Search(arr, n);
+            var expected = 8;
+            Assert.Equal(expected, actual);
+        }
+    }
+}
