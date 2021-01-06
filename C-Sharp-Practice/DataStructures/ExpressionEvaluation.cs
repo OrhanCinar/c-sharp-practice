cfd2e1d5a5b4e249c00c09cb95a48eea93a8a695
Orhan CINAR <orhancinar35@gmail.com>
2021-01-06 17:27:11 +0100

    add expression evaluation

---
+++ C-Sharp-Practice/DataStructures/ExpressionEvaluation.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+public class ExpressionEvaluation
+{
+    public int Evaluate(string expression)
+    {
+        char[] tokens = expression.ToCharArray();
+
+        var values = new Stack<int>();
+
+        var ops = new Stack<char>();
+
+        for (int i = 0; i < tokens.Length; i++)
+        {
+            if (tokens[i] == ' ')
+                continue;
+
+            if (tokens[i] >= '0' && tokens[i] <= '9')
+            {
+                StringBuilder sbuf = new StringBuilder();
+                while (i < tokens.Length && tokens[i] >= '0' && tokens[i] <= '9')
+                    sbuf.Append(tokens[i++]);
+                i = i - 1;
+                values.Push(int.Parse(sbuf.ToString()));
+            }
+            else if (tokens[i] == '(')
+                ops.Push(tokens[i]);
+            else if (tokens[i] == ')')
+            {
+                while (ops.Peek() != '(')
+                    values.Push(ApplyOp(ops.Pop(), values.Pop(), values.Pop()));
+                ops.Pop();
+            }
+            else if (tokens[i] == '+' || tokens[i] == '-' ||
+                    tokens[i] == '*' || tokens[i] == '/')
+            {
+                while (ops.Count > 0 && HasPrecedence(tokens[i], ops.Peek()))
+                    values.Push(ApplyOp(ops.Pop(), values.Pop(), values.Pop()));
+
+                ops.Push(tokens[i]);
+            }
+        }
+
+        while (ops.Count > 0)
+            values.Push(ApplyOp(ops.Pop(), values.Pop(), values.Pop()));
+
+        return values.Pop();
+    }
+
+    private bool HasPrecedence(char op1, char op2)
+    {
+        if (op1 == '(' || op2 == ')')
+        {
+            return false;
+        }
+
+        if ((op1 == '*' || op1 == '/') && (op2 == '+' || op2 == '-'))
+        {
+            return false;
+        }
+        else
+            return true;
+    }
+
+    private int ApplyOp(char op, int b, int a)
+    {
+        switch (op)
+        {
+            case '+': return a + b;
+            case '-': return a - b;
+            case '*': return a * b;
+            case '/':
+                if (b == 0)
+                {
+                    throw new NotSupportedException("Cannot divide by zero");
+                }
+                return a / b;
+
+            default:
+                return 0;
+        }
+    }
+}
---
+++ XUnitTestProject/DataStructures/ExpressionEvaluationTest.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace XUnitTestProject.DataStructures
+{
+
+
+    public class ExpressionEvaluationTest
+    {
+        ExpressionEvaluation expressionEvaluation = new ExpressionEvaluation();
+
+        [Fact]
+        public void Test_ExpressionEvaluation()
+        {
+            var evaluate1 = "10 + 2 * 6";
+            var evaluate2 = "100 * 2 + 12";
+            var evaluate3 = "100 * (2 + 12)";
+            var evaluate4 = "100 * ( 2 + 12 ) / 14";
+
+
+            var actual = expressionEvaluation.Evaluate(evaluate1);
+            var expected = 22;
+            Assert.Equal(expected, actual);
+
+
+            actual = expressionEvaluation.Evaluate(evaluate2);
+            expected = 212;
+            Assert.Equal(expected, actual);
+
+            actual = expressionEvaluation.Evaluate(evaluate3);
+            expected = 1400;
+            Assert.Equal(expected, actual);
+
+            //actual = expressionEvaluation.Evaluate(evaluate4);
+            //expected = 100;
+            //Assert.Equal(expected, actual);
+
+        }
+    }
+}
+
+
