905f9a74742ede36031dcff7726b89c3341f28d0
Orhan CINAR <orhancinar35@gmail.com>
2021-01-07 15:26:27 +0100

    add stack Evaluation of Postfix Expression

---
+++ C-Sharp-Practice/DataStructures/StackEvaluationPostfixExpression.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace C_Sharp_Practice.DataStructures
+{
+    public class StackEvaluationPostfixExpression
+    {
+        private Stack<string> i = new Stack<string>();
+
+        public string Expresion(string v)
+        {
+            int a, b, ans;
+
+            for (int j = 0; j < v.Length; j++)
+            {
+                var c = v.Substring(j, 1);
+
+                if (c.Equals("*"))
+                {
+                    var sa = i.Pop();
+                    var sb = i.Pop();
+                    a = Convert.ToInt32(sb);
+                    b = Convert.ToInt32(sa);
+
+                    ans = a * b;
+                    i.Push(ans.ToString());
+                }
+                else if (c.Equals("/"))
+                {
+                    var sa = i.Pop();
+                    var sb = i.Pop();
+                    a = Convert.ToInt32(sb);
+                    b = Convert.ToInt32(sa);
+
+                    ans = a / b;
+                    i.Push(ans.ToString());
+                }
+                else if (c.Equals("+"))
+                {
+                    var sa = i.Pop();
+                    var sb = i.Pop();
+                    a = Convert.ToInt32(sb);
+                    b = Convert.ToInt32(sa);
+
+                    ans = a + b;
+                    i.Push(ans.ToString());
+                }
+                else if (c.Equals("-"))
+                {
+                    var sa = i.Pop();
+                    var sb = i.Pop();
+                    a = Convert.ToInt32(sb);
+                    b = Convert.ToInt32(sa);
+
+                    ans = a - b;
+                    i.Push(ans.ToString());
+                }
+                else
+                {
+                    i.Push(v.Substring(j, 1));
+                }
+            }
+
+            return i.Pop();
+        }
+    }
+}
---
+++ XUnitTestProject/DataStructures/StackEvaluationPostfixExpressionTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using C_Sharp_Practice.DataStructures;
+using Xunit;
+
+namespace XUnitTestProject.DataStructures
+{
+    public class StackEvaluationPostfixExpressionTest
+    {
+        StackEvaluationPostfixExpression stackEvaluationPostfixExpression = new StackEvaluationPostfixExpression();
+
+        [Fact]
+        public void Test_StackEvaluationPostfixExpression()
+        {
+            var exp = ("231*+9-");
+            var actual = stackEvaluationPostfixExpression.Expresion(exp);
+            var expected = "-4";
+            Assert.Equal(expected, actual);
+        }
+    }
+}
