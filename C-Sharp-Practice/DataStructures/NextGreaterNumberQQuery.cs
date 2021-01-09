44965fee072b1cd1cc54961ff52b86f8a4a7cd63
Orhan CINAR <orhancinar35@gmail.com>
2021-01-09 17:30:11 +0100

    add Print next greater number of Q queries

---
+++ C-Sharp-Practice/DataStructures/NextGreaterNumberQQuery.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace C_Sharp_Practice.DataStructures
+{
+    public class NextGreaterNumberQQuery
+    {
+        public int[] Query(int[] arr, int[] query)
+        {
+            int[] ans = new int[arr.Length];
+
+            Stack<int> s = new Stack<int>();
+
+            s.Push(arr[0]);
+            int j = 0;
+
+            for (int i = 1; i < arr.Length; i++)
+            {
+                int next = arr[i];
+
+                if(s.Count > 0)
+                {
+                    int element = s.Pop();
+
+                    while(next > element)
+                    {
+                        ans[j] = next;
+                        j++;
+
+                        if (s.Count == 0)
+                        {
+                            break;
+                        }
+                        element = s.Pop();
+                    }
+
+
+
+                    if (element > next)
+                    {
+                        s.Push(element);
+                    }
+                }
+
+                s.Push(next);
+            }
+
+            while (s.Count > 0)
+            {
+                s.Pop();
+                ans[j] = -1;
+                j++;
+            }
+
+            return ans;
+        }
+    }
+}
---
+++ XUnitTestProject/DataStructures/NextGreaterNumberQQueryTest.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using C_Sharp_Practice.DataStructures;
+using Xunit;
+
+namespace XUnitTestProject.DataStructures
+{
+    public class NextGreaterNumberQQueryTest
+    {
+        NextGreaterNumberQQuery nextGreaterNumberQQuery = new NextGreaterNumberQQuery();
+
+        [Fact]
+        public void Test_NextGreaterNumberQQuery()
+        {
+
+            int[] arr = new int[] { 3, 4, 2, 7, 5, 8, 10, 6 };
+            int[] query = new int[] { 3, 6, 1 };
+            int[] ans = nextGreaterNumberQQuery.Query(arr, query);
+
+            var expected = new int[] { 8, -1, 7 };
+            var actual = new int[expected.Length];
+            for (int i = 0; i < query.Length; i++)
+            {
+                actual[i] = ans[query[i]];
+            }
+
+            Assert.Equal(expected, actual);
+        }
+    }
+}
