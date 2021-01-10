c6059247384bf2ec028104ef6c0f380dd1e283c5
Orhan CINAR <orhancinar35@gmail.com>
2021-01-10 18:29:18 +0100

    add Reverse a stack using recursion

---
+++ C-Sharp-Practice/DataStructures/ReverseStackRecursion.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace C_Sharp_Practice.DataStructures
+{
+    public class ReverseStackRecursion
+    {
+        Stack<char> st = new Stack<char>();
+
+        public void ReverseStack()
+        {
+            if (st.Count > 0)
+            {
+                char x = st.Peek();
+                st.Pop();
+                ReverseStack();
+
+                InsertAtBottom(x);
+            }
+        }
+
+        public void Push(char x)
+        {
+            st.Push(x);
+        }
+
+        public Stack<char> GetStack()
+        {
+            return st;
+        }
+
+        void InsertAtBottom(char x)
+        {
+            if (st.Count == 0)
+            {
+                st.Push(x);
+            }
+            else
+            {
+                char a = st.Peek();
+                st.Pop();
+                InsertAtBottom(x);
+
+                st.Push(a);
+            }
+        }
+    }
+}
---
+++ XUnitTestProject/DataStructures/ReverseStackRecursionTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using C_Sharp_Practice.DataStructures;
+using Xunit;
+
+namespace XUnitTestProject.DataStructures
+{
+    public class ReverseStackRecursionTest
+    {
+
+        ReverseStackRecursion reverseStackRecursion = new ReverseStackRecursion();
+
+        [Fact]
+        public void Test_ReverseStackRecursion()
+        {
+
+            reverseStackRecursion.Push('1');
+            reverseStackRecursion.Push('2');
+            reverseStackRecursion.Push('3');
+            reverseStackRecursion.Push('4');
+
+            reverseStackRecursion.ReverseStack();
+            var stack = reverseStackRecursion.GetStack();
+        }
+    }
+}
