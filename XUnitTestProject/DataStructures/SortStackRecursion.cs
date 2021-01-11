76fefdceae50a4b4cfba019297ad53e6da2c430d
Orhan CINAR <orhancinar35@gmail.com>
2021-01-11 18:24:35 +0100

    add Sort Stack with recursion

---
+++ XUnitTestProject/DataStructures/SortStackRecursion.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace XUnitTestProject.DataStructures
+{
+    public class SortStackRecursion
+    {
+        void SortedInsert(Stack<int>s, int x)
+        {
+            if (s.Count == 0 || x > s.Peek())
+            {
+                s.Push(x);
+                return;
+            }
+
+            int temp = s.Peek();
+            s.Pop();
+            SortedInsert(s, x);
+
+            s.Push(temp);
+        }
+
+        public void SortStack(Stack<int> s)
+        {
+            if (s.Count > 0)
+            {
+                int x = s.Peek();
+                s.Pop();
+                SortStack(s);
+
+                SortedInsert(s, x);
+            }
+        }
+    }
+}
