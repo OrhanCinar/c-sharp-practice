8c3868aaec1e281ec6cd7ce6bd74a51d04a1d79f
Orhan CINAR <orhancinar35@gmail.com>
2021-01-12 15:00:28 +0100

    add SortStackWithTempStack

---
+++ C-Sharp-Practice/DataStructures/SortStackWithTempStack.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace C_Sharp_Practice.DataStructures
+{
+    public class SortStackWithTempStack
+    {
+        public Stack<int> SortStack(Stack<int> s)
+        {
+            Stack<int> tmpStack = new Stack<int>();
+
+            while (s.Count > 0)
+            {
+                var tmp = s.Pop();
+
+                while(tmpStack.Count > 0 && tmpStack.Peek() > tmp)
+                {
+                    s.Push(tmpStack.Pop());
+                }
+
+                tmpStack.Push(tmp);
+            }
+
+            return tmpStack;
+        }
+    }
+}
