8ab68857c3fb763fc8f97d3158e7d95fbb65f4b0
Orhan CINAR <orhancinar35@gmail.com>
2021-01-14 15:16:53 +0100

    add stack delete middle element

---
+++ C-Sharp-Practice/DataStructures/StackDeleteMiddleElement.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace C_Sharp_Practice.DataStructures
+{
+    public class StackDeleteMiddleElement
+    {
+        void DeleteMid(Stack<char> st, int n, int curr = 0)
+        {
+            if (st.Count == 0 || curr == null)
+            {
+                return;
+            }
+
+            char x = st.Peek();
+            st.Pop();
+
+            DeleteMid(st, n, curr + 1);
+
+            if (curr != n / 2)
+            {
+                st.Push(x);
+            }
+        }
+    }
+}
