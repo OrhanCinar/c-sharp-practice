327adf68d075bc4b94c2010f14d7b7a35eb2ae2e
Orhan CINAR <orhancinar35@gmail.com>
2021-01-15 20:16:31 +0100

    add sort array with stack

---
+++ C-Sharp-Practice/DataStructures/SortArrayWithStack.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace C_Sharp_Practice.DataStructures
+{
+    public class SortArrayWithStack
+    {
+        public Stack<int> Sort(Stack<int> input)
+        {
+            Stack<int> tmpStack = new Stack<int>();
+
+            while (input.Count != 0)
+            {
+                int tmp = input.Peek();
+                input.Pop();
+
+                while(tmpStack.Count != 0 && tmpStack.Peek() < tmp)
+                {
+                    input.Push(tmpStack.Peek());
+                    tmpStack.Pop();
+                }
+
+                tmpStack.Push(tmp);
+            }
+
+
+            return tmpStack;
+        }
+
+
+    }
+}
