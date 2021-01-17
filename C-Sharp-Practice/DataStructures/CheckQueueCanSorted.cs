1fd3581577c4e7abfb7f6ef51821b50e365e2fad
Orhan CINAR <orhancinar35@gmail.com>
2021-01-17 15:28:59 +0100

    add Check if a queue can be sorted into another queue using a stack

---
+++ C-Sharp-Practice/DataStructures/CheckQueueCanSorted.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace C_Sharp_Practice.DataStructures
+{
+    public class CheckQueueCanSorted
+    {
+        public bool CheckSorted(int n, ref Queue<int> q)
+        {
+            Stack<int> st = new Stack<int>();
+            int expected = 1;
+            int fnt;
+
+            while (q.Count != 0)
+            {
+                fnt = q.Peek();
+                q.Dequeue();
+
+                if (fnt == expected)
+                {
+                    expected++;
+                }
+                else
+                {
+                    if (st.Count != 0)
+                    {
+                        st.Push(fnt);
+                    }
+                    else if (st.Count != 0 && st.Peek() < fnt)
+                    {
+                        return false;
+                    }
+                    else
+                    {
+                        st.Push(fnt);
+                    }
+                }
+
+                while (st.Count != 0 && st.Peek() == expected)
+                {
+                    st.Pop();
+                    expected++;
+                }
+            }
+
+            if (expected - 1 == n && st.Count == 0)
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
