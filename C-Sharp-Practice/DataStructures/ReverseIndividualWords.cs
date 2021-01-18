e6547cced517ba73e2eb7c1167d29c67bfb7f9bf
Orhan CINAR <orhancinar35@gmail.com>
2021-01-18 15:41:51 +0100

    add Reverse individual words

---
+++ C-Sharp-Practice/DataStructures/ReverseIndividualWords.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace C_Sharp_Practice.DataStructures
+{
+    public class ReverseIndividualWords
+    {
+        public void ReverseWord(string str)
+        {
+            Stack<char> st = new Stack<char>();
+
+            for (int i = 0; i < str.Length; ++i)
+            {
+                if(str[i] != ' ')
+                {
+                    st.Push(str[i]);
+                }
+                else
+                {
+                    while (st.Count > 0)
+                    {
+                        Console.Write(st.Pop());
+                    }
+
+                    Console.Write(" ");
+                }
+            }
+
+            while(st.Count> 0)
+            {
+                Console.Write(st.Pop());
+            }
+        }
+    }
+}
