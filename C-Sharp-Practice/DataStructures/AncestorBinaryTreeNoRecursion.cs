551e066ff31fb6897ff8d40c32f69801a385eac5
Orhan CINAR <orhancinar35@gmail.com>
2021-01-23 13:13:54 +0100

    add Print ancestors of a given binary tree node without recursion

---
+++ C-Sharp-Practice/DataStructures/AncestorBinaryTreeNoRecursion.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace C_Sharp_Practice.DataStructures
+{
+    public class AncestorBinaryTreeNoRecursion
+    {
+        public class BNode
+        {
+            public int data;
+            public BNode left, right;
+
+            public BNode(int data)
+            {
+                this.data = data;
+            }
+        }
+
+        public void PrintAcestors(BNode root, int key)
+        {
+            if (root == null)
+            {
+                return;
+            }
+
+            Stack<BNode> st = new Stack<BNode>();
+
+            while (true)
+            {
+                while(root != null && root.data != key)
+                {
+                    st.Push(root);
+                    root = root.left;
+                }
+
+                if (root != null && root.data == key)
+                {
+                    break;
+                }
+
+                if (st.Peek().right == null)
+                {
+                    root = st.Peek();
+                    st.Pop();
+
+                    while(st.Count > 0 && st.Peek().right == root)
+                    {
+                        root = st.Peek();
+                        st.Pop();
+                    }
+                }
+
+                root = st.Count == 0 ? null : st.Peek().right;
+            }
+
+            while (st.Count > 0)
+            {
+                Console.Write(st.Peek().data + " ");
+                st.Pop();
+            }
+        }
+    }
+}
