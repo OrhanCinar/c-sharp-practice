86914f58a9d41dd1f54fd67f377c7d98879a8d73
Orhan CINAR <orhancinar35@gmail.com>
2021-01-29 15:22:00 +0100

    add Minimum number of bracket reversals needed to make an expression balanced

---
+++ C-Sharp-Practice/DataStructures/MinNumBracketReversalExpBalanced.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace C_Sharp_Practice.DataStructures
+{
+    public class MinNumBracketReversalExpBalanced
+    {
+        public int CountMinReversal(string exp)
+        {
+            int len = exp.Length;
+
+            if (len % 2 != 0)
+            {
+                return -1;
+            }
+
+            Stack<char> s = new Stack<char>();
+
+            for (int i = 0; i < len; i++)
+            {
+                char c = exp[i];
+
+                if (c == '}' && s.Count > 0)
+                {
+                    if (s.Peek() == '{')
+                    {
+                        s.Pop();
+                    }
+                    else
+                    {
+                        s.Push(c);
+                    }
+                }
+                else
+                {
+                    s.Push(c);
+                }
+            }
+
+            int red_len = s.Count;
+
+            int n = 0;
+
+            while (s.Count > 0 && s.Peek() == '{')
+            {
+                s.Pop();
+                n++;
+            }
+
+            return red_len / 2 + n % 2;
+        }
+    }
+}
