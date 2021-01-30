4f641d802fc71f00ce7bcd22d6b39331b196c4bc
Orhan CINAR <orhancinar35@gmail.com>
2021-01-30 22:54:04 +0100

    add Expression contains redundant bracket or not

---
+++ C-Sharp-Practice/DataStructures/RedundantBracketCheck.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace C_Sharp_Practice.DataStructures
+{
+    public class RedundantBracketCheck
+    {
+        public bool CheckRedundancy(string s)
+        {
+            Stack<char> st = new Stack<char>();
+            char[] str = s.ToCharArray();
+
+            foreach (var item in str)
+            {
+                if (item == ')')
+                {
+                    char top = st.Peek();
+                    st.Pop();
+
+                    bool flag = true;
+
+                    while (top != ')')
+                    {
+                        if (top == '+' || top == '-' || top == '*' || top == '/')
+                        {
+                            flag = false;
+                        }
+
+                        top = st.Peek();
+                        st.Pop();
+                    }
+
+                    if (flag)
+                    {
+                        return true;
+                    }
+                }
+                else
+                {
+                    st.Push(item);
+                }
+            }
+
+
+            return false;
+        }
+    }
+}
