5b670e0f1a70b408bf3368c7c9341f3af405bcc0
Orhan CINAR <orhancinar35@gmail.com>
2021-01-31 16:57:22 +0100

    add Find index of closing bracket for a given opening bracket in an expression

---
+++ C-Sharp-Practice/DataStructures/FindIndexOfClosingBracket.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace C_Sharp_Practice.DataStructures
+{
+    public class FindIndexOfClosingBracket
+    {
+        public void FindIndex(string s, int index)
+        {
+
+            if (s[index] != '[')
+            {
+                Console.Write(s + ", " + index + " : -1");
+            }
+
+
+            Stack<int> st = new Stack<int>();
+            int i;
+
+            for (i = index; i < s.Length; i++)
+            {
+                if (s[i] == '[')
+                {
+                    st.Push(s[i]);
+                }else if (s[i] == ']')
+                {
+                    st.Pop();
+                    if(st.Count == 0)
+                    {
+                        Console.Write(s + ", " + index + " : "+ i);
+                        return;
+                    }
+                }
+            }
+
+            Console.Write(s + ", " + index + " : -1");
+        }
+    }
+}
