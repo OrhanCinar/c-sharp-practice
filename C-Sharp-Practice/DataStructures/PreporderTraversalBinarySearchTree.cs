d599ea6b7012b975fcfdd560431cec9396898267
Orhan CINAR <orhancinar35@gmail.com>
2021-02-03 22:52:53 +0100

    addd Check if a given array can represent Preorder Traversal of Binary Search Tree

---
+++ C-Sharp-Practice/DataStructures/PreporderTraversalBinarySearchTree.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace C_Sharp_Practice.DataStructures
+{
+    public class PreporderTraversalBinarySearchTree
+    {
+        public bool CanRepresentBts(int[] pre, int n)
+        {
+            Stack<int> s = new Stack<int>();
+
+            int root = int.MinValue;
+
+            for (int i = 0; i < n; i++)
+            {
+                if (pre[i] < root)
+                {
+                    return false;
+                }
+
+                while(s.Count > 0 && s.Peek() < pre[i])
+                {
+                    root = s.Peek();
+                    s.Pop();
+                }
+
+                s.Push(pre[i]);
+            }
+            
+            return true;
+        }
+    }
+}
