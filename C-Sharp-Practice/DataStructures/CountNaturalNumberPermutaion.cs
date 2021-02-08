c1d1a6acc5c8806cd56a6476fdfcc5102d454c9f
Orhan CINAR <orhancinar35@gmail.com>
2021-02-08 17:44:56 +0100

    add Count natural numbers whose all permutation are greater than that number

---
+++ C-Sharp-Practice/DataStructures/CountNaturalNumberPermutaion.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace C_Sharp_Practice.DataStructures
+{
+    public class CountNaturalNumberPermutaion
+    {
+        int CountNumber(int n)
+        {
+            int result = 0;
+            Stack<int> s = new Stack<int>();
+
+
+            for (int i = 0; i <= 9; i++)
+            {
+                if (i <= n)
+                {
+                    s.Push(i);
+                    result++;
+                }
+
+                while (s.Count != 0)
+                {
+                    int tp = s.Peek();
+                    s.Pop();
+
+                    for (int j = tp % 10; j <= 9; j++)
+                    {
+                        int x = tp * 10 + j;
+                        if (x <= n)
+                        {
+                            s.Push(x);
+                            result++;
+                        }
+                    }
+                }
+            }
+
+            return result;
+        }
+    }
+}
