10afdfb3442727c5a1f01a3f2b37ed533a0a88d6
Orhan CINAR <orhancinar35@gmail.com>
2021-02-09 18:32:26 +0100

    add Delete consecutive same words in a sequence

---
+++ C-Sharp-Practice/DataStructures/DeleteConsecutiveSameWords.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace C_Sharp_Practice.DataStructures
+{
+    public class DeleteConsecutiveSameWords
+    {
+        public int RemoveConsecutiveSame(List<string> v)
+        {
+            int n = v.Count;
+
+            for (int i = 0; i < n - 1; i++)
+            {
+                if (v[i].Equals(v[i + 1]))
+                {
+                    v.RemoveAt(i);
+                    v.RemoveAt(i);
+
+                    if (i > 0)
+                    {
+                        i--;
+                    }
+
+                    n = n - 2;
+
+                }
+                else
+                {
+                    i++;
+                }
+            }
+
+            return v.Count;
+        }
+    }
+}
