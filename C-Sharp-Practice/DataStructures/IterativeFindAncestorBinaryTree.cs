e6eddb03b11905881c31223e92ac444d6f922925
Orhan CINAR <orhancinar35@gmail.com>
2021-02-12 14:50:25 +0100

    add Iterative method to find ancestors of a given binary tree

---
+++ C-Sharp-Practice/DataStructures/IterativeFindAncestorBinaryTree.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace C_Sharp_Practice.DataStructures
+{
+    public class IterativeNode
+    {
+        public int data;
+        public IterativeNode left, right;
+    }
+
+    public class IterativeFindAncestorBinaryTree
+    {
+        public void PrintAcestors(IterativeNode root, int key)
+        {
+            if (root == null)
+            {
+                return;
+            }
+
+            Stack<IterativeNode> st = new Stack<IterativeNode>();
+
+            while (true)
+            {
+                while (root != null && root.data != key)
+                {
+                    st.Push(root);
+                    root = root.left;
+                }
+
+                if (root != null && root.data == key)
+                {
+                    break;
+                }
+
+                if (st.Peek().right != null)
+                {
+                    root = st.Peek();
+                    st.Pop();
+
+                    while (st.Count != 0 && st.Peek().right == root)
+                    {
+                        root = st.Peek();
+                        st.Pop();
+                    }
+                }
+
+                root = st.Count == 0 ? null : st.Peek().right;
+            }
+
+            while (st.Count != 0)
+            {
+                Console.Write(st.Peek().data + " ");
+                st.Pop();
+            }
+        }
+    }
+}
