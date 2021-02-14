24f7403114d79be45eed88c58fa805c8014bfecf
Orhan CINAR <orhancinar35@gmail.com>
2021-02-14 15:19:01 +0100

    add Tracking current Maximum Element in a Stack

---
+++ C-Sharp-Practice/DataStructures/TrackingMaximumElementInStack.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace C_Sharp_Practice.DataStructures
+{
+    public class TrackingMaximumElementInStack
+    {
+        Stack<int> mainStack = new Stack<int>();
+        Stack<int> trackStack = new Stack<int>();
+
+        public void Push(int x)
+        {
+            mainStack.Push(x);
+
+            if (mainStack.Count == 1)
+            {
+                trackStack.Push(x);
+                return;
+            }
+
+            if (x > trackStack.Peek())
+            {
+                trackStack.Push(x);
+            }
+            else
+            {
+                trackStack.Push(trackStack.Peek());
+            }
+        }
+
+        public void Pop()
+        {
+            mainStack.Pop();
+            trackStack.Pop();
+        }
+
+        public int GetMax()
+        {
+            return trackStack.Peek();
+        }
+
+    }
+}
