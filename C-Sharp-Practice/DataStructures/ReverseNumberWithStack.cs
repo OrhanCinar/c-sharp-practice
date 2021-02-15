fc0f7f6e4c4fad74784b07e4a71c120d34806a74
Orhan CINAR <orhancinar35@gmail.com>
2021-02-15 13:28:11 +0100

    add Reverse a number using stack

---
+++ C-Sharp-Practice/DataStructures/ReverseNumberWithStack.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace C_Sharp_Practice.DataStructures
+{
+    public class ReverseNumberWithStack
+    {
+        Stack<int> st = new Stack<int>();
+
+        public void Push(int number)
+        {
+            while (number != 0)
+            {
+                st.Push(number % 10);
+                number = number / 10;
+            }
+        }
+
+        public int ReverseNumber(int number)
+        {
+            Push(number);
+            int reverse = 0;
+            int i = 1;
+
+
+            while (st.Count > 0)
+            {
+                reverse = reverse + (st.Peek() * i);
+                st.Pop();
+                i *= 10;
+            }
+
+            return reverse;
+        }
+
+    }
+}
