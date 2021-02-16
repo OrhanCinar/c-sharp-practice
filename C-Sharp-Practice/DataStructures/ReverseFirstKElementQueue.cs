b3019268dc1f7c8efbb34600da72627a9b78dc52
Orhan CINAR <orhancinar35@gmail.com>
2021-02-16 20:52:28 +0100

    add Reversing the first K elements of a Queue

---
+++ C-Sharp-Practice/DataStructures/ReverseFirstKElementQueue.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace C_Sharp_Practice.DataStructures
+{
+    public class ReverseFirstKElementQueue
+    {
+        LinkedList<int> queue;
+
+        public void ReverseQueueFİrstKElement(int k)
+        {
+            if (queue.Count == 0 || k > queue.Count)
+            {
+                return;
+            }
+
+            if (k <= 0)
+            {
+                return;
+            }
+
+            Stack<int> stack = new Stack<int>();
+
+            for (int i = 0; i < k; i++)
+            {
+                stack.Push(queue.First.Value);
+                queue.RemoveFirst();
+            }
+
+            while(stack.Count > 0)
+            {
+                queue.AddLast(stack.Peek());
+                stack.Pop();
+            }
+
+            for (int i = 0; i < queue.Count-k; i++)
+            {
+                queue.AddLast(queue.First.Value);
+
+                queue.RemoveFirst();
+            }
+        }
+    }
+}
