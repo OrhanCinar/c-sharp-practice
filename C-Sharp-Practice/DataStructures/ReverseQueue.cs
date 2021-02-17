f24a1775c4581d548dc69562d03c9dd7d6c550bd
Orhan CINAR <orhancinar35@gmail.com>
2021-02-17 16:59:38 +0100

    add Reversing a Queue

---
+++ C-Sharp-Practice/DataStructures/ReverseQueue.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace C_Sharp_Practice.DataStructures
+{
+    public class ReverseQueue
+    {
+        LinkedList<int> queue = new LinkedList<int>();
+
+        public void Reverse()
+        {
+            Stack<int> stack = new Stack<int>();
+
+            while(queue.Count > 0)
+            {
+                stack.Push(queue.First.Value);
+                queue.RemoveFirst();
+            }
+
+            while(stack.Count > 0)
+            {
+                queue.AddLast(stack.Peek());
+                stack.Pop();
+            }
+        }
+    }
+}
