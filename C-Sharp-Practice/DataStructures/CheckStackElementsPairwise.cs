5bf4862d245d26c5191bd5909c54f4982076eaf2
Orhan CINAR <orhancinar35@gmail.com>
2021-02-18 23:35:56 +0100

    add Check if stack elements are pairwise consecutive

---
+++ C-Sharp-Practice/DataStructures/CheckStackElementsPairwise.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace C_Sharp_Practice.DataStructures
+{
+    public class CheckStackElementsPairwise
+    {
+        public bool PairWiseConsecutive(Stack<int> s)
+        {
+
+            Stack<int> aux = new Stack<int>();
+
+            while(s.Count != 0)
+            {
+                aux.Push(s.Peek());
+                s.Pop();
+            }
+
+
+            bool result = true;
+
+            while(aux.Count > 1)
+            {
+                int x = aux.Peek();
+                aux.Pop();
+                int y = aux.Peek();
+                aux.Pop();
+
+                if(Math.Abs(x-y) != 1)
+                {
+                    result = false;
+                }
+
+                s.Push(x);
+                s.Push(y);
+            }
+
+            if (aux.Count == 1)
+            {
+                s.Push(aux.Peek());
+            }
+
+            return result;
+        }
+    }
+}
