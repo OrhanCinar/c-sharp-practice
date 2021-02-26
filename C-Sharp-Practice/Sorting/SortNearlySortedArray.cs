6a4e2aad5116d9765582197d02348f3076f87fc5
Orhan CINAR <orhancinar35@gmail.com>
2021-02-26 19:09:40 +0100

    add Sort a nearly sorted (or K sorted) array

---
+++ C-Sharp-Practice/Sorting/SortNearlySortedArray.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace C_Sharp_Practice.Sorting
+{
+    public class SortNearlySortedArray
+    {
+        public void InsertionSort(int[] a, int size)
+        {
+            int i, key, j;
+
+            for (i = 0; i < size; i++)
+            {
+                key = a[i];
+                j = i - 1;
+
+                while (j >= 0 && a[j] > key)
+                {
+                    a[j + 1] = a[j];
+                    j = j - 1;
+                }
+
+                a[j + 1] = key;
+            }
+        }
+    }
+}
