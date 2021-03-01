1b2add5c0fc911d095121e50e93f9c06b814ddbf
Orhan CINAR <orhancinar35@gmail.com>
2021-03-01 18:22:17 +0100

    add Search in an almost sorted array

---
+++ C-Sharp-Practice/Searching/SearchInAlmosSortedArray.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace C_Sharp_Practice.Searching
+{
+    public class SearchInAlmosSortedArray
+    {
+        public int BinarySearch(int[] arr, int l, int r, int x)
+        {
+            if (r >= l)
+            {
+                int mid = l + (r - l) / 2;
+
+                if (arr[mid] == x)
+                {
+                    return mid;
+                }
+
+                if (mid > l && arr[mid - 1] == x)
+                {
+                    return mid - 1;
+                }
+
+                if (mid < r && arr[mid + 1] == x)
+                {
+                    return mid + 1;
+                }
+
+                if (arr[mid] > x)
+                {
+                    return BinarySearch(arr, l, mid - 2, x);
+                }
+
+                return BinarySearch(arr, mid + 2, r, x);
+            }
+
+            return -1;
+        }
+    }
+}
