ef60ba05112928dbe2de65d6cd6c7ed4844a3b87
Orhan CINAR <orhancinar35@gmail.com>
2021-03-02 13:36:27 +0100

    add Sort an array in wave form

---
+++ C-Sharp-Practice/Sorting/SortArrayWaveForm.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace C_Sharp_Practice.Sorting
+{
+    public class SortArrayWaveForm
+    {
+        void Swap(int[] arr, int a, int b)
+        {
+            int temp = arr[a];
+            arr[a] = arr[b];
+            arr[b] = temp;
+        }
+
+        void SortInWave(int[] arr, int n)
+        {
+            Array.Sort(arr);
+
+            for (int i = 0; i < n - 1; i += 2)
+            {
+                Swap(arr, i, i + 1);
+            }
+        }
+    }
+}
