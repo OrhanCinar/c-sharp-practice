5176096ac786dd442ac631cfe80948c04df2ccb7
Orhan CINAR <orhancinar35@gmail.com>
2021-03-06 15:57:27 +0100

    add Given a sorted array and a number x, find the pair in array whose sum is closest to x

---
+++ C-Sharp-Practice/Sorting/FindSumClosestToX.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace C_Sharp_Practice.Sorting
+{
+    public class FindSumClosestToX
+    {
+        public void PrintClosest(int[] arr, int n, int x)
+        {
+            int res_l = 0, res_r = 0;
+
+            int l = 0, r = n - 1, diff = int.MaxValue;
+
+            while (r > l)
+            {
+                if (Math.Abs(arr[l]+ arr[r] - x) < diff)
+                {
+                    res_l = l;
+                    res_r = r;
+                    diff = Math.Abs(arr[l] + arr[r] - x);
+                }
+
+                if (arr[l] + arr[r] > x)
+                {
+                    r--;
+                }
+                else
+                {
+                    l++;
+                }
+            }
+
+            Console.Write(" The closest pair is " + arr[res_l] + " and " + arr[res_r]);
+        }
+    }
+}
