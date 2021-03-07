3e80d8bfbbbaa728e5deed5518d19e70910b4338
Orhan CINAR <orhancinar35@gmail.com>
2021-03-07 15:16:46 +0100

    add Count 1’s in a sorted binary array

---
+++ C-Sharp-Practice/Sorting/Count1InSortedArray.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace C_Sharp_Practice.Sorting
+{
+    public class Count1InSortedArray
+    {
+        public int CountOnes(int[] arr, int low, int high)
+        {
+            if (high >= low)
+            {
+                int mid = low + (high - low) / 2;
+
+                if ((mid == high || arr[mid + 1] == 0) && (arr[mid]) == 1)
+                {
+                    return mid + 1;
+                }
+
+                if (arr[mid] == 1)
+                {
+                    return CountOnes(arr, (mid + 1), high);
+                }
+
+                return CountOnes(arr, low, (mid - 1));
+            }
+
+            return 0;
+        }
+    }
+}
