658cb6325e87fb798fd47c98feab47e5448d52d8
Orhan CINAR <orhancinar35@gmail.com>
2021-03-09 14:02:09 +0100

    add Activity Selection Problem

---
+++ C-Sharp-Practice/Greedy/ActivitySelectionProblem.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace C_Sharp_Practice.Sorting
+{
+    public class ActivitySelectionProblem
+    {
+        public void PrintMaxActivities(int[] s, int[] f, int n)
+        {
+            int i, j;
+
+            Console.Write("Following activities are selected : ");
+
+            i = 0;
+
+            Console.Write(i + " ");
+
+            for (j = 0; j < n; j++)
+            {
+                if (s[j] >= f[i])
+                {
+                    Console.Write(j + " ");
+                    i = j;
+                }
+            }
+        }
+    }
+}
