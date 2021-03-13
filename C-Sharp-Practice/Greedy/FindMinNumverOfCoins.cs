e97ddc971191e5911b2d6a1f6118489c2ac53323
Orhan CINAR <orhancinar35@gmail.com>
2021-03-13 13:39:36 +0100

    add Greedy Algorithm to find Minimum number of Coins

---
+++ C-Sharp-Practice/Greedy/FindMinNumverOfCoins.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Practice.Greedy
+{
+    public class FindMinNumverOfCoins
+    {
+        int[] deno = { 1, 2, 5, 10, 20, 50, 100, 500, 1000 };
+        int n = 0;
+        public FindMinNumverOfCoins()
+        {
+            n = deno.Length;
+        }
+
+        void FindMin(int V)
+        {
+            List<int> ans = new List<int>();
+
+            for (int i = n - 1; i >= 0; i--)
+            {
+                while (V >= deno[i])
+                {
+                    V -= deno[i];
+                    ans.Add(deno[i]);
+                }
+            }
+
+            for (int i = 0; i < ans.Count; i++)
+            {
+                Console.Write(" ", ans[i]);
+            }
+        }
+    }
+}
