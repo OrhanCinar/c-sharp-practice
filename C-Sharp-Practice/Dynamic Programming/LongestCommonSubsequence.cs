f167d9a2ef6821ed1a097455668cf7ebea9bcde1
Orhan CINAR <orhancinar35@gmail.com>
2021-03-16 17:19:42 +0100

    add

---
+++ C-Sharp-Practice/Dynamic Programming/LongestCommonSubsequence.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Practice.Dynamic_Programming
+{
+    public class LongestCommonSubsequence
+    {
+        public int LCS(char[] X, char[] Y, int m, int n)
+        {
+            if (m == 0 || n == 0)
+            {
+                return 0;
+            }
+
+            if (X[m - 1] == Y[n - 1])
+            {
+                return 1 + LCS(X, Y, m - 1, n - 1);
+            }
+
+            return Max(LCS(X, Y, m, n - 1), LCS(X, Y, m - 1, n));
+        }
+
+        private int Max(int a, int b)
+        {
+            return a > b ? a : b;
+        }
+    }
+}
