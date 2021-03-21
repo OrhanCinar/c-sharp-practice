a4229cc429f90019a579a0343a6458bf50fb8c93
Orhan Çınar <orhancinar35@gmail.com>
2021-03-21 17:28:40 +0100

    add Min Cost Path

---
+++ C-Sharp-Practice/Dynamic Programming/MinCostPath.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Practice.Dynamic_Programming
+{
+    public class MinCostPath
+    {
+        private int Min(int x, int y, int z)
+        {
+            if (x < y)
+            {
+                return x < z ? x : z;
+            }
+
+            return y < z ? y : z;
+        }
+
+        public int MinCost(int[,] cost, int m, int n)
+        {
+            if (n < 0 || m < 0)
+            {
+                return int.MaxValue;
+            }
+            else if (m == 0 && n == 0)
+            {
+                return cost[m, n];
+            }
+            else
+            {
+                return cost[m, n] + Min(MinCost(cost, m - 1, n - 1),
+                    MinCost(cost, m - 1, n),
+                    MinCost(cost, m, n - 1));
+            }
+        }
+    }
+}
