6062d9fbee2b9f0c16f68df410d361112fade9e0
Orhan Çınar <orhancinar35@gmail.com>
2021-03-24 13:58:28 +0100

    add Fibonacci

---
+++ C-Sharp-Practice/Dynamic Programming/Fibonacci.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Practice.Dynamic_Programming
+{
+    public class Fibonacci
+    {
+        public int Fib(int n)
+        {
+            if (n<= 1)
+            {
+                return n;
+            }
+            else
+            {
+                return Fib(n - 1) + Fib(n - 2);
+            }
+        }
+    }
+}
