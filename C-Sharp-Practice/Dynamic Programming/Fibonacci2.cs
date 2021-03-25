fafa81921042b0685f809f61fe085a1d14c2185e
Orhan Çınar <orhancinar35@gmail.com>
2021-03-25 15:30:14 +0100

    add dynamic fibonacci

---
+++ C-Sharp-Practice/Dynamic Programming/Fibonacci2.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Practice.Dynamic_Programming
+{
+    public class Fibonacci2
+    {
+        public int Fib(int n)
+        {
+            int f[n + 2];
+            int i;
+
+            f[0] = 0;
+            f[1] = 1;
+
+            for (i = 2; i <= n; i++)
+            {
+                f[i] = f[i - 1] + f[i - 2];
+            }
+
+            return f[n];
+        }
+    }
+}
