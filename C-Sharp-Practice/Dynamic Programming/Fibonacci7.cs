359ba5a8efcd2417ccdece9b7185790fdc611530
Orhan Çınar <orhancinar35@gmail.com>
2021-03-30 15:16:20 +0200

    add fibonacci

---
+++ C-Sharp-Practice/Dynamic Programming/Fibonacci7.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Practice.Dynamic_Programming
+{
+    public class Fibonacci7
+    {
+        public int Fib(int n)
+        {
+            double phi = (1 + Math.Sqrt(5)) / 2;
+            return (int)Math.Round(Math.Pow(phi, n) / Math.Sqrt(5));
+        }
+    }
+}
