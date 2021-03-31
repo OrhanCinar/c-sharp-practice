216a6663d1fd3c1253f99bbeebc69a755dbf822a
Orhan Çınar <orhancinar35@gmail.com>
2021-03-31 23:46:12 +0200

    add fibonacci

---
+++ C-Sharp-Practice/Dynamic Programming/Fibonacci8.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Practice.Dynamic_Programming
+{
+    public class Fibonacci8
+    {
+        int[] dp = new int[10];
+
+        public int Fib(int n)
+        {
+            if (n <= 1)
+            {
+                return n;
+            }
+
+            int first, second;
+
+            if (dp[n-1] != -1)
+            {
+                first = dp[n - 1];
+            }
+            else
+            {
+                first = Fib(n - 1);
+            }
+
+            if (dp[n - 2] != -1)
+            {
+                second = dp[n - 2];
+            }
+            else
+            {
+                second = Fib(n - 2);
+            }
+
+            return dp[n] = first + second;
+        }
+    }
+}
