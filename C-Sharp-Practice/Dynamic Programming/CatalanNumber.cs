e3856fe6719eb0ce443fe7aaaf742f3e345a337a
Orhan Çınar <orhancinar35@gmail.com>
2021-04-01 14:31:12 +0200

    add catalan number

---
+++ C-Sharp-Practice/Dynamic Programming/CatalanNumber.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Practice.Dynamic_Programming
+{
+    public class CatalanNumber
+    {
+        public int Catalan(int n)
+        {
+            int res = 0;
+
+
+            if (n <= 1)
+            {
+                return 1;
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+                res += Catalan(i) * Catalan(n - i - 1);
+            }
+
+
+            return res;
+        }
+    }
+}
