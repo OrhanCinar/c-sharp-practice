d100a48a7a496baa02fd4e9a4c5ff97653728025
Orhan Çınar <orhancinar35@gmail.com>
2021-04-02 14:20:17 +0200

    add catalan number dp

---
+++ C-Sharp-Practice/Dynamic Programming/CatalanNumber2.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Practice.Dynamic_Programming
+{
+    public class CatalanNumber2
+    {
+        public uint Catalan(uint n)
+        {
+            uint[] catalan = new uint[n + 2];
+
+            catalan[0] = catalan[1] = 1;
+
+            for (uint i = 2; i <= n; i++)
+            {
+                catalan[i] = 0;
+
+                for (uint j = 0; j < i; j++)
+                {
+                    catalan[i] += catalan[j] * catalan[i - j - 1];
+                }
+            }
+
+            return catalan[n];
+        }
+    }
+}
