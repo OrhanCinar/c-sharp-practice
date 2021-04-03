dd61101d937e013656e8b602b5f283e9c122d66f
Orhan Çınar <orhancinar35@gmail.com>
2021-04-03 15:31:45 +0200

    add catalan number

---
+++ C-Sharp-Practice/Dynamic Programming/CatalanNumber3.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Practice.Dynamic_Programming
+{
+    public class CatalanNumber3
+    {
+        private long BinomialCoeff(int n, int k)
+        {
+            long res = 1;
+
+            if (k > n - k)
+            {
+                k = n - k;
+            }
+
+            for (int i = 0; i < k; ++i)
+            {
+                res *= n - i;
+                res /= i + 1;
+            }
+
+            return res;
+        }
+
+        public long Catalan(int n)
+        {
+            long c = BinomialCoeff(2 * n, n);
+
+            return c / (n + 1);
+        }
+    }
+}
