b1cc079d45dd4250de972dec2d18b99942d6016c
Orhan Çınar <orhancinar35@gmail.com>
2021-04-05 15:21:25 +0200

    add Binomial Coefficient

---
+++ C-Sharp-Practice/Dynamic Programming/BinomialCoefficient.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Practice.Dynamic_Programming
+{
+    public class BinomialCoefficient
+    {
+        public int BinomialCoeff(int n, int k)
+        {
+            if (k > n)
+            {
+                return 0;
+            }
+
+            if (k == 0 || k == n)
+            {
+                return 1;
+            }
+
+            return BinomialCoeff(n - 1, k - 1) + BinomialCoeff(n - 1, k);
+        }
+
+    }
+}
