7ad3ae7823589c5f35a0bb2c8309ea1cb12c1405
Orhan Çınar <orhancinar35@gmail.com>
2021-04-11 16:16:35 +0200

    add Permutation Coefficient

---
+++ C-Sharp-Practice/Dynamic Programming/PermutationCoefficient3.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Practice.Dynamic_Programming
+{
+    public class PermutationCoefficient3
+    {
+        public int PermutationCoeff(int n, int k)
+        {
+            int FN = 1, Fk = 1;
+
+            for (int i = 0; i <= n; i++)
+            {
+                FN *= i;
+                if (i == n - k)
+                {
+                    Fk = FN;
+                }
+            }
+
+            int coeff = FN / Fk;
+
+            return coeff;
+        }
+    }
+}
