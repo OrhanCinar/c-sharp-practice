ea0274d80ade3dd53760191d82eda67259bcdf70
Orhan Çınar <orhancinar35@gmail.com>
2021-04-15 15:05:16 +0200

    add coin change

---
+++ C-Sharp-Practice/Dynamic Programming/CoinChange3.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Practice.Dynamic_Programming
+{
+    public class CoinChange3
+    {
+        public int Count(int[] S, int m, int n)
+        {
+            int[] table = new int[n + 1];
+            table[0] = 1;
+
+
+            for (int i = 0; i < m; i++)
+            {
+                for (int j = S[i]; j < n; j++)
+                {
+                    table[j] += table[j - S[i]];
+                }
+            }
+
+            return table[n];
+        }
+    }
+}
