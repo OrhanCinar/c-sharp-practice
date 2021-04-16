1714df9b9bacfea484011c15ebc85005ab18aa56
Orhan Çınar <orhancinar35@gmail.com>
2021-04-16 20:09:19 +0200

    add Friends Pairing Problem

---
+++ C-Sharp-Practice/Dynamic Programming/FriendsPairing.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Practice.Dynamic_Programming
+{
+    public class FriendsPairing
+    {
+        public int Count(int n)
+        {
+            int[] dp = new int[n + 1];
+            for (int i = 0; i < n; i++)
+            {
+                if (i <= 2)
+                {
+                    dp[i] = i;
+                }
+                else
+                {
+                    dp[i] = dp[i - 1] + (i - 1) * dp[i - 2];
+                }
+            }
+
+            return dp[n];
+        }
+    }
+}
