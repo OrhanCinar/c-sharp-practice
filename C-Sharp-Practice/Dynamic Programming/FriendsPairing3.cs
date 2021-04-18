f5d7a95d5e7f801aac61c2f0783bcacd100d7d18
Orhan Çınar <orhancinar35@gmail.com>
2021-04-18 16:26:52 +0200

    add Friends Pairing

---
+++ C-Sharp-Practice/Dynamic Programming/FriendsPairing3.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Practice.Dynamic_Programming
+{
+    public class FriendsPairing3
+    {
+        public int Count(int n)
+        {
+            int a = 1, b = 2, c = 0;
+
+            if (n <= 2)
+            {
+                return n;
+            }
+
+            for (int i = 3; i <= n; i++)
+            {
+                c = b + (i - 1) * a;
+                a = b;
+                b = c;
+            }
+
+            return c;
+        }
+    }
+}
