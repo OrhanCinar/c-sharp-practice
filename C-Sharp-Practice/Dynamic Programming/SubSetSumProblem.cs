1040d3c3e377d0ee7573edfb16f15f04fb12e6e0
Orhan Çınar <orhancinar35@gmail.com>
2021-04-19 15:13:01 +0200

    add Subset sum problem

---
+++ C-Sharp-Practice/Dynamic Programming/SubSetSumProblem.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Practice.Dynamic_Programming
+{
+    public class SubSetSumProblem
+    {
+        public bool IsSubsetSum(int[] set, int n, int sum)
+        {
+            if (sum == 0)
+            {
+                return true;
+            }
+
+            if (n == 0)
+            {
+                return false;
+            }
+
+            if (set[n - 1] > sum)
+            {
+                return IsSubsetSum(set, n - 1, sum);
+            }
+
+            return IsSubsetSum(set, n - 1, sum) || IsSubsetSum(set, n - 1, sum - set[n - 1]);
+        }
+    }
+}
