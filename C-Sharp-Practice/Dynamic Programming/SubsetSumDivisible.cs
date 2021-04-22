2f74600842bfec613a62effc4307b3a17ba2976d
Orhan Çınar <orhancinar35@gmail.com>
2021-04-22 13:56:36 +0200

    add Subset with sum divisible by m

---
+++ C-Sharp-Practice/Dynamic Programming/SubsetSumDivisible.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Practice.Dynamic_Programming
+{
+    public class SubsetSumDivisible
+    {
+        public bool ModularSum(int[] arr, int n, int m)
+        {
+            if (n > m)
+            {
+                return true;
+            }
+
+            bool[] DP = new bool[m];
+
+            for (int i = 0; i < DP.Length; i++)
+            {
+                DP[i] = false;
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+                if (DP[0])
+                {
+                    return true;
+                }
+
+                bool[] temp = new bool[m];
+
+                for (int l = 0; l < temp.Length; l++)
+                {
+                    temp[l] = false;
+                }
+
+                for (int j = 0; j < m; j++)
+                {
+                    if (DP[j])
+                    {
+                        if (!DP[(j + arr[i]) % m])
+                        {
+                            temp[(j + arr[i]) % m] = true;
+                        }
+                    }
+                }
+
+                for (int j = 0; j < m; j++)
+                {
+                    if (temp[j])
+                    {
+                        DP[j] = true;
+                    }
+                }
+
+                DP[arr[i] % m] = true;
+            }
+
+            return DP[0];
+        }
+    }
+}
