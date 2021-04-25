058e35e726b35ed83658442827fc2ef7299bfe6c
Orhan Çınar <orhancinar35@gmail.com>
2021-04-25 16:48:08 +0200

    add Cut a rod

---
+++ C-Sharp-Practice/Dynamic Programming/CuttingARod.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Practice.Dynamic_Programming
+{
+    public class CuttingARod
+    {
+        public int CutRod(int[] price, int n)
+        {
+            if (n <= 0)
+            {
+                return 0;
+            }
+
+            int max_val = int.MinValue;
+
+            for (int i = 0; i < n; i++)
+            {
+                max_val = Math.Max(max_val, price[i] + CutRod(price, n - i - 1));
+            }
+
+            return max_val;
+        }
+    }
+}
