df96f3bd69f297018b7f30fb1949b64a7ed3b1ce
Orhan Çınar <orhancinar35@gmail.com>
2021-04-28 14:52:20 +0200

    add Tiling With Dominoes

---
+++ C-Sharp-Practice/Dynamic Programming/TilingWithDominoes.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Practice.Dynamic_Programming
+{
+    public class TilingWithDominoes
+    {
+        public int Count(int n)
+        {
+            int[] A = new int[n + 1];
+            int[] B = new int[n + 1];
+
+            A[0] = 1;
+            A[1] = 1;
+
+            B[0] = 0;
+            B[1] = 1;
+
+            for (int i = 2; i <= n; i++)
+            {
+                A[i] = A[i - 2] + 2 * B[i - 1];
+                B[i] = A[i - 1] + B[i - 2];
+            }
+
+            return A[n];
+        }
+    }
+}
