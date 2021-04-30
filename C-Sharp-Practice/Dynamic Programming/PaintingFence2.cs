1c2ab218640fce7e769cedccd2e9c6da991683c0
Orhan Çınar <orhancinar35@gmail.com>
2021-04-30 20:02:07 +0200

    add Painting Fence

---
+++ C-Sharp-Practice/Dynamic Programming/PaintingFence2.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Practice.Dynamic_Programming
+{
+    public class PaintingFence2
+    {
+        public long Count(int n, int k)
+        {
+            long total = k;
+            int mod = 1000000007;
+
+            long same = 0, diff = k;
+
+            for (int i = 2; i <= n; i++)
+            {
+                same = diff;
+                diff = total * (k - 1);
+                diff = diff % mod;
+                total = (same + diff) % mod;
+            }
+
+            return total;
+        }
+    }
+}
