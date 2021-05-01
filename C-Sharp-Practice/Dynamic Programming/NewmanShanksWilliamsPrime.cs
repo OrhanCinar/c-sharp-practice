d0204f746f372dcf56d3185a2873f81ecac37bfc
Orhan Çınar <orhancinar35@gmail.com>
2021-05-01 16:26:13 +0200

    add Newman Shanks Williams Prime

---
+++ C-Sharp-Practice/Dynamic Programming/NewmanShanksWilliamsPrime.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Practice.Dynamic_Programming
+{
+    public class NewmanShanksWilliamsPrime
+    {
+        public int NWSP(int n)
+        {
+            if (n == 0 || n == 1)
+            {
+                return 1;
+            }
+
+            return 2 * NWSP(n - 1) + NWSP(n - 2);
+        }
+    }
+}
