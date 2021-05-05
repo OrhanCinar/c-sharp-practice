a0eaa65cfbcad97bff322625a9be655186b73f09
Orhan Çınar <orhancinar35@gmail.com>
2021-05-05 15:03:13 +0200

    add Golomb Sequence

---
+++ C-Sharp-Practice/Dynamic Programming/GolombSequence.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Practice.Dynamic_Programming
+{
+    class GolombSequence
+    {
+        public int FindGolomb(int n)
+        {
+            if (n == 1)
+            {
+                return 1;
+            }
+
+            return 1 + FindGolomb(n - FindGolomb(FindGolomb(n - 1)));
+        }
+    }
+}
