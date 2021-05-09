cfda7c1d4c3c458546b502e5831677bc4f69c450
Orhan Çınar <orhancinar35@gmail.com>
2021-05-09 15:32:44 +0200

    add Newman Conway Sequence

---
+++ C-Sharp-Practice/Dynamic Programming/NewmanConwaySequence.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Practice.Dynamic_Programming
+{
+    class NewmanConwaySequence
+    {
+        public int Sequence(int n)
+        {
+            if (n == 1 || n == 2)
+            {
+                return 1;
+            }
+            else
+            {
+                return Sequence(Sequence(n - 1)) + Sequence(n - Sequence(n - 1));
+            }
+        }
+    }
+}
