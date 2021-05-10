ba3b0112a85b8848b2b347bc3a33fb3d9f007f25
Orhan Çınar <orhancinar35@gmail.com>
2021-05-10 16:41:07 +0200

    add Newman Conway Sequence

---
+++ C-Sharp-Practice/Dynamic Programming/NewmanConwaySequence2.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Practice.Dynamic_Programming
+{
+    class NewmanConwaySequence2
+    {
+        public int Sequence(int n)
+        {
+            int[] f = new int[n + 1];
+
+            f[0] = 0;
+            f[1] = 1;
+            f[2] = 1;
+
+            int i;
+
+            for (i = 3; i <= n; i++)
+            {
+                f[i] = f[f[i - 1]] + f[i - f[i - 1]];
+            }
+
+            return f[n];
+        }
+    }
+}
