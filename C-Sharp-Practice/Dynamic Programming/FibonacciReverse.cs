5b0ed5649e98e40d79c6eb9d036a64ef6dbd1bb3
Orhan Çınar <orhancinar35@gmail.com>
2021-05-12 14:16:34 +0200

    add reverse fibonacci

---
+++ C-Sharp-Practice/Dynamic Programming/FibonacciReverse.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Practice.Dynamic_Programming
+{
+    class FibonacciReverse
+    {
+        public void ReverseFib(int n)
+        {
+            int[] a = new int[n];
+
+            a[0] = 0;
+            a[1] = 1;
+
+            for (int i = 2; i < n; i++)
+            {
+                a[i] = a[i - 2] + a[i - 1];
+            }
+
+            for (int i = n - 1; i >= 0; i--)
+            {
+                Console.Write(a[i] + " ");
+            }
+        }
+    }
+}
