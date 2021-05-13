18e5a57803523077132b1a40342355d233470169
Orhan Çınar <orhancinar35@gmail.com>
2021-05-13 20:42:46 +0200

    add Count even length binary sequences with same sum of first and second half bits

---
+++ C-Sharp-Practice/Dynamic Programming/CountEvenLengthBinarySeq.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Practice.Dynamic_Programming
+{
+    class CountEvenLengthBinarySeq
+    {
+        public int Count(int n, int diff)
+        {
+            if (Math.Abs(diff) > n)
+            {
+                return 0;
+            }
+
+            if (n == 1 && diff == 0)
+            {
+                return 2;
+            }
+
+            if (n == 1 && Math.Abs(diff) == 1)
+            {
+                return 1;
+            }
+
+            int res = Count(n - 1, diff + 1) + 2 * Count(n - 1, diff) + Count(n - 1, diff - 1);
+
+            return res;
+        }
+    }
+}
