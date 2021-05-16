f4f9323a94fb0cb681e4a5a5ced51d72bda69dea
Orhan Çınar <orhancinar35@gmail.com>
2021-05-16 15:16:51 +0200

    add Sequences of given length where every element is more than or equal to twice of previous

---
+++ C-Sharp-Practice/Dynamic Programming/SeqOfGivenLenElementEqualOrTwice.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Practice.Dynamic_Programming
+{
+    class SeqOfGivenLenElementEqualOrTwice
+    {
+        public int getTotalSequence(int m, int n)
+        {
+            if (m < n)
+            {
+                return 0;
+            }
+
+            if (n == 0)
+            {
+                return 1;
+            }
+
+            return getTotalSequence(m - 1, n) + getTotalSequence(m / 2, n - 1);
+        }
+    }
+}
