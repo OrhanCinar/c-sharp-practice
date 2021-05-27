ae3f1fd1567b304219330b649daaaa0e88786d30
Orhan Çınar <orhancinar35@gmail.com>
2021-05-27 15:30:43 +0200

    add Maximum product of an increasing subsequence

---
+++ C-Sharp-Practice/Dynamic Programming/MaxProductIncSubSeq.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Practice.Dynamic_Programming
+{
+    class MaxProductIncSubSeq
+    {
+        long Lis(long[] arr, long n)
+        {
+            long[] mpis = new long[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                mpis[i] = arr[i];
+            }
+
+            for (int i = 1; i < n; i++)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    if (arr[i] > arr[j] && mpis[i] < (mpis[j] * arr[i]))
+                    {
+                        mpis[i] = mpis[j] * arr[i];
+                    }
+                }
+            }
+
+
+            return mpis.Max();
+        }
+    }
+}
