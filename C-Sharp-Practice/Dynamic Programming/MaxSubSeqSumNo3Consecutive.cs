b5426ee0e41df876b4beaf94155ef0d06336797d
Orhan Çınar <orhancinar35@gmail.com>
2021-05-29 14:02:44 +0200

    add Maximum subsequence sum such that no three are consecutive

---
+++ C-Sharp-Practice/Dynamic Programming/MaxSubSeqSumNo3Consecutive.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Practice.Dynamic_Programming
+{
+    class MaxSubSeqSumNo3Consecutive
+    {
+        int MaxSumWo3Consex(int[] arr, int n)
+        {
+            int[] sum = new int[n];
+
+            if (n >= 1)
+            {
+                sum[0] = arr[0];
+            }
+
+            if (n >= 2)
+            {
+                sum[1] = arr[0] + arr[1];
+            }
+
+            if (n > 2)
+            {
+                sum[2] = Math.Max(sum[1], Math.Max(arr[1] + arr[2], arr[0] + arr[2]));
+            }
+
+
+            for (int i = 3; i < n; i++)
+            {
+                sum[i] = Math.Max(Math.Max(sum[i - 1], sum[i - 2] + arr[i]), arr[i] + arr[i - 1] + sum[i - 3]);
+            }
+
+            return sum[n - 1];
+        }
+    }
+}
