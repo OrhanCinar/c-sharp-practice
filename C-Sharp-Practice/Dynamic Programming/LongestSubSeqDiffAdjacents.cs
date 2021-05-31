14eef9408dd93cab24f31adfd432a96a508b8e03
Orhan Çınar <orhancinar35@gmail.com>
2021-05-31 19:50:37 +0200

    add Longest subsequence such that difference between adjacents is one

---
+++ C-Sharp-Practice/Dynamic Programming/LongestSubSeqDiffAdjacents.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Practice.Dynamic_Programming
+{
+    class LongestSubSeqDiffAdjacents
+    {
+        int LlongestSubseqWithDiffOne(int[] arr, int n)
+        {
+            int[] dp = new int[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                dp[i] = 1;
+            }
+
+
+            for (int i = 1; i < n; i++)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    if ((arr[i] == arr[j] + 1) || (arr[i] == arr[j] - 1))
+                    {
+                        dp[i] = Math.Max(dp[i], dp[j] + 1);
+                    }
+                }
+            }
+
+            int result = 1;
+
+            for (int i = 0; i < n; i++)
+            {
+                if (result < dp[i])
+                {
+                    result = dp[i];
+                }
+            }
+
+            return result;
+        }
+    }
+}
