4cb4891cd25d813663c42f1dab9c8df806f7b543
Orhan Çınar <orhancinar35@gmail.com>
2021-06-04 15:57:07 +0200

    add Maximum Length Chain of Pairs

---
+++ C-Sharp-Practice/Dynamic Programming/MaxLenChainPairs.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Practice.Dynamic_Programming
+{
+    class MaxLenChainPairs
+    {
+        internal class Pair
+        {
+            public int a;
+            public int b;
+
+            public Pair(int a, int b)
+            {
+                this.a = a;
+                this.b = b;
+            }
+        }
+
+        int MaxChainLength(Pair[] arr, int n)
+        {
+            int i, j, max = 0;
+            int[] mcl = new int[n];
+
+            for (i = 0; i < n; i++)
+            {
+                mcl[i] = 1;
+            }
+
+            for (i = 1; i < n; i++)
+            {
+                for (j = 0; j < i; j++)
+                {
+                    if (arr[i].a > arr[j].b && mcl[i] < mcl[j] + 1)
+                    {
+                        mcl[i] = mcl[j] + 1;
+                    }
+                }
+            }
+
+            for (i = 0; i < n; i++)
+            {
+                if (max < mcl[i])
+                {
+                    max = mcl[i];
+                }
+            }
+
+            return max;
+        }
+    }
+}
