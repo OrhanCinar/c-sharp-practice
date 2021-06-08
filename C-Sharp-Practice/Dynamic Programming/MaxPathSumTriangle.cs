1c4af6ec22368a8eb9efad48db5a169f3cc15d29
Orhan Çınar <orhancinar35@gmail.com>
2021-06-08 14:05:34 +0200

    add Maximum path sum in a triangle

---
+++ C-Sharp-Practice/Dynamic Programming/MaxPathSumTriangle.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Practice.Dynamic_Programming
+{
+    class MaxPathSumTriangle
+    {
+        int MaxPathSum(int[,] tri, int m, int n)
+        {
+            for (int i = m - 1; i >= 0; i--)
+            {
+                for (int j = 0; j <= i; j++)
+                {
+                    if (tri[i + 1, j] > tri[i + 1, j + 1])
+                    {
+                        tri[i, j] += tri[i + 1, j];
+                    }
+                    else
+                    {
+                        tri[i, j] += tri[i + 1, j + 1];
+                    }
+                }
+            }
+
+            return tri[0, 0];
+        }
+    }
+}
