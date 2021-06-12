66aec45e58c115158d275812c34a42f6b77e897f
Orhan Çınar <orhancinar35@gmail.com>
2021-06-12 16:16:26 +0200

    add Maximum sum of pairs with specific difference

---
+++ C-Sharp-Practice/Dynamic Programming/MaxSumPairSpecificDiff.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Practice.Dynamic_Programming
+{
+    class MaxSumPairSpecificDiff
+    {
+        int maxSumPairWithDifferenceLessThanK(int[] arr, int N, int K)
+        {
+            Array.Sort(arr);
+
+            int[] dp = new int[N];
+            dp[0] = 0;
+
+            for (int i = 1; i < N; i++)
+            {
+                dp[i] = dp[i - 1];
+
+                if (arr[i] - arr[i - 1] < K)
+                {
+                    if (i >= 2)
+                    {
+                        dp[i] = Math.Max(dp[i], dp[i - 2] + arr[i] + arr[i - 1]);
+                    }
+                    else
+                    {
+                        dp[i] = Math.Max(dp[i], arr[i] + arr[i - 1]);
+                    }
+                }
+            }
+
+            return dp[N - 1];
+        }
+    }
+}
