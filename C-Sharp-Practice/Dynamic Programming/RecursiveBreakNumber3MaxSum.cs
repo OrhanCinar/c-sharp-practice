b70077c5622aead2d52a47e7074974affd8aafdc
Orhan Çınar <orhancinar35@gmail.com>
2021-06-16 12:20:57 +0200

    add Recursively break a number in 3 parts to get maximum sum

---
+++ C-Sharp-Practice/Dynamic Programming/RecursiveBreakNumber3MaxSum.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Practice.Dynamic_Programming
+{
+    class RecursiveBreakNumber3MaxSum
+    {
+        int BreakSum(int n)
+        {
+            if (n == 0 || n == 1)
+            {
+                return n;
+            }
+
+
+            return Math.Max((BreakSum(n / 2) + BreakSum(n / 3) + BreakSum(n / 4)), n);
+        }
+    }
+}
