82c5214ce89c2ed4f309d1a68ea3adb94d506013
Orhan Çınar <orhancinar35@gmail.com>
2021-06-18 13:51:59 +0200

    add Maximum value with the choice of either dividing or considering as it is

---
+++ C-Sharp-Practice/Dynamic Programming/MaxValueWCoicheOfDividing.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Practice.Dynamic_Programming
+{
+    class MaxValueWCoicheOfDividing
+    {
+        int MaxDp(int n)
+        {
+            int[] res = new int[n + 1];
+            res[0] = 0;
+            res[1] = 1;
+
+            for (int i = 2; i <= n; i++)
+            {
+                res[i] = Math.Max(i, (res[i / 2] + res[i / 3] + res[i / 4] + res[i / 5]));
+            }
+
+            return res[n];
+        }
+    }
+}
