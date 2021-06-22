5a114f626a4c6f56c90c8c1c1f277bf316c28014
Orhan Çınar <orhancinar35@gmail.com>
2021-06-22 14:56:14 +0200

    add Maximum path sum for each position with jumps under divisibility condition

---
+++ C-Sharp-Practice/Dynamic Programming/MaxPathSumPosJump.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Practice.Dynamic_Programming
+{
+    class MaxPathSumPosJump
+    {
+        void PrintMaxSum(int[] arr, int n)
+        {
+            int[] dp = new int[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                dp[i] = arr[i];
+
+
+                int maxi = 0;
+
+                for (int j = 1; j < Math.Sqrt(i + 1); j++)
+                {
+                    if (((i + 1) % j == 0) && (i + 1) != j)
+                    {
+                        if (dp[j - 1] > maxi)
+                        {
+                            maxi = dp[j - 1];
+                        }
+
+                        if (dp[i + 1 / j - 1] > maxi && j != 1)
+                        {
+                            maxi = dp[(i + 1) / j - 1];
+                        }
+                    }
+                }
+
+                dp[i] += maxi;
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write(dp[i] + " ");
+            }
+        }
+    }
+}
