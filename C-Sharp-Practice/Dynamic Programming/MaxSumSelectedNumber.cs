fbbbb2aab33ac08a1342c94ce8162f0cfd217504
Orhan Çınar <orhancinar35@gmail.com>
2021-06-23 14:06:54 +0200

    add Maximize the sum of selected numbers from an array to make it empty

---
+++ C-Sharp-Practice/Dynamic Programming/MaxSumSelectedNumber.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Practice.Dynamic_Programming
+{
+    class MaxSumSelectedNumber
+    {
+        int GetMaximumSum(int[] arr)
+        {
+            int n = arr.Length;
+
+            int max = arr.Max();
+
+            int[] freq = new int[max + 1];
+
+            for (int j = 0; j < n; j++)
+            {
+                freq[arr[j]]++;
+            }
+
+            int ans = 0, i = max;
+
+            while (i > 0)
+            {
+                if (freq[i] > 0)
+                {
+                    ans += 1;
+
+                    freq[i - 1]--;
+                    freq[i]--;
+                }
+                else
+                {
+                    i--;
+                }
+            }
+
+            return ans;
+        }
+    }
+}
