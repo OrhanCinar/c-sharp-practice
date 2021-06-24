44e0aa60cb5831c96022ff98985bde8840399966
Orhan Çınar <orhancinar35@gmail.com>
2021-06-24 14:23:32 +0200

    add Maximum subarray sum in an array created after repeated concatenation

---
+++ C-Sharp-Practice/Dynamic Programming/MaxSubArrSumInArrAfterConcat.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Practice.Dynamic_Programming
+{
+    class MaxSubArrSumInArrAfterConcat
+    {
+        int MaxSubArraySumRepeated(int[] a, int n, int k)
+        {
+
+            int max_so_far = 0;
+            int max_ending_here = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                max_ending_here = max_ending_here + a[i % n];
+
+                if (max_so_far < max_ending_here)
+                {
+                    max_so_far = max_ending_here;
+                }
+
+                if (max_ending_here < 0)
+                {
+                    max_ending_here = 0;
+                }
+            }
+
+            return max_so_far;
+        }
+    }
+}
