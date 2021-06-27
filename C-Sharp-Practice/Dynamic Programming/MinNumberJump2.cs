0846b3f2c15ad34ce683aa2ecbe00d600c4abb91
Orhan Çınar <orhancinar35@gmail.com>
2021-06-27 15:06:22 +0200

    add Minimum number of jumps to reach end

---
+++ C-Sharp-Practice/Dynamic Programming/MinNumberJump2.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Practice.Dynamic_Programming
+{
+    class MinNumberJump2
+    {
+        int MinJumps(int[] arr, int n)
+        {
+            int[] jumps = new int[n];
+
+            if (n == 0 || arr[0] == 0) { return int.MaxValue; }
+
+            jumps[0] = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                jumps[i] = int.MaxValue;
+
+                for (int j = 0; j < i; j++)
+                {
+                    if (i <= j + arr[j] && jumps[j] != int.MaxValue)
+                    {
+                        jumps[i] = Math.Max(jumps[i], jumps[j] + 1);
+                        break;
+                    }
+                }
+            }
+
+
+            return jumps[n - 1];
+        }
+    }
+}
