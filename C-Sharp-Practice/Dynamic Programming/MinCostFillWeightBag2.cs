383ce518b4877902090fa519a1791d516264f09f
Orhan Çınar <orhancinar35@gmail.com>
2021-06-30 17:24:47 +0200

    add Minimum cost to fill given weight in a bag

---
+++ C-Sharp-Practice/Dynamic Programming/MinCostFillWeightBag2.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Practice.Dynamic_Programming
+{
+    class MinCostFillWeightBag2
+    {
+        int MinCost(int[] cost, int n)
+        {
+            int[] dp = new int[n + 1];
+            dp[0] = 0;
+
+            for (int i = 1; i <= n; i++)
+            {
+                int min_cost = int.MaxValue;
+
+                for (int j = 0; j < i; j++)
+                {
+                    if (j < n && cost[j] != -1)
+                    {
+                        min_cost = Math.Min(min_cost, cost[j] + dp[i - j - 1]);
+                    }
+                }
+
+                dp[i] = min_cost;
+            }
+
+            return dp[n];
+        }
+    }
+}
