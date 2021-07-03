df56bbeeb713cba9d93a97fcb8bccbfaeaddb3a7
Orhan Çınar <orhancinar35@gmail.com>
2021-07-03 13:58:04 +0200

    add Minimum removals from array to make max – min <= K

---
+++ C-Sharp-Practice/Dynamic Programming/MinRemovalFromArrToMakeMaxMin2.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Practice.Dynamic_Programming
+{
+    class MinRemovalFromArrToMakeMaxMin2
+    {
+        int FindInd(int key, int i,int n, int k, int[] arr)
+        {
+            int start, end, mid, ind = -1;
+            start = i + 1;
+            end = n - 1;
+
+            while (start < end)
+            {
+                mid = start + (end - start) / 2;
+
+                if (arr[mid] - k <= k)
+                {
+                    ind = mid;
+                    start = mid + 1;
+                }
+                else
+                {
+                    end = mid;
+                }
+            }
+
+            return ind;
+        }
+
+        int Removals(int[] arr, int n, int k)
+        {
+            int i, j, ans = n - 1;
+          
+            Array.Sort(arr);
+            
+            for (i = 0; i < n; i++)
+            {
+                j = FindInd(arr[i], i, n, k, arr);
+                
+                if (j != -1)
+                {
+                    ans = Math.Min(ans,n - (j - i + 1));
+                }
+            }
+            
+            return ans;
+        }
+    }
+}
