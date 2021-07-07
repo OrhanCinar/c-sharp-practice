00122af9a0f552f992024c5e357c5b2109206e74
Orhan Çınar <orhancinar35@gmail.com>
2021-07-07 14:51:32 +0200

    add Longest Common Substring

---
+++ C-Sharp-Practice/Dynamic Programming/LongestCommonSubstring3.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Practice.Dynamic_Programming
+{
+    class LongestCommonSubstring3
+    {
+        String X, Y;
+        int Lcs(int i, int j, int count)
+        {
+            if (i == 0 || j == 0)
+            {
+                return count;
+            }
+
+            if (X[i - 1] == Y[j - 1])
+            {
+                count = Lcs(i - 1, j - 1, count + 1);
+            }
+            count = Math.Max(count, Math.Max(Lcs(i, j - 1, 0), Lcs(i - 1, j, 0)));
+
+            return count;
+        }
+    }
+}
