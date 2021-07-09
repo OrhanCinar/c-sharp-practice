5ed1de24be6d558014d20c6fcbbcf0cdb1b099a5
Orhan Çınar <orhancinar35@gmail.com>
2021-07-09 22:49:13 +0200

    add Sum of all substrings of a string representing a number

---
+++ C-Sharp-Practice/Dynamic Programming/SumAllSubStringsOfString.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Practice.Dynamic_Programming
+{
+    class SumAllSubStringsOfString
+    {
+        int SumOfSubstrings(String num)
+        {
+            int n = num.Length;
+
+            int[] sumOfDigit = new int[n];
+            sumOfDigit[0] = num[0] - '0';
+            int res = sumOfDigit[0];
+
+            for (int i = 1; i < n; i++)
+            {
+                int numi = num[i] - '0';
+                sumOfDigit[i] = (i + 1) * numi + 10 * sumOfDigit[i - 1];
+                res += sumOfDigit[i];
+            }
+
+            return res;
+        }
+    }
+}
