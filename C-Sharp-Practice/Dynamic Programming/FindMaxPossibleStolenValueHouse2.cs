1e03c35212c3b43c47ba9cf6b95e5193aaae0682
Orhan Çınar <orhancinar35@gmail.com>
2021-07-14 16:41:58 +0200

    Find maximum possible stolen value from houses

---
+++ C-Sharp-Practice/Dynamic Programming/FindMaxPossibleStolenValueHouse2.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Practice.Dynamic_Programming
+{
+    class FindMaxPossibleStolenValueHouse2
+    {
+        int MaxLoot(int[] hval, int n)
+        {
+            if (n == 0)
+            {
+                return 0;
+            }
+
+
+            int value1 = hval[0];
+
+            if (n == 1)
+            {
+                return value1;
+            }
+
+            int value2 = Math.Max(hval[0], hval[1]);
+
+            if (n == 2)
+            {
+                return value2;
+            }
+
+            int max_val = 0;
+
+            for (int i = 2; i < n; i++)
+            {
+                max_val = Math.Max(hval[i] + value1, value2);
+                value1 = value2;
+                value2 = max_val;
+            }
+
+
+            return max_val;
+        }
+    }
+}
