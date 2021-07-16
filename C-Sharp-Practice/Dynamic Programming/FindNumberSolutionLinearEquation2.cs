7aa2a000bdb7922012c3df308e0e263d345e634a
Orhan Çınar <orhancinar35@gmail.com>
2021-07-16 15:20:05 +0200

    add Find number of solutions of a linear equation of n variables

---
+++ C-Sharp-Practice/Dynamic Programming/FindNumberSolutionLinearEquation2.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Practice.Dynamic_Programming
+{
+    class FindNumberSolutionLinearEquation2
+    {
+        int CountSol(int[] coeff, int n, int rhs)
+        {
+            int[] dp = new int[rhs + 1];
+            dp[0] = 1;
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = coeff[i]; j < rhs; j++)
+                {
+                    dp[j] += dp[j - coeff[i]];
+                }
+            }
+
+            return dp[rhs];
+        }
+    }
+
+}
