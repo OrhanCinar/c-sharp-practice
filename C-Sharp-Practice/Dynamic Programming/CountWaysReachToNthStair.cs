3e1aa63dfcbc85090479a4a2d4be74fa9f0a0cfc
Orhan Çınar <orhancinar35@gmail.com>
2021-07-18 16:34:08 +0200

    add Count ways to reach the nth stair using step 1, 2 or 3

---
+++ C-Sharp-Practice/Dynamic Programming/CountWaysReachToNthStair.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Practice.Dynamic_Programming
+{
+    class CountWaysReachToNthStair
+    {
+        int FindStep(int n)
+        {
+            if (n == 1 || n == 0)
+            {
+                return 1;
+            }
+            else if (n == 2)
+            {
+                return 2;
+            }
+            else
+            {
+                return FindStep(n - 3) + FindStep(n - 2) + FindStep(n - 1);
+            }
+        }
+    }
+}
