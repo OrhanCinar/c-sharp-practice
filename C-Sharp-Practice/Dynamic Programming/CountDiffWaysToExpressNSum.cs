f1860f789c5a8790b8256251a3df3afa741740f0
Orhan Çınar <orhancinar35@gmail.com>
2021-07-20 13:55:58 +0200

    add Count of different ways to express N as the sum of 1, 3 and 4

---
+++ C-Sharp-Practice/Dynamic Programming/CountDiffWaysToExpressNSum.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Practice.Dynamic_Programming
+{
+    class CountDiffWaysToExpressNSum
+    {
+        int CountWays(int n)
+        {
+            int[] Dp = new int[n + 1];
+            Dp[0] = Dp[1] = Dp[2] = 1;
+            Dp[3] = 2;
+
+            for (int i = 4; i <= n; i++)
+            {
+                Dp[i] = Dp[i - 1] + Dp[i - 3] + Dp[i - 4];
+            }
+
+            return Dp[n];
+        }
+    }
+}
