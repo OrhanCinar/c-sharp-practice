b902f8f4a7408101392eadce126ad20a6e229f73
Orhan Çınar <orhancinar35@gmail.com>
2021-07-22 15:37:50 +0200

    add Count ways to build street under given constraints

---
+++ C-Sharp-Practice/Dynamic Programming/CountWaysBuildStreet2.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Practice.Dynamic_Programming
+{
+    class CountWaysBuildStreet2
+    {
+        int Countways(int n)
+        {
+            int[] A = new int[n + 1];
+            A[0] = 1;
+            A[1] = 3;
+            A[2] = 7;
+
+            for (int i = 2; i <= n; i++)
+            {
+                A[i] = 2 * A[i - 1] + A[i - 2];
+            }
+
+            return A[n];
+        }
+    }
+}
