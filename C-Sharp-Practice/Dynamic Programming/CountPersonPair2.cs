c0c895c996e97bbf940e6fd44e729f8f9b836642
Orhan Çınar <orhancinar35@gmail.com>
2021-07-25 16:17:02 +0200

    add Counting pairs when a person can form pair with at most one

---
+++ C-Sharp-Practice/Dynamic Programming/CountPersonPair2.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Practice.Dynamic_Programming
+{
+    class CountPersonPair2
+    {
+        int NumberOfWays(int x)
+        {
+            int[] dp = new int[x + 1];
+            dp[0] = dp[1] = 1;
+
+            for (int i = 2; i <= x; i++)
+            {
+                dp[i] = dp[i - 1] + (i - 1) * dp[i - 2];
+            }
+
+            return dp[x];
+        }
+    }
+}
