4c031367a60429ccedef78cea0dbde7428da43d0
Orhan Çınar <orhancinar35@gmail.com>
2021-07-26 15:05:58 +0200

    add Counts paths from a point to reach Origin

---
+++ C-Sharp-Practice/Dynamic Programming/CountPathFromPointToOrigin.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Practice.Dynamic_Programming
+{
+    class CountPathFromPointToOrigin
+    {
+        int CountPaths(int n, int m)
+        {
+            if (n == 0 || m == 0)
+            {
+                return 1;
+            }
+
+            return (CountPaths(n - 1, m)) + CountPaths(n, m - 1);
+        }
+    }
+}
