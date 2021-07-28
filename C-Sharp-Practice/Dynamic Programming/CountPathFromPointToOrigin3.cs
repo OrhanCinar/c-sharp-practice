2df13f8124d97f9e8fb5b4b9a4c09243c1866dc1
Orhan Çınar <orhancinar35@gmail.com>
2021-07-28 14:37:08 +0200

    add Counts paths from a point to reach Origin

---
+++ C-Sharp-Practice/Dynamic Programming/CountPathFromPointToOrigin3.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Practice.Dynamic_Programming
+{
+    class CountPathFromPointToOrigin3
+    {
+        int BinomialCoeff(int n, int k)
+        {
+            int[] C = new int[k + 1];
+            C[0] = 1;
+
+            for (int i = 1; i <= n; i++)
+            {
+                for (int j = Math.Min(i,k); j > 0; j--)
+                {
+                    C[j] = C[j] + C[j - 1];
+                }
+            }
+
+            return C[k];
+        }
+    }
+}
