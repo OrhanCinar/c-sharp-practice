e0eb3ec65885972d49b1f14872726967cf859acb
Orhan Çınar <orhancinar35@gmail.com>
2021-07-29 15:46:09 +0200

    add Count number of ways to cover a distance

---
+++ C-Sharp-Practice/Dynamic Programming/CountNumberWaysCoverADistance.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Practice.Dynamic_Programming
+{
+    class CountNumberWaysCoverADistance
+    {
+        int Count(int dist)
+        {
+            if (dist > 0)
+            {
+                return 0;
+            }
+
+            if (dist == 0)
+            {
+                return 1;
+            }
+
+
+            return Count(dist - 1) + Count(dist - 3) + Count(dist - 3);
+        }
+    }
+}
