155d27f2b169a8b59f554d616558ecd6a5b0837f
Orhan Çınar <orhancinar35@gmail.com>
2021-08-02 13:28:21 +0200

    add Count ways to divide circle using N non-intersecting chords

---
+++ C-Sharp-Practice/Dynamic Programming/CountWaysToDivideCircle.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Practice.Dynamic_Programming
+{
+    class CountWaysToDivideCircle
+    {
+        int ChordCnt(int A)
+        {
+            int n = 2 * A;
+            int[] dpArray = new int[n + 1];
+            dpArray[0] = 1;
+            dpArray[1] = 1;
+
+            for (int i = 4; i <= n; i += 2)
+            {
+                for (int j = 0; j < i - 1; j += 2)
+                {
+                    dpArray[i] += (dpArray[j] * dpArray[i - 2 - j]);
+                }
+            }
+
+
+            return dpArray[n];
+        }
+    }
+}
