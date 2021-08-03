4b1fb7d25cb55f9a91616afebb8ad1a280534c72
Orhan Çınar <orhancinar35@gmail.com>
2021-08-03 16:18:07 +0200

    add Count the number of ways to tile the floor of size n x m using 1 x m size tiles

---
+++ C-Sharp-Practice/Dynamic Programming/CountWaysTileFloor.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Practice.Dynamic_Programming
+{
+    class CountWaysTileFloor
+    {
+        int CountWays(int n, int m)
+        {
+            int[] count = new int[n + 1];
+            count[0] = 0;
+            int i;
+
+
+            for (i = 1; i <= n; i++)
+            {
+                if (i > m)
+                {
+                    count[i] = count[i - 1] + count[i - m];
+                }
+                else if (i < m || i == 1)
+                {
+                    count[i] = 1;
+                }
+                else
+                {
+                    count[i] = 2;
+                }
+            }
+
+
+            return count[n];
+        }
+    }
+}
