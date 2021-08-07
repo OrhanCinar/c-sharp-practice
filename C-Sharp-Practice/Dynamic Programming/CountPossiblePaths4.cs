24644b2c50e035f0f5cccf03e3c92b453a9c891f
Orhan Çınar <orhancinar35@gmail.com>
2021-08-07 16:43:00 +0200

    add Count all possible paths from top left to bottom right of a mXn matrix

---
+++ C-Sharp-Practice/Dynamic Programming/CountPossiblePaths4.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Practice.Dynamic_Programming
+{
+    class CountPossiblePaths4
+    {
+        int NumberOfPaths(int m, int n)
+        {
+            int path = 1;
+
+            for (int i = 1; i < m + n - 1; i++)
+            {
+                path *= i;
+                path /= i - n + 1;
+            }
+
+            return path;
+        }
+    }
+}
