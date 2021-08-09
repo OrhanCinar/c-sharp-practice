601fd82b81389792c56bf22fc9d843190fc8717b
Orhan Çınar <orhancinar35@gmail.com>
2021-08-09 20:28:06 +0200

    add Largest Sum Contiguous Subarray

---
+++ C-Sharp-Practice/Dynamic Programming/LargestSumContiguousSubarray.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Practice.Dynamic_Programming
+{
+    class LargestSumContiguousSubarray
+    {
+        int MaxSubArraySum(int[] a)
+        {
+            int size = a.Length;
+            int max_so_far = int.MinValue, max_ending_here = 0;
+
+            for (int i = 0; i < size; i++)
+            {
+                max_ending_here = max_ending_here + a[i];
+
+                if (max_so_far < max_ending_here)
+                {
+                    max_so_far = max_ending_here;
+                }
+
+                if (max_ending_here < 0)
+                {
+                    max_ending_here = 0;
+                }
+            }
+
+            return max_so_far;
+        }
+    }
+}
