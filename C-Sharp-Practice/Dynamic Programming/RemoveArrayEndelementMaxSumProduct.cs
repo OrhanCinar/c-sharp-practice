dff025cbcb9400204f0a3ece4e2e1067f8b9987d
Orhan Çınar <orhancinar35@gmail.com>
2021-08-23 16:02:23 +0200

    add Remove array end element to maximize the sum of product

---
+++ C-Sharp-Practice/Dynamic Programming/RemoveArrayEndelementMaxSumProduct.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Practice.Dynamic_Programming
+{
+    class RemoveArrayEndelementMaxSumProduct
+    {
+        int MAX = 50;
+
+        int Solve(int[,] dp, int[] a, int low, int high, int turn)
+        {
+            if (low == high)
+            {
+                return a[low] * turn;
+            }
+
+            if (dp[low, high] != 0)
+            {
+                return dp[low, high];
+            }
+
+            dp[low, high] = Math.Max(a[low] * turn + Solve(dp, a, low + 1, high, turn + 1), a[high] * turn + Solve(dp, a, low, high - 1, turn + 1));
+
+            return 0;
+        }
+    }
+}
