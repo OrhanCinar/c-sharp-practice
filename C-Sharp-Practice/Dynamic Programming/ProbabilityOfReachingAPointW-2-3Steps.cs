9e075686346f59dab1cbe3cbb45ae278cc068211
Orhan Çınar <orhancinar35@gmail.com>
2021-08-30 17:11:33 +0200

    add Probability of reaching a point with 2 or 3 steps at a time

---
+++ C-Sharp-Practice/Dynamic Programming/ProbabilityOfReachingAPointW-2-3Steps.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Practice.Dynamic_Programming
+{
+    class ProbabilityOfReachingAPointW_2_3Steps
+    {
+        float Find_prob(int N, float P)
+        {
+            double[] dp = new double[N + 1];
+            dp[0] = 1;
+            dp[1] = 0;
+            dp[2] = P;
+            dp[3] = 1 - P;
+
+
+
+            for (int i = 4; i <= N; ++i)
+            {
+                dp[i] = P * dp[i - 2] + (1 - P) * dp[i - 3];
+            }
+
+            return (float)dp[N];
+        }
+    }
+}
