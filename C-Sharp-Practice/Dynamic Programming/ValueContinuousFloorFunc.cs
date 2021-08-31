0ea0df8c653c05f51a05774bafc7d90551e2d933
Orhan Çınar <orhancinar35@gmail.com>
2021-08-31 13:14:47 +0200

    add Value of continuous floor function : F(x) = F(floor(x/2)) + x

---
+++ C-Sharp-Practice/Dynamic Programming/ValueContinuousFloorFunc.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Practice.Dynamic_Programming
+{
+    class ValueContinuousFloorFunc
+    {
+        int max = 10000;
+        int[] dp;
+
+        void InitDP()
+        {
+            dp = new int[max];
+            for (int i = 0; i < max; i++)
+            {
+                dp[i] = -1;
+            }
+        }
+
+        int Func(int x)
+        {
+            if(x == 0)
+            {
+                return 0;
+            }
+
+            if (dp[x] == -1)
+            {
+                dp[x] = x + Func(x / 2);
+            }
+
+            return dp[x];
+        }
+        void PrintFloor(int[] arr, int n)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write(Func(arr[i]) + " ");
+            }
+        }
+    }
+}
