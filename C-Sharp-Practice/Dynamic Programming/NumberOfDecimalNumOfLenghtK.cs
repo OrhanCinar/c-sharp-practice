3b22ba7d9adb451858d7e1a776a982b27035c799
Orhan Çınar <orhancinar35@gmail.com>
2021-09-01 14:32:48 +0200

    add Number of decimal numbers of length k, that are strict monotone

---
+++ C-Sharp-Practice/Dynamic Programming/NumberOfDecimalNumOfLenghtK.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Practice.Dynamic_Programming
+{
+    class NumberOfDecimalNumOfLenghtK
+    {
+        int DP_s = 9;
+
+        int GetNumStrictMonotone(int len)
+        {
+            int[,] DP = new int[len, DP_s];
+
+            for (int i = 0; i < DP_s; ++i)
+            {
+                DP[0, i] = i + 1;
+            }
+
+            for (int i = 1; i < len; ++i)
+            {
+                for (int j = 1; j < DP_s; j++)
+                {
+                    DP[i, j] = DP[i - 1, j - 1] + DP[i, j - 1];
+                }
+            }
+
+            return DP[len - 1, DP_s - 1];
+        }
+    }
+}
