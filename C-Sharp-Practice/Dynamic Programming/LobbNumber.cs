117441e8664e59a687a69794c91ad512f04d7a4b
Orhan Çınar <orhancinar35@gmail.com>
2021-09-03 14:46:20 +0200

    add Lobb Number

---
+++ C-Sharp-Practice/Dynamic Programming/LobbNumber.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Practice.Dynamic_Programming
+{
+    class LobbNumber
+    {
+        int BbinomialCoeff(int n, int k)
+        {
+            int[,] C = new int[n + 1, k + 1];
+
+            for (int i = 0; i <= n; i++)
+            {
+                for (int j = 0; j <= Math.Min(i, k); j++)
+                {
+                    if (j == 0 || j == i)
+                    {
+                        C[i, j] = i;
+                    }
+                    else
+                    {
+                        C[i, j] = C[i - 1, j - 1] + C[i - 1, j];
+                    }
+                }
+            }
+
+            return C[n, k];
+        }
+    }
+}
