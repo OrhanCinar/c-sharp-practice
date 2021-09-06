aa4ad6abe4f7d7e372f35ad41baff8afca3b6f9d
Orhan Çınar <orhancinar35@gmail.com>
2021-09-06 13:23:27 +0200

    add Delannoy Number

---
+++ C-Sharp-Practice/Dynamic Programming/DelannoyNumber.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Practice.Dynamic_Programming
+{
+    class DelannoyNumber
+    {
+        int Dealnnoy(int n, int m)
+        {
+            if (m == 0 || n == 0)
+            {
+                return 1;
+            }
+
+            return Dealnnoy(m - 1, n) + Dealnnoy(m - 1, n - 1) + Dealnnoy(m, n - 1);
+        }
+    }
+}
