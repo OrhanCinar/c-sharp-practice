3eff5282d1d556809341c4b956e754987cc05f33
Orhan Çınar <orhancinar35@gmail.com>
2021-09-07 14:54:21 +0200

    add Delannoy Number

---
+++ C-Sharp-Practice/Dynamic Programming/DelannoyNumber2.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Practice.Dynamic_Programming
+{
+    class DelannoyNumber2
+    {
+        int Dealnnoy(int n, int m)
+        {
+            int[,] dp = new int[m + 1, n + 1];
+
+            for (int i = 0; i <= m; i++)
+            {
+                dp[i, 0] = 1;
+            }
+
+            for (int i = 0; i < m; i++)
+            {
+                dp[0, i] = 1;
+            }
+
+            for (int i = 1; i <= m; i++)
+            {
+                for (int j = 1; j <= n; j++)
+                {
+                    dp[i, j] = dp[i - 1, j] + dp[i - 1, j - 1] + dp[i, j - 1];
+                }
+            }
+
+            return dp[m, n];
+        }
+    }
+}
