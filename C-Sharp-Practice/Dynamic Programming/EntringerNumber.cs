597058ea66b4afa023dd26a62f9193f03d600a6e
Orhan Çınar <orhancinar35@gmail.com>
2021-09-08 12:40:29 +0200

    add Entringer Number

---
+++ C-Sharp-Practice/Dynamic Programming/EntringerNumber.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Practice.Dynamic_Programming
+{
+    class EntringerNumber
+    {
+        int Zigzag(int n, int k)
+        {
+            if (n == 0 && k == 0)
+            {
+                return 1;
+            }
+
+            if (k == 0)
+            {
+                return 0;
+            }
+
+            return Zigzag(n, k - 1) + Zigzag(n - 1, n - k);
+        }
+    }
+}
