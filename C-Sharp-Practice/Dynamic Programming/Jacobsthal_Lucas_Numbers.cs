f519438b3de0fe9306f352010fce8b02bd51c2fe
Orhan Çınar <orhancinar35@gmail.com>
2021-09-12 16:04:20 +0200

    add Jacobsthal and Jacobsthal-Lucas numbers

---
+++ C-Sharp-Practice/Dynamic Programming/Jacobsthal_Lucas_Numbers.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Practice.Dynamic_Programming
+{
+    class Jacobsthal_Lucas_Numbers
+    {
+        int Jacobsthal(int n)
+        {
+            if (n == 0)
+            {
+                return 0;
+            }
+
+            if (n == 1)
+            {
+                return 1;
+            }
+
+            return Jacobsthal(n - 1) + 2 * Jacobsthal(n - 2);
+        }
+
+
+        int Jacobsthal_Lucas(int n)
+        {
+            if (n == 0)
+            {
+                return 2;
+            }
+
+            if (n == 1)
+            {
+                return 1;
+            }
+
+            return Jacobsthal_Lucas(n - 1) + 2 * Jacobsthal_Lucas(n - 2);
+        }
+    }
+}
