44c3389ded9669879d7e73c38b83f265d8068543
Orhan Çınar <orhancinar35@gmail.com>
2021-09-19 14:50:40 +0200

    add Unbounded Knapsack (Repetition of items allowed)

---
+++ C-Sharp-Practice/Dynamic Programming/UnboundedKnapsack.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Practice.Dynamic_Programming
+{
+    class UnboundedKnapsack
+    {
+        int GetUnboundedKnapsack(int W, int n, int[] val, int[] wt)
+        {
+            int[] dp = new int[W + 1];
+
+            for (int i = 0; i < W; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if (wt[j] <= i)
+                    {
+                        dp[i] = Math.Max(dp[i], dp[i - wt[j]] + val[j]);
+                    }
+                }
+            }
+
+            return dp[W];
+        }
+    }
+}
