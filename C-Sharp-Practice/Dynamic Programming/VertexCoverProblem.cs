a3fb6bfc61d09fe4114ecb8987307c163c78c536
Orhan Çınar <orhancinar35@gmail.com>
2021-09-26 16:02:05 +0200

    add Vertex Cover Problem

---
+++ C-Sharp-Practice/Dynamic Programming/VertexCoverProblem.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Practice.Dynamic_Programming
+{
+    public class node
+    {
+        public int data;
+        public node left, right;
+    };
+
+    class VertexCoverProblem
+    {
+        int VCover(node root)
+        {
+            if (root == null)
+            {
+                return 0;
+            }
+
+            if (root.left == null && root.right == null)
+            {
+                return 0;
+            }
+
+            int size_incl = 1 + VCover(root.left) + VCover(root.right);
+            int size_excl = 0;
+
+            if (root.left != null)
+            {
+                size_excl += 1 + VCover(root.right.left) + VCover(root.right.right);
+            }
+
+            return Math.Min(size_incl, size_excl);
+        }
+    }
+}
