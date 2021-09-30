7bef0b52c547a2f96242c20008b2a17792c54ed7
Orhan Çınar <orhancinar35@gmail.com>
2021-09-30 16:25:23 +0200

    add Partition problem

---
+++ C-Sharp-Practice/Dynamic Programming/PartitionProblem.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Practice.Dynamic_Programming
+{
+    class PartitionProblem
+    {
+        private bool IsSubsetSum(int[] arr, int n, int sum)
+        {
+            if (sum == 0)
+            {
+                return true;
+            }
+
+            if (n == 0 && sum != 0)
+            {
+                return false;
+            }
+
+            if (arr[n - 1] > sum)
+            {
+                return IsSubsetSum(arr, n - 1, sum);
+            }
+
+            return IsSubsetSum(arr, n - 1, sum) || IsSubsetSum(arr, n - 1, sum - arr[arr[n - 1]]);
+        }
+
+        public bool FindPartition(int[] arr, int n)
+        {
+            int sum = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                sum += arr[i];
+            }
+
+            if (sum % 2 != 0)
+            {
+                return false;
+            }
+
+            return IsSubsetSum(arr, n, sum / 2);
+        }
+    }
+}
