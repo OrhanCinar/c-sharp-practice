12811702f55bf9bbe666f6bc1c0a48a0dee1a0b4
Orhan Çınar <orhancinar35@gmail.com>
2021-10-05 17:23:59 +0200

    add High-effort vs. Low-effort Tasks Problem

---
+++ C-Sharp-Practice/Dynamic Programming/HighEffortLowEffortTask.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Practice.Dynamic_Programming
+{
+    class HighEffortLowEffortTask
+    {
+        int MaxTasks(int[] high, int[] low, int n)
+        {
+            if (n <= 0)
+            {
+                return 0;
+            }
+
+            return Math.Max(high[n - 1] + MaxTasks(high, low, (n - 2)), low[n - 1] + MaxTasks(high, low, (n - 1)));
+        }
+    }
+}
