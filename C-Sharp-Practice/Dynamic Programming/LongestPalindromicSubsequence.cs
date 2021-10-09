b63b10e4479b1e55713067864d3a7f683b0628d1
Orhan Çınar <orhancinar35@gmail.com>
2021-10-09 13:26:29 +0200

    add Longest Palindromic Subsequence

---
+++ C-Sharp-Practice/Dynamic Programming/LongestPalindromicSubsequence.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Practice.Dynamic_Programming
+{
+    class LongestPalindromicSubsequence
+    {
+        int Lps(char[] seq, int i, int j)
+        {
+            if (i == j)
+            {
+                return 1;
+            }
+
+            if (seq[i] == seq[j] && i + 1 == j)
+            {
+                return 2;
+            }
+
+            if (seq[i] == seq[j])
+            {
+                return Lps(seq, i + 1, j - 1) + 2;
+            }
+
+            return Math.Max(Lps(seq, i, j - 1), Lps(seq, i + 1, j));
+        }
+    }
+}
