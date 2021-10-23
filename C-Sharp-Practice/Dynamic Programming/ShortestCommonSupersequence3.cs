febd6ac36213559d757a87a208febef1e86f512e
Orhan Çınar <orhancinar35@gmail.com>
2021-10-23 14:39:54 +0200

    add Shortest Common Supersequence

---
+++ C-Sharp-Practice/Dynamic Programming/ShortestCommonSupersequence3.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Practice.Dynamic_Programming
+{
+    class ShortestCommonSupersequence3
+    {
+        int SuperSeq(String X, String Y, int m, int n)
+        {
+            int[,] dp = new int[m + 1, n + 1];
+
+            for (int i = 0; i <= m; i++)
+            {
+                for (int j = 0; j <= n; j++)
+                {
+                    if (i == 0)
+                    {
+                        dp[i, j] = j;
+                    }
+                    else if (j == 0)
+                    {
+                        dp[i, j] = i;
+                    }
+                    else if (X[i - 1] == Y[j - 1])
+                    {
+                        dp[i, j] = 1 + dp[i - 1, j - 1];
+                    }
+                    else
+                    {
+                        dp[i, j] = 1 + Math.Min(dp[i - 1, j], dp[i, j - 1]);
+                    }
+                }
+            }
+
+            return dp[m, n];
+        }
+    }
+}
