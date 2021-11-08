99a1142152ebbc4d0b07c16ebc4ee6002a0f010a
Orhan Çınar <orhancinar35@gmail.com>
2021-11-08 15:02:07 +0100

    add Count number of increasing subsequences of size k

---
+++ C-Sharp-Practice/Dynamic Programming/Countnumberofincsubseqofsizek.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Practice.Dynamic_Programming
+{
+    class Countnumberofincsubseqofsizek
+    {
+        int numOfIncSubseqOfSizeK(int[] arr, int n, int k)
+        {
+            int[,] dp = new int[k, n]; int sum = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                dp[0, i] = 1;
+            }
+
+            for (int l = 1; l < k; l++)
+            {
+                for (int i = l; i < n; i++)
+                {
+                    dp[l, i] = 0;
+
+                    for (int j = l - 1; j < i; j++)
+                    {
+                        if (arr[j] < arr[i])
+                        {
+                            dp[l, i] += dp[l - 1, j];
+                        }
+                    }
+                }
+            }
+
+            for (int i = k; i < n; i++)
+            {
+                sum += dp[k - 1, i];
+            }
+
+
+            return sum;
+        }
+    }
+}
