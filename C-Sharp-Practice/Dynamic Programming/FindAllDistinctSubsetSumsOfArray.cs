f9f42329c056a7b7302301a2319936b41c0d43b8
Orhan Çınar <orhancinar35@gmail.com>
2021-11-16 14:54:35 +0100

    add Find all distinct subset (or subsequence) sums of an array

---
+++ C-Sharp-Practice/Dynamic Programming/FindAllDistinctSubsetSumsOfArray.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Practice.Dynamic_Programming
+{
+    class FindAllDistinctSubsetSumsOfArray
+    {
+        void DistSumRec(int[] arr, int n, int sum, int currindex, HashSet<int> s)
+        {
+            if (currindex > n)
+            {
+                return;
+            }
+
+            if (currindex == n)
+            {
+                s.Add(sum);
+                return;
+            }
+
+            DistSumRec(arr, n, sum + arr[currindex], currindex + 1, s);
+            DistSumRec(arr, n, sum, currindex + 1, s);
+        }
+
+        void PrintDistSum(int[] arr, int n)
+        {
+            HashSet<int> s = new HashSet<int>();
+            DistSumRec(arr, n, 0, 0, s);
+
+            foreach (int i in s)
+            {
+                Console.Write(i + " ");
+            }
+        }
+    }
+}
