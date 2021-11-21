04c9307c48839e71ac6fff855f7650f6ff1b5b56
Orhan Çınar <orhancinar35@gmail.com>
2021-11-21 15:18:19 +0100

    add Length of Longest Balanced Subsequence

---
+++ C-Sharp-Practice/Dynamic Programming/LengthofLongestBalancedSubsequence2.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Practice.Dynamic_Programming
+{
+    class LengthofLongestBalancedSubsequence2
+    {
+        int MaxLength(String s, int n)
+        {
+            int invalidOpenBraces = 0;
+            int invalidCloseBraces = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                if (s[i] == '(')
+                {
+                    invalidOpenBraces++;
+                }
+                else
+                {
+                    if (invalidOpenBraces == 0)
+                    {
+                        invalidCloseBraces++;
+                    }
+                    else
+                    {
+                        invalidOpenBraces--;
+                    }
+                }
+            }
+
+            return n - invalidOpenBraces + invalidCloseBraces;
+        }
+    }
+}
