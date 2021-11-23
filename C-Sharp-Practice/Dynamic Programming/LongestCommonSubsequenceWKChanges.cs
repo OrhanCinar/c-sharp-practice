bf5314d4a28203bafe6b00446a60cdb3fa79ba3d
Orhan Çınar <orhancinar35@gmail.com>
2021-11-23 02:10:31 +0100

    add Longest Common Subsequence with at most k changes allowed

---
+++ C-Sharp-Practice/Dynamic Programming/LongestCommonSubsequenceWKChanges.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Practice.Dynamic_Programming
+{
+    class LongestCommonSubsequenceWKChanges
+    {
+        int MAX = 10;
+
+
+        int Lcs(int[,,] dp, int[] arr1,
+                        int n, int[] arr2,
+                        int m, int k)
+        {
+            if (k < 0)
+            {
+                return -10000000;
+            }
+
+            if (n <0 || m < 0)
+            {
+                return 0;
+            }
+
+            int ans = dp[n, m, k];
+
+            if (ans != -1)
+            {
+                return ans;
+            }
+
+            ans = Math.Max(Lcs(dp, arr1, n - 1, arr2, m, k), Lcs(dp, arr1, n, arr2, m - 1, k));
+
+            if (arr1[n-1] == arr2[m - 1])
+            {
+                ans = Math.Max(ans, 1 + Lcs(dp, arr1, n - 1, arr2, m - 1, k));
+            }
+
+            return ans;
+        }
+    }
+}
