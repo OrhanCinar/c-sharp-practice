b5a24fdb837d66d9172815d8c4a62c60417fe514
Orhan Çınar <orhancinar35@gmail.com>
2021-12-01 14:15:35 +0100

    add Count all increasing subsequences

---
+++ C-Sharp-Practice/Dynamic Programming/CountAllIncreasingSubSeq.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Practice.Dynamic_Programming
+{
+    class CountAllIncreasingSubSeq
+    {
+        int CountSub(int[] arr, int n)
+        {
+            int[] count = new int[10];
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = arr[i] - 1; j >= 0; j--)
+                {
+                    count[arr[i]] += count[j];
+                }
+
+                count[arr[i]]++;
+            }
+
+            int result = 0;
+
+            for (int i = 0; i < 10; i++)
+            {
+                result += count[i];
+            }
+
+            return result;
+        }
+    }
+}
