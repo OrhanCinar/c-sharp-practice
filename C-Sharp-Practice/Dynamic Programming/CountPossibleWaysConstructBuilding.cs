e77b6de34c87e4b82d860b682fb4f77c6507089b
Orhan Çınar <orhancinar35@gmail.com>
2021-12-03 14:49:28 +0100

    add Count possible ways to construct buildings

---
+++ C-Sharp-Practice/Dynamic Programming/CountPossibleWaysConstructBuilding.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Practice.Dynamic_Programming
+{
+    class CountPossibleWaysConstructBuilding
+    {
+        int CountWays(int N)
+        {
+            if (N == 1)
+            {
+                return 4;
+            }
+
+            int countB = 1, countS = 1, prevcountB, prevcountS;
+
+            for (int i = 2; i < N; i++)
+            {
+                prevcountB = countB;
+                prevcountS = countS;
+
+                countS = prevcountB + prevcountS;
+                countB = prevcountS;
+            }
+
+            int result = countS + countB;
+
+            return result * result;
+        }
+    }
+}
