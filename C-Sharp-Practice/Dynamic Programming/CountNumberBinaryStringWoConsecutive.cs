b0f7efecb3c92a0e3d622c977a756a083f6d81c2
Orhan Çınar <orhancinar35@gmail.com>
2021-12-07 15:11:43 +0100

    add Count number of binary strings without consecutive 1’s

---
+++ C-Sharp-Practice/Dynamic Programming/CountNumberBinaryStringWoConsecutive.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Practice.Dynamic_Programming
+{
+    class CountNumberBinaryStringWoConsecutive
+    {
+        int CountStrings(int n)
+        {
+            int[] a = new int[n];
+            int[] b = new int[n];
+
+            for (int i = 1; i < n; i++)
+            {
+                a[i] = a[i - 1] + b[i - 1];
+                b[i] = a[i - 1];
+            }
+
+            return a[n - 1] + b[n - 1];
+        }
+    }
+}
