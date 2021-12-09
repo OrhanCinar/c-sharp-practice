4fdcd714bb19d5bd014ca9cae2e3d1376b724862
Orhan Çınar <orhancinar35@gmail.com>
2021-12-09 12:07:05 +0100

    add Count Possible Decodings of a given Digit Sequence

---
+++ C-Sharp-Practice/Dynamic Programming/CountPossibleDecodings.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Practice.Dynamic_Programming
+{
+    class CountPossibleDecodings
+    {
+        int CountDecoding(char[] digits, int n)
+        {
+            if (n == 0 || n == 1)
+            {
+                return 1;
+            }
+
+            int count = 0;
+
+            if (digits[n - 1] > '0')
+            {
+                count = CountDecoding(digits, n - 1);
+            }
+
+            if (digits[n - 2] == '1' || digits[n - 2] == '2' && digits[n - 1] < '7')
+            {
+                count += CountDecoding(digits, n - 2);
+            }
+
+            return count;
+        }
+    }
+}
