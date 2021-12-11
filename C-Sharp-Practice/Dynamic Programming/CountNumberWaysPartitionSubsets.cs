9c04b19577c157707a7bf72754d67e005190d7a0
Orhan Çınar <orhancinar35@gmail.com>
2021-12-11 15:11:52 +0100

    add Count number of ways to partition a set into k subsets

---
+++ C-Sharp-Practice/Dynamic Programming/CountNumberWaysPartitionSubsets.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Practice.Dynamic_Programming
+{
+    internal class CountNumberWaysPartitionSubsets
+    {
+        int CountP(int n, int k)
+        {
+            if (n == 0 || k == 0 || k > n)
+            {
+                return 0;
+            }
+
+            if (k == 1 || k == n)
+            {
+                return 1;
+            }
+
+            return (k * CountP(n - 1, k) + CountP(n - 1, k - 1);
+        }
+    }
+}
