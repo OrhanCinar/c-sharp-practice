51694315cbe090a3be4b6cae9b30cf442e49ac8d
Orhan Çınar <orhancinar35@gmail.com>
2021-12-19 16:15:49 +0100

    add Count of strings that can be formed using a, b and c under given constraints

---
+++ C-Sharp-Practice/Dynamic Programming/CountStringsCanFormABC3.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Practice.Dynamic_Programming
+{
+    internal class CountStringsCanFormABC3
+    {
+        int CountStr(int n)
+        {
+            return 1 + (n * 2) + (n * ((n * n) - 1) / 2);
+        }
+    }
+}
