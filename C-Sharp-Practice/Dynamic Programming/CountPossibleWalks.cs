73cd7992acd3d6903b3645a72e645dbd34aab8a3
Orhan Çınar <orhancinar35@gmail.com>
2021-12-22 15:22:33 +0100

    add Count all possible walks from a source to a destination with exactly k edges

---
+++ C-Sharp-Practice/Dynamic Programming/CountPossibleWalks.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Practice.Dynamic_Programming
+{
+    internal class CountPossibleWalks
+    {
+        int V = 4;
+
+        int Countwalks(int[,] graph, int u, int v, int k)
+        {
+            if (k == 0 && u == v)
+            {
+                return 1;
+            }
+
+            if (k == 1 && graph[u, v] == 1)
+            {
+                return 1;
+            }
+
+            if (k <= 0)
+            {
+                return 0;
+            }
+
+            int count = 0;
+
+            for (int i = 0; i < V; i++)
+            {
+                if (graph[u, i] == 1)
+                {
+                    count += Countwalks(graph, i, v, k - 1);
+                }
+            }
+
+            return count;
+        }
+    }
+}
