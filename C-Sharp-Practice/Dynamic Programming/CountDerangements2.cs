2e1b99e21453e5a6bb3f140fbca8cfcec2e091b2
Orhan Çınar <orhancinar35@gmail.com>
2021-12-25 15:41:20 +0100

    add Count Derangements

---
+++ C-Sharp-Practice/Dynamic Programming/CountDerangements2.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Practice.Dynamic_Programming
+{
+    internal class CountDerangements2
+    {
+        int CountDer(int n)
+        {
+            int[] der = new int[n + 1];
+
+            der[1] = 0;
+            der[2] = 1;
+
+            for (int i = 3; i <= n; i++)
+            {
+                der[i] = (i - 1) * (der[i - 1] + der[i - 2]);
+            }
+
+            return der[n];
+        }
+    }
+}
