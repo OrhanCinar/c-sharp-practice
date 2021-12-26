4ed4cc881fd95bd0dc7062427460251de22697c5
Orhan Çınar <orhancinar35@gmail.com>
2021-12-26 16:05:44 +0100

    add Count Derangements

---
+++ C-Sharp-Practice/Dynamic Programming/CountDerangements3.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Practice.Dynamic_Programming
+{
+    internal class CountDerangements3
+    {
+        int CountDer(int n)
+        {
+            if (n == 1 || n == 2)
+            {
+                return n - 1;
+            }
+
+            int a = 0;
+            int b = 1;
+
+            for (int i = 3; i <= n; i++)
+            {
+                int cur = (i - 1) * (a + b);
+                a = b;
+                b = cur;
+            }
+
+            return b;
+        }
+    }
+}
