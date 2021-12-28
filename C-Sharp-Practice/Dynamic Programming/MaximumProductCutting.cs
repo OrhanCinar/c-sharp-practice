2f92723931e2de76e8808bad8d4d279d50caac60
Orhan Çınar <orhancinar35@gmail.com>
2021-12-28 16:00:05 +0100

    add Maximum Product Cutting

---
+++ C-Sharp-Practice/Dynamic Programming/MaximumProductCutting.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Practice.Dynamic_Programming
+{
+    internal class MaximumProductCutting
+    {
+        int MaxProd(int n)
+        {
+            if (n == 2 || n == 3)
+            {
+                return n - 1;
+            }
+
+            int res = 1;
+
+            while (n > 4)
+            {
+                n -= 3;
+                res *= 3;
+            }
+
+            return n * res;
+        }
+    }
+}
