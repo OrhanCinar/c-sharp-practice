9a4b7222bd36e605f4af7ca1e9c94621c95c5930
Orhan Çınar <orhancinar35@gmail.com>
2022-01-16 13:59:29 +0100

    add Maximum sum subsequence with at-least k distant elements

---
+++ C-Sharp-Practice/Dynamic Programming/MaxSumSubseqWLeastKDistant.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Practice.Dynamic_Programming
+{
+    internal class MaxSumSubseqWLeastKDistant
+    {
+        int MaxSum(int[] arr, int N, int k)
+        {
+            int[] MS = new int[N];
+
+            MS[N - 1] = arr[N - 1];
+
+            for (int i = N - 2; i >= 0; i--)
+            {
+                if (i + k + 1 >= N)
+                {
+                    MS[i] = Math.Max(arr[i], MS[i + 1]);
+                }
+                else
+                {
+                    MS[i] = Math.Max(arr[i] + MS[i + k + 1], MS[i + 1]);
+                }
+            }
+
+            return MS[0];
+        }
+    }
+}
