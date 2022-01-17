d8564c5c02f202a0d0cdc7dbd0ef5f36fac7c7f6
Orhan Çınar <orhancinar35@gmail.com>
2022-01-17 14:22:29 +0100

    add Maximum profit by buying and selling a share at most twice

---
+++ C-Sharp-Practice/Dynamic Programming/MaxProfitBuySellShareTwice.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Practice.Dynamic_Programming
+{
+    internal class MaxProfitBuySellShareTwice
+    {
+        int MaxProfit(int[] price, int n)
+        {
+            int[] profit = new int[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                profit[i] = 0;
+            }
+
+            int max_price = profit[n - 1];
+
+            for (int i = n - 2; i >= 0; i--)
+            {
+                if (profit[i] > max_price)
+                {
+                    max_price = price[i];
+                }
+
+                profit[i] = Math.Max(profit[i + 1], max_price - price[i]);
+            }
+
+            int min_price = price[0];
+
+            for (int i = 1; i < n; i++)
+            {
+                if (price[i] < min_price)
+                {
+                    min_price = price[i];
+                }
+
+                profit[i] = Math.Max(profit[i - 1], profit[i] - min_price);
+            }
+
+            int result = profit[n - 1];
+
+            return result;
+        }
+    }
+}
