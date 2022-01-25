4a46ebbada92c85ecf16ba9813b7482186b291eb
Orhan Çınar <orhancinar35@gmail.com>
2022-01-25 15:56:56 +0100

    add Maximum number of trailing zeros in the product of the subsets of size k

---
+++ C-Sharp-Practice/Dynamic Programming/MaxNumberTrailingZero.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Practice.Dynamic_Programming
+{
+    internal class MaxNumberTrailingZero
+    {
+        int MAX5 = 100;
+
+        int MaximumZeros(int[] arr, int n, int k)
+        {
+            int[,] subset = new int[k + 1, MAX5 + 5];
+
+            for (int i = 0; i < subset.GetLength(0); i++)
+            {
+                for (int j = 0; j < subset.GetLength(1); j++)
+                {
+                    subset[i, j] = -1;
+                }
+            }
+
+            subset[0, 0] = 0;
+
+            for (int p = 0; p < n; p++)
+            {
+                int pw2 = 0, pw5 = 0;
+
+                while (arr[p] % 2 == 0)
+                {
+                    pw2++;
+                    arr[p] /= 2;
+                }
+
+                while (arr[p] % 5 == 0)
+                {
+                    pw5++;
+                    arr[p] /= 5;
+                }
+
+                for (int i = k - 1; i >= 0; i--)
+                {
+                    for (int j = 0; j < MAX5; j++)
+                    {
+                        if (subset[i, j] != -1)
+                        {
+                            subset[i + 1, j + pw5] = Math.Max(subset[i + 1, j + pw5], subset[i, j] + pw2);
+                        }
+                    }
+                }
+            }
+
+            int ans = 0;
+
+            for (int i = 0; i < MAX5; i++)
+            {
+                ans = Math.Max(ans, Math.Min(i, subset[k, i]));
+            }
+
+            return ans;
+        }
+    }
+}
