042b4d8d2e30b3689354b796a0113b6ec8baec40
Orhan Çınar <orhancinar35@gmail.com>
2022-01-27 15:46:26 +0100

    add Minimum insertions to sort an array

---
+++ C-Sharp-Practice/Dynamic Programming/MinimumInsertionsToSortArray.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Practice.Dynamic_Programming
+{
+    internal class MinimumInsertionsToSortArray
+    {
+        int MinInsertionStepToSortArray(int[] arr, int N)
+        {
+            int[] lis = new int[N];
+
+            for (int i = 0; i < N; i++)
+            {
+                lis[i] = 1;
+            }
+
+            for (int i = 1; i < N; i++)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    if (arr[i] >= arr[j] && lis[i] < lis[j] + 1)
+                    {
+                        lis[i] = lis[j] + 1;
+                    }
+                }
+            }
+
+            int max = 0;
+
+            for (int i = 0; i < N; i++)
+            {
+                if (max < lis[i])
+                {
+                    max = lis[i];
+                }
+            }
+
+            return N - max;
+        }
+    }
+}
