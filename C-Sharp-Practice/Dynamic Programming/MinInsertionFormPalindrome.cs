0362082721a0b8b71d229adcd7d794b8a81b8b14
Orhan Çınar <orhancinar35@gmail.com>
2022-02-02 15:04:28 +0100

    add Minimum insertions to form a palindrome

---
+++ C-Sharp-Practice/Dynamic Programming/MinInsertionFormPalindrome.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Practice.Dynamic_Programming
+{
+    internal class MinInsertionFormPalindrome
+    {
+        int FindMinInsertions(char[] str, int l, int h)
+        {
+            if (l > h)
+            {
+                return int.MaxValue;
+            }
+
+            if (l == h)
+            {
+                return 0;
+            }
+
+            if (l == h - 1)
+            {
+                return str[l] == str[h] ? 0 : 1;
+            }
+
+            return str[l] == str[h] ? FindMinInsertions(str, l + 1, h - 1) : Math.Min(FindMinInsertions(str, l, h - 1), FindMinInsertions(str, l + 1, h)) + 1;
+        }
+    }
+}
