fd52a698f0ace4287bbba465f03a2f5a5720a9d1
Orhan Çınar <orhancinar35@gmail.com>
2022-02-06 17:33:26 +0100

    add Minimum number of deletions to make a string palindrome

---
+++ C-Sharp-Practice/Dynamic Programming/MinNumberDeletionMakeStringPalindrome2.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Practice.Dynamic_Programming
+{
+    internal class MinNumberDeletionMakeStringPalindrome2
+    {
+        int Delete(string str, int i, int j)
+        {
+            if (i >= j)
+            {
+                return 0;
+            }
+
+
+            if (str[i] == str[j])
+            {
+                return Delete(str, i + 1, j - 1);
+            }
+
+            return 1 + Math.Min(Delete(str, i + 1, j), Delete(str, i, j - 1));
+        }
+
+        int DeleteMinElement(string str)
+        {
+            return Delete(str, 0, str.Length - 1);
+        }
+    }
+}
