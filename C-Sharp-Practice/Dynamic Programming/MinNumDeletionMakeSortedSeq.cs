667ceda8015a17c9acd6ee1b2a1ae555816b3ca8
Orhan Çınar <orhancinar35@gmail.com>
2022-02-12 15:16:27 +0100

    add Minimum number of deletions to make a sorted sequence

---
+++ C-Sharp-Practice/Dynamic Programming/MinNumDeletionMakeSortedSeq.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Practice.Dynamic_Programming
+{
+    internal class MinNumDeletionMakeSortedSeq
+    {
+        int Lis(int[] arr, int n)
+        {
+            int result = 0;
+            int[] lis = new int[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                lis[i] = 1;
+            }
+
+
+            for (int i = 1; i < n; i++)
+            {
+                for (int j = 0; j < i; j++)
+                {
+                    if (arr[i] > arr[j] && lis[i] < lis[j] + 1)
+                    {
+                        lis[i] = lis[j] + 1;
+                    }
+                }
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+                if(result < lis[i])
+                {
+                    result = lis[i];
+                }
+            }
+
+            return result;
+        }
+
+        int MinimumNumberOfDeletions(int[] arr, int n)
+        {
+            int len = Lis(arr, n);
+            
+            return (n - len);
+        }
+    }
+}
