363493ddce3fec75995a9e4f646db893f2746e51
Orhan Çınar <orhancinar35@gmail.com>
2022-02-15 14:47:42 +0100

    add Minimum number of squares whose sum equals to given number n

---
+++ C-Sharp-Practice/Dynamic Programming/MinNumSquareSumEqualsToN3.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Practice.Dynamic_Programming
+{
+    internal class MinNumSquareSumEqualsToN3
+    {
+        int MinCount(int n)
+        {
+            int[] minSquareRequired = new int[n + 1];
+            minSquareRequired[0] = 0;
+            minSquareRequired[1] = 1;
+
+
+            for (int i = 2; i <= n; i++)
+            {
+                minSquareRequired[i] = Int32.MaxValue;
+
+                for (int j = 1; i - (j * j) >= 0; ++j)
+                {
+                    minSquareRequired[i] = Math.Min(minSquareRequired[i], minSquareRequired[i - (j * j)]);
+                }
+
+                minSquareRequired[i] += 1;
+            }
+
+            int result = minSquareRequired[n];
+            return result;
+        }
+    }
+}
