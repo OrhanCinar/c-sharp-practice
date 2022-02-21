52a2064165a5cbf7e29c33e68ad18953962be258
Orhan Çınar <orhancinar35@gmail.com>
2022-02-21 16:42:06 +0100

    add Remove minimum elements from either side such that 2*min becomes more than max

---
+++ C-Sharp-Practice/Dynamic Programming/RemoveMinElement2MinMax3.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Practice.Dynamic_Programming
+{
+    internal class RemoveMinElement2MinMax3
+    {
+        int MinRemovalsDP(int[] arr, int n)
+        {
+            int longest_start = -1, longest_end = 0;
+
+            for (int start = 0; start < n; start++)
+            {
+                int min = int.MaxValue, max = int.MinValue;
+
+
+                for (int end = start; end < n; end++)
+                {
+                    int val = arr[end];
+
+                    if (val < min)
+                    {
+                        min = val;
+                    }
+
+                    if (val > max)
+                    {
+                        max = val;
+                    }
+
+                    if (2 * min <= max)
+                    {
+                        break;
+                    }
+
+                    if (end - start > longest_end - longest_start || longest_start == -1)
+                    {
+                        longest_start = start;
+                        longest_end = end;
+                    }
+                }
+            }
+
+            if (longest_start == -1)
+            {
+                return n;
+            }
+
+            return (n - (longest_end - longest_start + 1));
+        }
+    }
+}
