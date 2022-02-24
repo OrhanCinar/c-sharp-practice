5a3c91d6d592484db898be92e122cee8f8e62d08
Orhan Çınar <orhancinar35@gmail.com>
2022-02-24 15:56:16 +0100

    add Clustering/Partitioning an array such that sum of square differences is minimum

---
+++ C-Sharp-Practice/Dynamic Programming/ClusterArraySumSquare.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Practice.Dynamic_Programming
+{
+    internal class ClusterArraySumSquare
+    {
+        int inf = 1000000000;
+        int ans = 1000000000;
+
+        void Solve(int i, int par, int[] a, int n, int k, int current_ans)
+        {
+            if (par > k)
+            {
+                return;
+            }
+
+            if (par == k && i == n - 1)
+            {
+                ans = Math.Min(ans, current_ans);
+                return;
+            }
+
+            for (int j = i + 1; j < n; j++)
+            {
+                Solve(j, par + 1, a, n, k, current_ans + (a[j] - a[i + 1]) * (a[j] - a[i + 1]));
+            }
+        }
+    }
+}
