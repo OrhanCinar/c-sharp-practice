3250847f1f84cf58631dd9f8c78f664a23652a76
Orhan Çınar <orhancinar35@gmail.com>
2022-03-02 15:22:32 +0100

    add Minimum time to finish tasks without skipping two consecutive

---
+++ C-Sharp-Practice/Dynamic Programming/MinTimeToFinishTask.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Practice.Dynamic_Programming
+{
+    internal class MinTimeToFinishTask
+    {
+        int MinTime(int[] arr, int n)
+        {
+            if (n <= 0)
+            {
+                return 0;
+            }
+
+            int incl = arr[0];
+
+            int excl = 0;
+
+            for (int i = 1; i < n; i++)
+            {
+                int incl_new = arr[i] + Math.Min(excl, incl);
+                int excl_new = incl;
+
+                incl = incl_new;
+                excl = excl_new;
+            }
+
+            return Math.Min(incl, excl);
+
+        }
+    }
+}
