94c02a96843c0a70636017ca2494dacbdc825c4d
Orhan Çınar <orhancinar35@gmail.com>
2022-03-04 15:29:34 +0100

    add Minimum number of deletions and insertions to transform one string into another

---
+++ C-Sharp-Practice/Dynamic Programming/MinNumberDelInsertTransString.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Practice.Dynamic_Programming
+{
+    internal class MinNumberDelInsertTransString
+    {
+        int Lcs(string str1, string str2, int m, int n)
+        {
+            int[,] L = new int[m + 1, n + 1];
+            int i, j;
+
+            for (i = 0; i <= m; i++)
+            {
+                for (j = 0; j <= n; j++)
+                {
+                    if (i == 0 || j == 0)
+                    {
+                        L[i, j] = 0;
+                    }
+                    else if (str1[i - 1] == str2[j - 1])
+                    {
+                        L[i, j] = L[i - 1, j - 1] + 1;
+                    }
+                    else
+                    {
+                        L[i, j] = Math.Max(L[i - 1, j], L[i, j - 1]);
+                    }
+                }
+            }
+
+            return L[m, n];
+        }
+    }
+}
