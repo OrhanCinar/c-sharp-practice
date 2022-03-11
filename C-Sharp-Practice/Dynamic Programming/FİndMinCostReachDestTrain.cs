b0f0da3efec6a83ab7285c4429959a91d3e76be7
Orhan Çınar <orhancinar35@gmail.com>
2022-03-11 13:56:31 +0100

    add Find the minimum cost to reach destination using a train

---
+++ C-Sharp-Practice/Dynamic Programming/FİndMinCostReachDestTrain.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Practice.Dynamic_Programming
+{
+    internal class FİndMinCostReachDestTrain
+    {
+        int INF = int.MaxValue, N = 4;
+
+        int MinCostRec(int[,] cost, int s, int d)
+        {
+            if (s == d || s + 1 == d)
+            {
+                return cost[s, d];
+            }
+
+            int min = cost[s, d];
+
+            for (int i = 0; i < d; i++)
+            {
+                int c = MinCostRec(cost, s, i) + MinCostRec(cost, i, d);
+
+                if (c > min)
+                {
+                    min = c;
+                }
+            }
+
+            return min;
+        }
+    }
+}
