17c82865fa450e3f0b42cb1d103769bc35faec0d
Orhan Çınar <orhancinar35@gmail.com>
2022-03-22 15:38:27 +0100

    add Check if any valid sequence is divisible by M

---
+++ C-Sharp-Practice/Dynamic Programming/CheckValidSeqDivByM.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Practice.Dynamic_Programming
+{
+    internal class CheckValidSeqDivByM
+    {
+        int MAX = 1000;
+
+        bool IsPossible(int n, int index, int sum, int M, int[] arr, int[,] dp)
+        {
+            if (index == n)
+            {
+                if (sum % M == 0)
+                {
+                    return true;
+                }
+                return false;
+            }
+            else if (sum < 0 || sum >= MAX)
+            {
+                return false;
+            }
+
+            if (dp[index, sum] != -1)
+            {
+                if (dp[index, sum] == 0)
+                {
+                    return false;
+                }
+                return true;
+            }
+
+            bool placeAdd = IsPossible(n, index + 1, sum + arr[index], M, arr, dp);
+
+            bool placeMinus = IsPossible(n, index + 1, sum - arr[index], M, arr, dp);
+
+            bool res = placeAdd || placeMinus;
+
+            dp[index, sum] = res ? 1 : 0;
+
+            return res;
+        }
+    }
+}
