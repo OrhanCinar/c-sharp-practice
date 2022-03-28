01e1cb4a87e60f6e3ef30ecc6bcdc9d6266a2ded
Orhan Çınar <orhancinar35@gmail.com>
2022-03-28 16:13:43 +0200

    add Given a large number, check if a subsequence of digits is divisible by 8

---
+++ C-Sharp-Practice/Dynamic Programming/GivenLargeNumberCheckSuSeqDivBy8.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Sharp_Practice.Dynamic_Programming
+{
+    internal class GivenLargeNumberCheckSuSeqDivBy8
+    {
+        bool IsSubSeqDivisible(string str)
+        {
+            int i, j, k, l, n = str.Length;
+
+            int[] arr = new int[l];
+
+            for (i = 0; i < n; i++)
+            {
+                arr[i] = str[i] - '0';
+            }
+
+            for (i = 0; i < l; i++)
+            {
+                for (j = 0; j < l; j++)
+                {
+                    for (k = 0; k < l; k++)
+                    {
+                        if (arr[i] % 8 == 0)
+                        {
+                            return true;
+                        }
+                        else if ((arr[i] * 10 + arr[j]) % 8 == 0)
+                        {
+                            return true;
+                        }
+                        else if ((arr[i] * 100 + arr[j] * 10 + arr[k]) % 8 == 0 && i != j && j != k && i != k)
+                        {
+                            return true;
+                        }
+                    }
+                }
+            }
+
+            return false;
+        }
+    }
+}
